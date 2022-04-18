87b2039f93e81a7f9797b25e81df55a93858e822
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-17 20:44:56 -0400

    Time: 112 ms (73.63%), Space: 17.1 MB (35.45%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,66 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        crsDic = defaultdict(list)
+        
+        adj_list = defaultdict(list)
+        visited = set()
+        
+        for crs, pre in prerequisites:
+            adj_list[crs].append(pre)
+        
+        
+        def dfs(crs):
+            if crs in visited:
+                return False
+            visited.add(crs)
+            for pre in adj_list[crs]:
+                if not dfs(pre):
+                    return False
+            adj_list[crs] = []
+            visited.remove(crs)
+            return True
+            
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        return True
+        
+        
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         preMap = {i: [] for i in range(numCourses)}
+
+#         for crs, pre in prerequisites:
+#             preMap[crs].append(pre)
+
+#         visited = set()
+
+#         def dfs(crs):
+#             if crs in visited:
+#                 return False
+#             if preMap[crs] == []:
+#                 return True
+#             visited.add(crs)
+#             for pre in preMap[crs]:
+#                 if not dfs(pre):
+#                     return False
+#             visited.remove(crs)
+#             preMap[crs] = []
+#             return True
+
+#         for crs in range(numCourses):
+#             if not dfs(crs):
+#                 return False
+
+#         return True
+
+        
