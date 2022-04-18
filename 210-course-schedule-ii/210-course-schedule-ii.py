a248b1cd566c61a2f53d7832c86752b9be53a883
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-17 21:17:34 -0400

    Time: 106 ms (83.23%), Space: 15.5 MB (88.01%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,64 +1,71 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-
+        
         adj_list = defaultdict(list)
-        visited, cycle = set(), set()
+        indegree = {}
         res = []
         
         for crs, pre in prerequisites:
-            adj_list[crs].append(pre)
+            adj_list[pre].append(crs)
+            indegree[crs] = indegree.get(crs, 0) + 1
         
-        def dfs(crs):
+        queue = deque([c for c in range(numCourses) if c not in indegree])
+        
+        while queue:
+            pre = queue.popleft()
+            res.append(pre)
             
-            if crs in cycle:
-                return False
-            if crs in visited:
-                return True
+            for crs in adj_list[pre]:
+                indegree[crs] -=1
+                
+                if indegree[crs] == 0:
+                    queue.append(crs)
             
-            cycle.add(crs)
-            for pre in adj_list[crs]:
-                if not dfs(pre):
-                    return False
-            cycle.remove(crs)
-            visited.add(crs)
-            res.append(crs)
-            return True
+                    
+        return res if len(res) == numCourses else []
         
         
-        for crs in range(numCourses):
-            if not dfs(crs):
-                return []
-        return res
+        
+        
+        
+        
+        
+
+#         adj_list = defaultdict(list)
+#         visited, cycle = set(), set()
+#         res = []
+        
+#         for crs, pre in prerequisites:
+#             adj_list[crs].append(pre)
+        
+#         def dfs(crs):
+            
+#             if crs in cycle:
+#                 return False
+#             if crs in visited:
+#                 return True
+            
+#             cycle.add(crs)
+#             for pre in adj_list[crs]:
+#                 if not dfs(pre):
+#                     return False
+#             cycle.remove(crs)
+#             visited.add(crs)
+#             res.append(crs)
+#             return True
+        
+        
+#         for crs in range(numCourses):
+#             if not dfs(crs):
+#                 return []
+#         return res
         
         
     
         
         
         
-        
-        
-        
-#         indegree = {}
-#         adj_list = defaultdict(list)
-#         res = []
-        
-#         for crs, pre in prerequisites:
-#             indegree[crs] = indegree.get(crs, 0) + 1
-#             adj_list[pre].append(crs)
-        
-#         queue =deque([c for c in range(numCourses) if c not in indegree])
-        
-#         while queue:
-#             pre = queue.popleft()
-#             res.append(pre)
-            
-#             for crs in adj_list[pre]:
-#                 indegree[crs] -=1
-#                 if indegree[crs] == 0:
-#                     queue.append(crs)
-        
-#         return res if len(res) == numCourses else []
-                    
+    
 
 
             
