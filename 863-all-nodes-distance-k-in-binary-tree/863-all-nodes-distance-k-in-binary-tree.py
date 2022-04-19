c8a5344cb52f5b89a64746dd72fec17672160551
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-18 23:07:53 -0400

    Time: 38 ms (87.79%), Space: 14.3 MB (77.35%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        
+        res = []
+        
+        parents = {}
+        
+        def findParent(node, parent):
+            if node:
+                parents[node] = parent
+                findParent(node.left, node)
+                findParent(node.right, node)
+                
+        findParent(root, None)
+        
+        visited = set()
+        
+        def search(node, distance):
+            
+            if not node or node.val in visited:
+                return 
+            visited.add(node.val)
+
+            if distance == k :
+                res.append(node.val)
+
+            for neighbor in [parents[node], node.left, node.right]:
+                search(neighbor, distance + 1)
+      
+        search(target, 0)       
+   
+        return res
+                
