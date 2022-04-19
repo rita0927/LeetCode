dfc7daf6d73b90ba27307240d28013dc3da6586b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-18 20:53:06 -0400

    Time: 34 ms (84.45%), Space: 14.3 MB (20.23%) - LeetHub

---
+++ 364-nested-list-weight-sum-ii/364-nested-list-weight-sum-ii.py
@@ -0,0 +1,93 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        
+        self.total = 0
+        maxDepth = 0
+        
+        stack1 = [(nestedList, 1)]
+        while stack1:
+            lst, level = stack1.pop()
+            
+            for el in lst:
+                if el.isInteger(): 
+                    maxDepth = max(maxDepth, level)
+                else:
+                    stack1.append((el.getList(), level + 1))
+                    
+        stack2 = [(nestedList,1)]
+        while stack2:
+            lst, level = stack2.pop()
+            
+            for el in lst:
+                if el.isInteger():
+                    self.total += el.getInteger() * (maxDepth - level + 1)
+                else:
+                    stack2.append((el.getList(), level + 1))
+                        
+        return self.total 
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
