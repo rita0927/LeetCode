11c15bf00a6f91a2698b00863d72930e4bb3641d
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-20 20:17:56 -0400

    Time: 61 ms (11.34%), Space: 13.8 MB (59.18%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -0,0 +1,93 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        queue = deque([root])
+        
+        while queue:
+            node = queue.popleft()
+            
+            if node:
+                left = node.left
+                right = node.right
+                
+                queue.append(left)
+                queue.append(right)
+                
+                temp = left
+                node.left = right
+                node.right = temp
+     
+        return root
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not root:
+#             return None
+        
+#         stack = [root]
+        
+#         while stack:
+#             node = stack.pop()
+#             if node:
+#                 temp = node.left
+#                 node.left = node.right
+#                 node.right = temp
+            
+#                 stack.append(node.left)
+#                 stack.append(node.right)
+                
+#         return root
+            
+            
+        
+        
+   
+        
+        
+#         if not root:
+#             return None
+        
+#         root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        
+#         # right = self.invertTree(root.right)
+#         # left = self.invertTree(root.left)
+#         # root.right = left
+#         # root.left = right
+#         return root 
+
