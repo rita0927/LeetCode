2863b80ab7cb2bb65fcda164f5781215f2bd806b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-04-28 22:09:45 -0400

    Time: 51 ms (33.28%), Space: 13.8 MB (67.21%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,20 +1,56 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        opens = []
-        i = 0
+        stack = []
         
-        while i < len(s):
-            if s[i] == "(":
-                opens.append(i)
-                i+=1
-            elif s[i] == ")":
-                start = opens.pop()
-                sub = s[start + 1:i]
-                sub = sub[::-1]
-                s = s[:start] + sub + s[i + 1:]
-                i-=1
+        for i in range(len(s)):
+            if s[i] != ")":
+                stack.append(s[i])
+            
             else:
-                i +=1
+                temp = ""
+                while stack and stack[-1] != "(":
+                    temp +=stack.pop()
                 
-        return s
+                stack.pop()
+                stack.extend(list(temp))
+                
+        return "".join(stack)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ 
+        
+        
+        
+#         opens = []
+#         i = 0
+        
+#         while i < len(s):
+#             if s[i] == "(":
+#                 opens.append(i)
+#                 i+=1
+#             elif s[i] == ")":
+#                 start = opens.pop()
+#                 sub = s[start + 1:i]
+#                 sub = sub[::-1]
+#                 s = s[:start] + sub + s[i + 1:]
+#                 i-=1
+#             else:
+#                 i +=1
+                
+#         return s
         
