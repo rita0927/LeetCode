11cbf24c67eb71728ba82b2b0a4163fc6c3c4456
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-01 16:59:13 -0400

    Time: 2279 ms (6.61%), Space: 20 MB (5.10%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,78 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        k = len(nums) - k
+        
+        def quickSelect (l,r):
+            pivot = nums[r]
+            p = l
+            
+            for i in range(l,r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p+=1
+            nums[p], nums[r] = pivot, nums[p]
+            
+            if p >k:
+                return quickSelect(l, p -1)
+            elif p < k:
+                    return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+        return quickSelect(0, len(nums) - 1)
+            
+        
+        
+        
+        
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#       def mergeSort(nums):
+#         if len(nums)<2:
+#           return nums
+#         left = nums[:len(nums)//2]
+#         right = nums[len(nums)//2:]
+
+#         mergeSort(left)
+#         mergeSort(right)
+
+#         left_index = 0
+#         right_index = 0
+#         index = 0
+
+#         while left_index <len(left) and right_index <len(right):
+#           if left[left_index] >right[right_index]:
+#             nums[index] = left[left_index]
+#             left_index+=1
+#           else:
+#             nums[index] = right[right_index]
+#             right_index+=1
+#           index+=1
+
+#         while left_index <len(left):
+#           nums[index] = left[left_index]
+#           left_index+=1
+#           index+=1
+
+#         while right_index <len(right):
+#           nums[index] = right[right_index]
+#           right_index +=1
+#           index+=1
+        
+#       mergeSort(nums)  
+#       return nums[k-1]
+
+        
