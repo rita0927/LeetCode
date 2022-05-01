0049054f09b6cc9c4dee087d120788360ef77654
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-01 14:29:20 -0400

    Time: 179 ms (21.76%), Space: 18.6 MB (90.94%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,11 +1,26 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
+        count = Counter(nums)
         
-        for n in nums:
-            count[n] = count.get(n, 0) + 1
+        # invoked with a comparison key function, count.get(key)
+        # print([count.get(key) for key in count.keys()])
         
-        sortCount = sorted(count.items(), key = lambda x:x[1], reverse=True)
+        return heapq.nlargest(k, count.keys(),count.get)
         
-        return [sortCount[i][0] for i in range(k)]
+        
+        
+                
+#         count = Counter(nums)
+        
+#         sortCount = sorted(count.items(), key = lambda x: x[1], reverse = True)
+
+#         return [sortCount[i][0] for i in range(k)]
+        
+        
+        
+        
+        
+        
+        
+        
         
