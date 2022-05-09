13c908fe66ffadb2b49874984e558d8005ab32a4
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-09 11:37:23 -0400

    Time: 184 ms (35.08%), Space: 14.6 MB (93.28%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -7,78 +7,29 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
-        def helper(root1, root2):
-            if not root1 and not root2:
+   
+        
+        def dfs(root, subRoot):
+            
+            if not root and not subRoot:
                 return True
-            elif not root1 or not root2 or root1.val != root2.val:
+            elif not root or not subRoot or root.val != subRoot.val:
                 return False
             else:
-                return helper(root1.left, root2.left) and helper(root1.right, root2.right)
-            
-            
-            
-            
-        def dfs(node):
-            if node:
-                if node.val == subRoot.val:
-                    if helper(node, subRoot):
-                        return True
-                return True if dfs(node.left) or dfs(node.right) else False 
-            
-        return dfs(root)
-    
-            
+                return dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right)
         
-#         queue = deque([root])
+        queue = deque([root])
         
-#         while queue:
-#             node = queue.popleft()
-            
-#             if node.val == subRoot.val:
-#                 if helper(node, subRoot):
-#                     return True
-            
-#             if node.left:
-#                 queue.append(node.left)
-#             if node.right:
-#                 queue.append(node.right)
-        # return False 
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         def dfs(root, subRoot):
-            
-#             if not root and not subRoot:
-#                 return True
-#             elif not root or not subRoot or root.val != subRoot.val:
-#                 return False
-#             else:
-#                 return dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right)
-        
-#         queue = deque([root])
-        
-#         while queue:
-#             node = queue.popleft()
-#             if node.val == subRoot.val:
-#                 if dfs(node, subRoot):
-#                     return True
-#             if node.left:
-#                 queue.append(node.left)
-#             if node.right:
-#                 queue.append(node.right)
-#         return False 
+        while queue:
+            node = queue.popleft()
+            if node.val == subRoot.val:
+                if dfs(node, subRoot):
+                    return True
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return False 
             
             
         
