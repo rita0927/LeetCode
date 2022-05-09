8d0b4a121ac3b8bd3bf329c7846de7ffdef83103
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-08 22:04:29 -0400

    Time: 210 ms (47.10%), Space: 13.8 MB (98.64%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -2,16 +2,23 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
         
-        def isPalindrom(l,r):
+        def countPalindrom(l,r):
             count = 0
-            while l >=0 and r <len(s) and s[l] == s[r]:
+            
+            while l >= 0 and r < len(s) and s[l]==s[r]:
                 count +=1
-                l -=1
-                r +=1
-            return count 
-        
+                l-=1
+                r+=1
+            return count
         
         for i in range(len(s)):
-            res +=isPalindrom(i,i)
-            res +=isPalindrom(i, i+1)
+            # two senarios for palindrome:
+            
+            # odd number: single letter as the center
+            res += countPalindrom(i, i)
+            
+            # even number: pair of letters as the center
+            res += countPalindrom(i, i+1)
         return res 
+            
+            
