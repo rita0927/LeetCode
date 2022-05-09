db151ab001b091e151e0e55830f2f17ec46508d1
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-09 12:00:54 -0400

    Time: 79 ms (97.24%), Space: 15.1 MB (37.27%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -6,30 +6,47 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def convertToString(node):
+            if node:
+                # use the "#" as a deliminator, example case  [12], [2] 
+                return f"#{node.val} {convertToString(node.left)} {convertToString(node.right)}"
+        
+        return convertToString(subRoot) in convertToString(root)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
    
         
-        def dfs(root, subRoot):
+#         def dfs(root, subRoot):
             
-            if not root and not subRoot:
-                return True
-            elif not root or not subRoot or root.val != subRoot.val:
-                return False
-            else:
-                return dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right)
+#             if not root and not subRoot:
+#                 return True
+#             elif not root or not subRoot or root.val != subRoot.val:
+#                 return False
+#             else:
+#                 return dfs(root.left, subRoot.left) and dfs(root.right, subRoot.right)
         
-        queue = deque([root])
+#         queue = deque([root])
         
-        while queue:
-            node = queue.popleft()
-            if node.val == subRoot.val:
-                if dfs(node, subRoot):
-                    return True
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        return False 
+#         while queue:
+#             node = queue.popleft()
+#             if node.val == subRoot.val:
+#                 if dfs(node, subRoot):
+#                     return True
+#             if node.left:
+#                 queue.append(node.left)
+#             if node.right:
+#                 queue.append(node.right)
+#         return False 
             
             
         
