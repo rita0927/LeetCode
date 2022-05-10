a42a1d70e05adef924e1b5bdba25ddd7e41d78a5
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-09 22:27:40 -0400

    Time: 86 ms (75.66%), Space: 18.8 MB (39.34%) - LeetHub

--- 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.py
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.py
@@ -6,33 +6,100 @@
 #         self.right = None
 
 class Codec:
-
     def serialize(self, root: Optional[TreeNode]) -> str:
-        """Encodes a tree to a single string.
-        """
-        if root:
-            return f"{root.val} {self.serialize(root.left)} {self.serialize(root.right)}" 
-        else:
-            return 'N'
-            
+
+        self.lst = []
+        def dfs(node):
+            if not node: return None 
+            self.lst.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(self.lst)
+                    
 
     def deserialize(self, data: str) -> Optional[TreeNode]:
         """Decodes your encoded data to tree.
         """
-        vals = data.split()
-        self.i = 0
+        if not data: return None 
+        vals = [int(val) for val in data.split(",")]
         
-        def dfs():
-            if vals[self.i] == 'N':
-                self.i +=1
-                return None
-            node = TreeNode(int(vals[self.i]))
-            self.i +=1
-            node.left = dfs()
-            node.right = dfs()
-            return node 
+        def dfs(max_val):
+            if not vals: return None
+            if vals[0] >= max_val: return None
             
-        return dfs()
+            val = vals.pop(0)
+            node = TreeNode(val)
+            node.left = dfs(val)
+            node.right = dfs(max_val)
+            return node
+        return dfs(float('inf'))
+
+#         def dfs(lst, min_val, max_val):
+#             if not lst: return None 
+#             if not min_val < lst[0] < max_val: return None 
+            
+#             val = lst.pop(0)
+#             node = TreeNode(val)
+            
+#             node.left = dfs(lst, min_val, val)
+#             node.right = dfs(lst, val, max_val)
+#             return node
+        
+#         return dfs(vals, float('-inf'), float('inf'))
+        
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+#     def serialize(self, root: Optional[TreeNode]) -> str:
+#         """Encodes a tree to a single string.
+#         """
+#         if root:
+#             return f"{root.val} {self.serialize(root.left)} {self.serialize(root.right)}" 
+#         else:
+#             return 'N'
+            
+
+#     def deserialize(self, data: str) -> Optional[TreeNode]:
+#         """Decodes your encoded data to tree.
+#         """
+#         vals = data.split()
+#         self.i = 0
+        
+#         def dfs():
+#             if vals[self.i] == 'N':
+#                 self.i +=1
+#                 return None
+#             node = TreeNode(int(vals[self.i]))
+#             self.i +=1
+#             node.left = dfs()
+#             node.right = dfs()
+#             return node 
+            
+#         return dfs()
         
         
 
