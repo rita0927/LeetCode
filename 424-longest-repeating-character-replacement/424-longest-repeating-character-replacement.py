245b03d5df62ee0183d1f6820f1434f3d79a2b11
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-10 19:39:42 -0400

    Time: 241 ms (27.96%), Space: 13.9 MB (93.68%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -8,15 +8,13 @@ class Solution:
         count = defaultdict(int)
         res = 0
         l = 0
-        maxFreq = 0
         
         for r in range(len(s)):
             count[s[r]]+=1
-            maxFreq = max(maxFreq, count[s[r]])
             
             # occurance of the most frequent letter + k results in the longest res 
             # when window width - occurance of most frequent letter > k, need to move left pointer
-            while (r-l+1) - maxFreq> k:
+            while (r-l+1) - max(count.values()) > k:
                 # decrease the l pointer occurance before moving the pointer
                 count[s[l]] -=1
                 l+=1
