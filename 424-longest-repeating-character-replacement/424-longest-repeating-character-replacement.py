9a9b3a04a5ae570e871d7f520f0d3f3ef0e48dca
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-10 20:23:54 -0400

    Time: 209 ms (38.65%), Space: 13.9 MB (58.06%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,22 +1,67 @@
 class Solution:
-    
-    # O(26n) results in O(n)
-    # O(1)
-    
     def characterReplacement(self, s: str, k: int) -> int:
         
+        if len(s) < 2: return len(s)
+         
+    # O(26n) results in O(n)
+    # O(1)
         count = defaultdict(int)
         res = 0
         l = 0
+        maxFreq = 0
         
         for r in range(len(s)):
             count[s[r]]+=1
+            maxFreq = max(maxFreq, count[s[r]])
             
             # occurance of the most frequent letter + k results in the longest res 
             # when window width - occurance of most frequent letter > k, need to move left pointer
-            while (r-l+1) - max(count.values()) > k:
+            if (r-l+1) - maxFreq > k:
                 # decrease the l pointer occurance before moving the pointer
                 count[s[l]] -=1
                 l+=1
             res = max(res, r-l+1)
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
+    
+#     def characterReplacement(self, s: str, k: int) -> int:
+         
+#     # O(26n) results in O(n)
+#     # O(1)
+#         count = defaultdict(int)
+#         res = 0
+#         l = 0
+        
+#         for r in range(len(s)):
+#             count[s[r]]+=1
+            
+#             # occurance of the most frequent letter + k results in the longest res 
+#             # when window width - occurance of most frequent letter > k, need to move left pointer
+#             while (r-l+1) - max(count.values()) > k:
+#                 # decrease the l pointer occurance before moving the pointer
+#                 count[s[l]] -=1
+#                 l+=1
+#             res = max(res, r-l+1)
+#         return res 
