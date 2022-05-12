b4d89dbc13bae9b96b6e6f1d8eec8113a1e26b8f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-12 19:51:01 -0400

    Time: 290 ms (95.32%), Space: 16.5 MB (45.86%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,232 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dir = [[-1,0], [1,0],[0,-1], [0,1]]
+        res = 0
+        
+        def dfs(r,c):
+            grid[r][c] = 0
+            
+            for x,y in dir:
+                nr, nc = x+r, y + c
+                
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc] == "1":
+                    dfs(nr, nc)
+        
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == "1":
+                    res +=1
+                    dfs(r,c)
+        return res 
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         count = 0
+#         dir = [(-1,0), (1,0), (0,-1), (0,1)]
+        
+#         for row in range(len(grid)):
+#             for col in range(len(grid[0])):
+#                 if grid[row][col] == "1":
+#                     stack = [(row,col)]
+#                     while stack:
+#                         r, c = stack.pop()
+                        
+#                         if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] == "0":
+#                             continue
+                            
+#                         grid[r][c] = "0"
+#                         for x,y in dir:
+#                             stack.append((x + r, y + c))
+#                     count +=1
+                    
+#         return count 
+                        
+                        
+                        
+                        
+                        
+      
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        # if not grid:
+#             return 0
+        
+#         result = 0
+#         dir = [(-1,0), (1,0), (0,-1), (0,1)]
+#         visited = set()
+        
+#         def dfs (r,c):
+#             if r < 0 or r>=len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0" or (r,c) in visited:
+#                 return 
+#             visited.add((r,c))
+            
+#             for x,y in dir:
+#                 dfs(x+r, y + c)
+               
+#         for row in range(len(grid)):
+#             for col in range(len(grid[0])):
+#                 if grid[row][col] == "1" and (row, col) not in visited:
+#                     dfs(row, col)
+#                     result +=1
+        
+#         return result
+    
+
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+            
+            
+                
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+#         count = 0
+#         for r in range(len(grid)):
+#             for c in range(len(grid[0])):
+#                 if grid[r][c] == "1":
+#                     self.dfs(grid, r,c)
+#                     count+=1
+#         return count
+    
+    
+#     def dfs(self, grid, row, column):
+#         if row <0 or column <0 or row >=len(grid) or column >= len(grid[0]) or grid[row][column] == "0":
+#             return 
+        
+#         grid[row][column] = "0"
+#         self.dfs(grid, row+1, column)
+#         self.dfs(grid, row-1, column)
+#         self.dfs(grid, row, column + 1)
+#         self.dfs(grid, row, column - 1)
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not grid:
+#             return 0
+
+#         count = 0
+#         for x in range(len(grid)):
+#             for y in range(len(grid[0])):
+#                 if grid[x][y] == "1":
+#                     self.dfs(grid, x, y)
+#                     count += 1
+
+#         return count
+
+#     def dfs(self, grid, x, y):
+#         if x<0 or y<0 or x>=len(grid) or y>=len(grid[0]) or grid[x][y] != "1":
+#             return
+
+#         grid[x][y] = "#"
+#         self.dfs(grid, x + 1, y)
+#         self.dfs(grid, x - 1, y)
+#         self.dfs(grid, x, y + 1)
+#         self.dfs(grid, x, y - 1)
+          
