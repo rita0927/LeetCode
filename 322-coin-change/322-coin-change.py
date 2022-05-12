ff0b181a2aa9d4bcb38fff62ea7ddab853c16c0b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-12 13:58:49 -0400

    Time: 2819 ms (14.60%), Space: 43.5 MB (7.32%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,16 +1,61 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
+        cache = {}
         
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a>=c:
-                    # dp[a-c] + 1, dp[a-c] to get the dp from cache, 1 is for the current coin c
-                    # get the min from available combinations (compare all coins combination and the default dp[a])
-                    dp[a] = min(dp[a], dp[a-c]+1)
+        def helper(balance):
+            if balance < 0: return -1
+            if not balance: return 0
             
-        return dp[amount] if dp[amount] != float('inf') else -1
+            if balance in cache:
+                return cache[balance]
+            
+            min_count = float('inf')
+            for c in coins:
+                count = helper(balance - c) 
+                if count == -1: continue
+                min_count = min(min_count, count + 1)
+            cache[balance] = min_count if min_count != float('inf') else -1
+            return cache[balance]
+        
+        return helper(amount)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp = [float('inf')] * (amount + 1)
+#         dp[0] = 0
+        
+#         for a in range(1, amount + 1):
+#             for c in coins:
+#                 if a>=c:
+#                     # dp[a-c] + 1, dp[a-c] to get the dp from cache, 1 is for the current coin c
+#                     # get the min from available combinations (compare all coins combination and the default dp[a])
+#                     dp[a] = min(dp[a], dp[a-c]+1)
+            
+#         return dp[amount] if dp[amount] != float('inf') else -1
             
         
         
