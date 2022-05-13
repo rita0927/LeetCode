44de3071372d3a9af25137f32b328db21c42b33f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-12 21:37:29 -0400

    Time: 100 ms (33.61%), Space: 14 MB (16.64%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,17 +1,20 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         nx = 0
-        cur = x
+        remainder = x
         if x < 0:
             return False
         if x < 10:
             return True
         
-        while cur: 
-            temp = cur%10 
+        while remainder: 
+            temp = remainder%10 
             nx = nx * 10 + temp
-            cur = cur//10
+            remainder = remainder//10
             
-        return nx == x 
+            if nx == x:
+                return True
+            
+        return False 
         
         
