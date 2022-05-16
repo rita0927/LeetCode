b9bf37829ca9787dadf02b2c9277985788d4653a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-16 16:51:20 -0400

    Time: 51 ms (35.05%), Space: 14.2 MB (58.37%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -6,26 +6,77 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
         if not root:
             return []
         
         res = []
+        queue = deque([(root, 0)])
         
-        def dfs(node, level):
-            if level == len(res):
-                res.append([])
-                
-            if level%2:
-                res[level] = [node.val] + res[level]
-            else:
-                res[level].append(node.val)
+        while queue:
             
-            if node.left:
-                dfs(node.left, level + 1)
-            if node.right:
-                dfs(node.right, level + 1)
-        dfs(root, 0)
+            level_size = len(queue)
+            temp = []
+            for _ in range(level_size):
+                node, level = queue.popleft()
+                if level%2:
+                    temp = [node.val] + temp
+                else:
+                    temp.append(node.val)
+                
+                if node.left:
+                    queue.append((node.left, level + 1))
+                if node.right:
+                    queue.append((node.right, level+ 1))
+            
+            res.append(temp)
+            
         return res 
+                
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not root:
+#             return []
+        
+#         res = []
+        
+#         def dfs(node, level):
+#             if level == len(res):
+#                 res.append([])
+                
+#             if level%2:
+#                 res[level] = [node.val] + res[level]
+#             else:
+#                 res[level].append(node.val)
+            
+#             if node.left:
+#                 dfs(node.left, level + 1)
+#             if node.right:
+#                 dfs(node.right, level + 1)
+#         dfs(root, 0)
+#         return res 
                 
                 
         
