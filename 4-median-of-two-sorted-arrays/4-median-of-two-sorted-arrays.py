2aef1b25c2247725fac2b89790920638cfbde6ff
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-16 12:39:51 -0400

    Time: 121 ms (54.65%), Space: 14.1 MB (96.82%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -2,36 +2,108 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         length = len(nums1) + len(nums2)
         mid = length//2
-        odd = length % 2
+        odd = length%2
         
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-            
-        l = 0
+        
+        l = 0 
         r = len(nums1) - 1
         
         while True:
-            mid1 = (l+r)//2
-            
-            # index of b, b_mid + 1 + a_mid + 1 = mid
-            mid2 = mid - mid1 - 2  
+            mid1 = (l + r)//2
+            mid2 = mid - mid1 - 2
             
             left1 = nums1[mid1] if mid1 >=0 else float('-inf')
-            right1 = nums1[mid1 + 1] if mid1 + 1 < len(nums1) else float('inf')
+            right1 = nums1[mid1 + 1] if (mid1 + 1) < len(nums1) else float('inf')
             left2 = nums2[mid2] if mid2 >=0 else float('-inf')
-            right2 = nums2[mid2 + 1] if mid2 + 1 < len(nums2) else float('inf')
+            right2 = nums2[mid2 + 1] if mid2 + 1 <len(nums2) else float('inf')
             
-            if left1<=right2 and left2 <=right1:
+            if left1 <= right2 and left2 <= right1:
                 if odd:
                     return min(right1, right2)
                 return (max(left1, left2) + min(right1, right2))/2
             
-            if left1 >right2:
+            elif left1 > right2:
                 r = mid1 - 1
-                
-            # left2 > right1
-            else:  
+            else:
                 l = mid1 + 1
+                
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         #O(log(min(m,n))), O(1)
+
+#         length = len(nums1) + len(nums2)
+#         mid = length//2
+#         odd = length % 2
+        
+#         if len(nums1) > len(nums2):
+#             nums1, nums2 = nums2, nums1
+            
+#         l = 0
+#         r = len(nums1) - 1
+        
+#         while True:
+#             mid1 = (l+r)//2
+            
+#             # mid index of b, b_mid + 1 + a_mid + 1 = mid
+#             mid2 = mid - mid1 - 2  
+            
+#             left1 = nums1[mid1] if mid1 >=0 else float('-inf')
+#             right1 = nums1[mid1 + 1] if mid1 + 1 < len(nums1) else float('inf')
+#             left2 = nums2[mid2] if mid2 >=0 else float('-inf')
+#             right2 = nums2[mid2 + 1] if mid2 + 1 < len(nums2) else float('inf')
+            
+#             if left1<=right2 and left2 <=right1:
+#                 if odd:
+#                     return min(right1, right2)
+#                 return (max(left1, left2) + min(right1, right2))/2
+            
+#             if left1 >right2:
+#                 r = mid1 - 1
+                
+#             # left2 > right1
+#             else:  
+#                 l = mid1 + 1
                 
                 
             
