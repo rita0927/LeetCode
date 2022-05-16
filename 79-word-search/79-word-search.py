1f64743a0ff70afb14887863e46d07013ff50354
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-15 23:00:42 -0400

    Time: 5420 ms (80.82%), Space: 13.9 MB (93.38%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,99 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        dir = [[-1,0], [1,0], [0,-1], [0,1]]
+        
+        
+        def backtrack(index, r,c):
+            if index == len(word):
+                return True
+            
+            if r < 0 or r >=m or c < 0 or c >=n or board[r][c] != word[index]:
+                return False
+            
+            board[r][c] = '#'
+            
+            for x,y in dir:
+                nr = x + r
+                nc = y + c
+                if backtrack(index + 1, nr, nc):
+                    return True
+            board[r][c] = word[index]
+            return False        
+            
+        for r in range(m):
+            for c in range(n):
+                if backtrack(0, r,c):
+                    return True
+            
+        return False 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def dfs(i, j, index):
+#             if index == len(word):
+#                 return True
+#             if (
+#                 i < 0
+#                 or i >= len(board)
+#                 or j < 0
+#                 or j >= len(board[0])
+#                 or word[index] != board[i][j]
+#             ):
+#                 return False
+#             temp = board[i][j]
+#             board[i][j] = "*"
+
+#             found = (
+#                 dfs(i, j - 1, index + 1)
+#                 or dfs(i + 1, j, index + 1)
+#                 or dfs(i, j + 1, index + 1)
+#                 or dfs(i - 1, j, index + 1)
+#             )
+
+#             board[i][j] = temp
+#             return found
+
+#         for i in range(len(board)):
+#             for j in range(len(board[0])):
+#                 if dfs(i, j, 0):
+#                     return True
+#         return False
