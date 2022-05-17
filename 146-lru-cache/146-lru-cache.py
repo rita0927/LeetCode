63bf2ac78e6efde39b542acdd9190d4afdab6d77
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-17 19:02:03 -0400

    Time: 1026 ms (57.39%), Space: 76.5 MB (21.58%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,24 +1,85 @@
+"""
+there are pseudo head and pseudo tail to mark the boundary, so that we don't need to check the null node during the update
+"""
+
+class DoubleLinkedList():
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
+
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.size = capacity
+        self.cache = {}
+        self.size = 0
+        self.capacity = capacity
+        self.head = DoubleLinkedList()
+        self.tail = DoubleLinkedList()
+        self.head.next = self.tail
+        self.tail.next = self.head
+        
+    # add the new node right after head (pseudo)
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next 
+        
+        self.head.next.prev = node
+        self.head.next = node
+        
+    def _remove_node(self, node):
+        prev= node.prev
+        next = node.next
+        
+        prev.next = next
+        next.prev = prev
+    
+    # move the node right after the pseudo head
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+     
+    # pop the current last node (right before pseudo tail)
+    def _pop_tail(self):
+        res = self.tail.prev
+        self._remove_node(res)
+        return res 
+        
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        node = self.cache.get(key, None)
+        if not node:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        
+        self._move_to_head(node)
+        return node.value
+
         
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.move_to_end(key)
+        node = self.cache.get(key)
         
-        elif len(self.cache) == self.size:
-            # OrderedDict popitem: FIFO(last = False), LIFO(last = True)
-            self.cache.popitem(last = False)
-        self.cache[key] = value 
+        if not node:
+            newNode = DoubleLinkedList()
+            newNode.key = key
+            newNode.value = value
+            
+            self.cache[key] = newNode
+            self.size +=1
+            self._add_node(newNode)
+            
+            if self.size > self.capacity:
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+        else:
+            node.value = value
+            self._move_to_head(node)
+            
+
             
             
         
