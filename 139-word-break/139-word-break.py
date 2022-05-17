7d6d149ac823bfc61ad584d99b9308bc2dc5f2fb
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-17 13:47:52 -0400

    Time: 68 ms (26.79%), Space: 14.1 MB (46.53%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -3,7 +3,6 @@ class Solution:
     
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         d = set(wordDict)
-        
         dp = [False] * (len(s) + 1)
         dp[0] = True
         
@@ -11,8 +10,48 @@ class Solution:
             for j in range(i):
                 if dp[j] and s[j:i] in d:
                     dp[i] = True
-                    # break
         return dp[len(s)]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         d = set(wordDict)
+        
+#         dp = [False] * (len(s) + 1)
+#         dp[0] = True
+        
+#         for i in range(1, len(s) + 1):
+#             for j in range(i):
+#                 if dp[j] and s[j:i] in d:
+#                     dp[i] = True
+#                     # break
+#         return dp[len(s)]
         
         
         
