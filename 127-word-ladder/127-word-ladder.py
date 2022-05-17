ae89035388d03b79d5f85e14c0566b557a352cca
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-16 20:10:16 -0400

    Time: 475 ms (46.07%), Space: 15 MB (93.21%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.py
@@ -0,0 +1,98 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        words = set(wordList)
+        level = 0
+        queue = deque([beginWord])
+        
+        while queue:
+            level_size = len(queue)
+            
+            for _ in range(level_size):
+                word = queue.popleft()
+                
+                if word == endWord:
+                    return level + 1
+                letters = list(word)
+                
+                for i in range(len(letters)):
+                    temp = letters[i]
+                    
+                    for ch in string.ascii_lowercase:
+                        letters[i] = ch
+                        newWord = ''.join(letters)
+                        
+                        if newWord in words:
+                            queue.append(newWord)
+                            words.remove(newWord)
+                    letters[i] = temp
+            level +=1
+        return 0
+        
+                        
+                    
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dictOfWords = {i: 1 for i in wordList}
+#         queue = deque([beginWord])
+#         level = 0
+
+#         while queue:
+#             size = len(queue)
+
+#             for _ in range(size):
+#                 word = queue.popleft()
+#                 if word == endWord:
+#                     return level + 1
+#                 letters = list(word)
+
+#                 for i in range(len(letters)):
+#                     temp = letters[i]
+#                     for ch in ascii_lowercase:
+#                         letters[i] = ch
+#                         newWord = "".join(letters)
+#                         if newWord in dictOfWords:
+#                             queue.append(newWord)
+#                             dictOfWords.pop(newWord)
+#                     letters[i] = temp
+#             level += 1
+#         return 0
+        
