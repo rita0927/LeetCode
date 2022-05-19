aef38fd041d8355268d0789448134da06c689a4a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 01:16:44 -0400

    Time: 215 ms (74.14%), Space: 14.3 MB (77.05%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -3,35 +3,109 @@ class Solution:
         n = len(isConnected)
         parents = [i for i in range(n)]
         rank = [1] * n
-        
-        def find(n1):
-            root = n1
+
+        def find(c):
+            root = c
             while root != parents[root]:
                 parents[root] = parents[parents[root]]
                 root = parents[root]
             return root
         
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
+        def union(c1, c2):
+            r1, r2 = find(c1), find(c2)
             
-            if p1 == p2:
+            if r1 == r2:
                 return 0
             
-            if rank[p2] > rank[p1]:
-                parents[p1] = p2
-                rank[p2] += rank[p1]
+            if rank[r2] > rank[r1]:
+                parents[r1] = r2
+                rank[r2] += rank[r1]
             else:
-                parents[p2] = p1
-                rank[p1] += rank[p2]
+                parents[r2] = r1
+                rank[r1] += rank[r2]
             return 1
         
         res = n
-        for r in range(n):
-            for c in range(r + 1, n):
-                if isConnected[r][c] == 1:
-                    res -= union(r, c)
-                    
+        for i in range(n-1):
+            for j in range(i+1, n):
+                if isConnected[i][j] == 1:
+                    res -= union(i,j)
         return res 
+                
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n^2*α(n)), O(n)
+          
+#         n = len(isConnected)
+#         parents = [i for i in range(n)]
+#         rank = [1] * n
+        
+#         def find(n1):
+#             root = n1
+#             while root != parents[root]:
+#                 parents[root] = parents[parents[root]]
+#                 root = parents[root]
+#             return root
+        
+#         def union(n1, n2):
+#             p1, p2 = find(n1), find(n2)
+            
+#             if p1 == p2:
+#                 return 0
+            
+#             if rank[p2] > rank[p1]:
+#                 parents[p1] = p2
+#                 rank[p2] += rank[p1]
+#             else:
+#                 parents[p2] = p1
+#                 rank[p1] += rank[p2]
+#             return 1
+        
+#         res = n
+#         for r in range(n):
+#             for c in range(r + 1, n):
+#                 if isConnected[r][c] == 1:
+#                     res -= union(r, c)
+                    
+#         return res 
                 
         
         
