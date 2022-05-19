fb6b0c0d5ae37f0f60f95d06b6799d0418ed46e2
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-18 21:26:36 -0400

    Time: 258 ms (71.15%), Space: 20.9 MB (96.28%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,15 +1,77 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
         res = [1] * len(nums)
         
         for i in range(1, len(nums)):
-            res[i]= res[i-1] * nums[i-1]
+            res[i] = res[i-1] * nums[i-1]
         
         right = 1
+        
         for j in range(len(nums) - 1, -1, -1):
-            res[j] = res[j] * right
+            res[j] *=right
             right*=nums[j]
+        
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n), O(n)
+#         left = [1] * len(nums)
+#         right = [1] * len(nums)
+#         res = [1] * len(nums)
+        
+#         for i in range(1, len(nums)):
+#             left[i] = left[i-1] * nums[i-1]
+        
+#         for j in range(len(nums)-2, -1, -1):
+#             right[j] = right[j+1] * nums[j+1]
+            
+#         res = [left[i] * right[i] for i in range(len(nums))]
+#         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = [1] * len(nums)
+        
+#         for i in range(1, len(nums)):
+#             res[i]= res[i-1] * nums[i-1]
+        
+#         right = 1
+#         for j in range(len(nums) - 1, -1, -1):
+#             res[j] *= right
+#             right*=nums[j]
+#         return res 
             
         
         
