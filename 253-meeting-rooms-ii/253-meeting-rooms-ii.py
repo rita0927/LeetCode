6787c81dd79b58b1f62d98f15cc45210531cc52e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 11:40:03 -0400

    Time: 91 ms (68.68%), Space: 17.5 MB (50.48%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,68 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        rooms = 0
+        starts = sorted([interval[0] for interval in intervals])
+        ends = sorted([interval[1] for interval in intervals])
+        
+        e = 0
+        
+        for s in range(len(intervals)):
+            if starts[s] >= ends[e]:
+                rooms -=1
+                e +=1
+            rooms +=1
+        
+        return rooms
+                
+        
+        
+  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(NlogN) due to sort and heap operation, O(N)
+        
+#         # use heap to store the sorted end time
+#         intervals.sort()
+#         rooms = []
+        
+#         heapq.heappush(rooms, intervals[0][1])
+        
+#         for start, end in intervals[1:]:
+            
+#             if start >= rooms[0]:
+#                 heapq.heappop(rooms)
+            
+#             heapq.heappush(rooms, end)
+#         return len(rooms)
+        
+        
+            
+            
+            
+            
+        
