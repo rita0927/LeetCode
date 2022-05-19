49bd02583ee65d62bbf9711627c2920b79a1f9de
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 00:59:05 -0400

    Time: 111 ms (32.53%), Space: 13.9 MB (89.73%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -6,38 +6,94 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
-        def reverse(node):
-            prev = None
-            while node:
-                next = node.next
-                node.next = prev
-                prev = node
-                node = next
+        def reverse(l):
+            prev = None        
+            while l:
+                next = l.next
+                l.next = prev
+                prev = l
+                l = next
             return prev
         
         l1 = reverse(l1)
         l2 = reverse(l2)
-        
         head = None
         temp = 0
         while l1 or l2:
-            val1 = l1.val if l1 else 0 
+
+            val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
             temp = temp + val1 + val2
             
             cur = ListNode(temp%10)
+            temp = temp //10
             cur.next = head
             head = cur
-            temp = temp // 10
             
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            
+        
         if temp:
             cur = ListNode(temp)
             cur.next = head
             head = cur
         return head 
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def reverse(node):
+#             prev = None
+#             while node:
+#                 next = node.next
+#                 node.next = prev
+#                 prev = node
+#                 node = next
+#             return prev
+        
+#         l1 = reverse(l1)
+#         l2 = reverse(l2)
+        
+#         head = None
+#         temp = 0
+#         while l1 or l2:
+#             val1 = l1.val if l1 else 0 
+#             val2 = l2.val if l2 else 0
+#             temp = temp + val1 + val2
+            
+#             cur = ListNode(temp%10)
+#             cur.next = head
+#             head = cur
+#             temp = temp // 10
+            
+#             l1 = l1.next if l1 else None
+#             l2 = l2.next if l2 else None
+            
+#         if temp:
+#             cur = ListNode(temp)
+#             cur.next = head
+#             head = cur
+#         return head 
             
 
             
