d12ae9ae306b476e5257a470c37ac7c45151d235
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 17:51:06 -0400

    Time: 155 ms (27.91%), Space: 16.6 MB (82.43%) - LeetHub

---
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.py
@@ -0,0 +1,28 @@
+class TicTacToe:
+
+    def __init__(self, n: int):
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.diagonal = 0
+        self.antiDiagonal = 0
+        
+
+    def move(self, row: int, col: int, player: int) -> int:
+        n = len(self.rows)
+        cur = 1 if player == 1 else -1
+        self.rows[row] += cur
+        self.cols[col] += cur
+        
+        if row == col:
+            self.diagonal += cur
+        
+        if col == (n-row-1):
+            self.antiDiagonal += cur
+        
+        if abs(self.rows[row]) == n or abs(self.cols[col]) == n or abs(self.diagonal) == n or abs(self.antiDiagonal) == n:
+            return player
+        return 0
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
