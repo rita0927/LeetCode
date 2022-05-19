62545a3e2cc069b68894b3c018056a20186e4b9b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 11:45:21 -0400

    Time: 132 ms (25.11%), Space: 17.5 MB (50.48%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,19 +1,62 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        rooms = []
+        intervals.sort()
         
-        rooms = 0
-        starts = sorted([interval[0] for interval in intervals])
-        ends = sorted([interval[1] for interval in intervals])
+        heapq.heappush(rooms, intervals[0][1])
         
-        e = 0
+        for start, end in intervals[1:]:
+            if start >= rooms[0]:
+                heapq.heappop(rooms)
+            
+            heapq.heappush(rooms, end)
         
-        for s in range(len(intervals)):
-            if starts[s] >= ends[e]:
-                rooms -=1
-                e +=1
-            rooms +=1
+        return len(rooms)
         
-        return rooms
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(NlogN) due to sort and heap operation, O(N)        
+#         rooms = 0
+#         starts = sorted([interval[0] for interval in intervals])
+#         ends = sorted([interval[1] for interval in intervals])
+        
+#         e = 0
+        
+#         for s in range(len(intervals)):
+#             if starts[s] >= ends[e]:
+#                 rooms -=1
+#                 e +=1
+#             rooms +=1
+        
+#         return rooms
                 
         
         
@@ -28,19 +71,7 @@ class Solution:
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+      
         
         
         
