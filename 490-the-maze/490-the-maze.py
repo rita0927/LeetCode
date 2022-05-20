6406267bbbcb85b68848d4eef7dbe5029277f42c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 22:05:35 -0400

    Time: 896 ms (7.45%), Space: 14.7 MB (57.90%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -2,27 +2,30 @@ class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
         m = len(maze)
         n = len(maze[0])
-        visited = set()
         dir = [[-1,0], [1,0], [0,-1], [0,1]]
+        visited = set()
         
-        def dfs(r,c):
+        queue = deque([(start[0], start[1])])
+        
+        while queue:
+            r,c = queue.popleft()
+            visited.add((r,c))
             
             if [r,c] == destination:
                 return True
-            visited.add((r,c))
+            
             for x,y in dir:
                 nr = r
                 nc = c
                 
-                while 0 <= nr + x < m and 0 <= nc + y < n and maze[nr + x][nc + y] == 0:
+                while 0<= nr + x < m and 0<= nc + y < n and maze[nr + x][nc + y] == 0:
                     nr += x
                     nc += y
                 
-                if (nr, nc) not in visited and dfs(nr,nc):
-                    return True
-                  
-        
-        return dfs(start[0], start[1])
+                if (nr, nc) not in visited:
+                    queue.append((nr, nc))
+            
+        return False 
         
         
         
