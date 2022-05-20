b12033396ba39a4567a2990c6a8ed198d548dcee
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-19 20:50:19 -0400

    Time: 588 ms (18.28%), Space: 14.7 MB (57.90%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -5,8 +5,9 @@ class Solution:
         visited = set()
         dir = [[-1,0], [1,0], [0,-1], [0,1]]
         
-        queue =deque([(start[0], start[1])])
-
+        sr, sc = start
+        queue=deque([(sr, sc)])
+        
         while queue:
             r,c = queue.popleft()
             visited.add((r,c))
@@ -18,13 +19,64 @@ class Solution:
                 nr = r + x
                 nc = c + y
                 
-                while 0<=nr<m and 0<=nc<n and maze[nr][nc] == 0:
+                while 0<= nr < m and 0<= nc < n and maze[nr][nc] == 0:
                     nr += x
                     nc += y
-                nr-=x
-                nc-=y
+                # deduct the last addition (invalid cell)
+                nr -= x
+                nc -= y
                 if (nr,nc) not in visited:
-                    queue.append((nr,nc))
-                    
+                    queue.append((nr, nc))
+            
         return False 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         m = len(maze)
+#         n = len(maze[0])
+#         visited = set()
+#         dir = [[-1,0], [1,0], [0,-1], [0,1]]
+        
+#         queue =deque([(start[0], start[1])])
+
+#         while queue:
+#             r,c = queue.popleft()
+#             visited.add((r,c))
+            
+#             if [r,c] == destination:
+#                 return True
+            
+#             for x,y in dir:
+#                 nr = r + x
+#                 nc = c + y
+                
+#                 while 0<=nr<m and 0<=nc<n and maze[nr][nc] == 0:
+#                     nr += x
+#                     nc += y
+#                 nr-=x
+#                 nc-=y
+#                 if (nr,nc) not in visited:
+#                     queue.append((nr,nc))
+                    
+#         return False 
         
