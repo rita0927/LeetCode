b68871a80999de133dc1bba4fec58f8287b3bf7e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-23 09:49:14 -0400

    Time: 1206 ms (5.01%), Space: 18.8 MB (89.22%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,7 +1,6 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
         n = len(arr)
-        
         l = [1] * n
         stack = [(arr[0], 1)]
         
@@ -9,18 +8,68 @@ class Solution:
             while stack and arr[i] <= stack[-1][0]:
                 l[i] += stack.pop()[1]
             stack.append((arr[i], l[i]))
-            
+        
         r = [1] * n
-        stack = [(arr[-1], 1)]
+        stack = [(arr[n-1], 1)]
         
         for i in range(n-2, -1, -1):
-            while stack and arr[i] < stack[-1][0]:
+            while stack and arr[i]< stack[-1][0]:
                 r[i] += stack.pop()[1]
             stack.append((arr[i], r[i]))
-        
-        res = 0
-        
+        res = 0 
         for i in range(n):
-            res += arr[i] * l[i] * r[i]
+            res += arr[i] * l[i]*r[i]
+        return res % (10**9 + 7)
+            
+            
         
-        return res % (10** 9 + 7)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(arr)
+        
+#         l = [1] * n
+#         stack = [(arr[0], 1)]
+        
+#         for i in range(1, n):
+#             while stack and arr[i] <= stack[-1][0]:
+#                 l[i] += stack.pop()[1]
+#             stack.append((arr[i], l[i]))
+            
+#         r = [1] * n
+#         stack = [(arr[-1], 1)]
+        
+#         for i in range(n-2, -1, -1):
+#             while stack and arr[i] < stack[-1][0]:
+#                 r[i] += stack.pop()[1]
+#             stack.append((arr[i], r[i]))
+        
+#         res = 0
+        
+#         for i in range(n):
+#             res += arr[i] * l[i] * r[i]
+        
+#         return res % (10** 9 + 7)
