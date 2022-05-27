e7e600611273bbbad6d8e7d2f93a89ac00ce71ce
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-27 17:53:35 -0400

    Time: 40 ms (57.22%), Space: 13.9 MB (11.60%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,14 +1,61 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         
-        if len(palindrome) <= 1:
+        if len(palindrome) <=1:
             return ''
         
         for i in range(len(palindrome)//2):
-            if palindrome[i] != 'a':
+            if palindrome[i] !='a':
                 return palindrome[:i] + 'a' + palindrome[i+1:]
-        
+        # all ch are 'a'
         return palindrome[:-1] + 'b'
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(palindrome) <= 1:
+#             return ''
+        
+#         for i in range(len(palindrome)//2):
+#             if palindrome[i] != 'a':
+#                 return palindrome[:i] + 'a' + palindrome[i+1:]
+        
+#         return palindrome[:-1] + 'b'
         
         
         
