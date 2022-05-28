915335e92dd7c08d67bfa9a300ddd64e14ac4d0f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-27 22:45:53 -0400

    Time: 188 ms (62.53%), Space: 16.6 MB (43.04%) - LeetHub

--- 716-max-stack/716-max-stack.py
+++ 716-max-stack/716-max-stack.py
@@ -2,13 +2,16 @@ class MaxStack:
 
     def __init__(self):
         self.stack = []
+        self.max = [float('-inf')]
         
         
     def push(self, x: int) -> None:
         self.stack.append(x)
+        self.max.append(max(self.max[-1], x))
         
 
     def pop(self) -> int:
+        self.max.pop()
         return self.stack.pop()
         
         
@@ -17,18 +20,55 @@ class MaxStack:
         
 
     def peekMax(self) -> int:
-        return max(self.stack)
+        return self.max[-1]
         
 
     def popMax(self) -> int:
-        max_val = max(self.stack)
-        
+        max_val = self.max[-1]
+        temp = []
         for i in range(len(self.stack)-1, -1, -1):
-            if self.stack[i] == max_val:
-                del self.stack[i]
-                return max_val
+            cur = self.pop()
+            if cur == max_val:
+                break
+            else:
+                temp.append(cur)
+  
+        for n in reversed(temp):
+            self.push(n)
+
+        return max_val
         
 
+        
+        
+        
+#     def __init__(self):
+#         self.stack = []
+        
+        
+#     def push(self, x: int) -> None:
+#         self.stack.append(x)
+        
+
+#     def pop(self) -> int:
+#         return self.stack.pop()
+        
+        
+#     def top(self) -> int:
+#         return self.stack[-1]
+        
+
+#     def peekMax(self) -> int:
+#         return max(self.stack)
+        
+
+#     def popMax(self) -> int:
+#         max_val = max(self.stack)
+        
+#         for i in range(len(self.stack)-1, -1, -1):
+#             if self.stack[i] == max_val:
+#                 del self.stack[i]
+#                 return max_val
 
 # Your MaxStack object will be instantiated and called as such:
 # obj = MaxStack()
