ce54eec6748d4d08e76936a9b67f20a0c8b801f6
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-27 22:10:03 -0400

    Time: 155 ms (86.96%), Space: 16.5 MB (75.03%) - LeetHub

---
+++ 716-max-stack/716-max-stack.py
@@ -0,0 +1,39 @@
+class MaxStack:
+
+    def __init__(self):
+        self.stack = []
+        
+        
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        return self.stack.pop()
+        
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def peekMax(self) -> int:
+        return max(self.stack)
+        
+
+    def popMax(self) -> int:
+        max_val = max(self.stack)
+        
+        for i in range(len(self.stack)-1, -1, -1):
+            if self.stack[i] == max_val:
+                del self.stack[i]
+                return max_val
+        
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
