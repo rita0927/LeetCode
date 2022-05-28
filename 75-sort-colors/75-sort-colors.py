0a0a53aa44db437a2dd3c07d753984120286aa43
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 18:46:37 -0400

    Time: 40 ms (70.39%), Space: 14 MB (15.03%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,105 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        p0 = -1
+        p2 = len(nums)
+        cur = 0
+        
+        while cur < p2:
+            if nums[cur] == 1:
+                cur += 1
+            elif nums[cur] == 2:
+                p2 -=1
+                nums[p2], nums[cur] = nums[cur], nums[p2]
+            else:
+                p0 += 1
+                nums[p0], nums[cur] = nums[cur], nums[p0]
+                cur += 1
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         p0 = 0
+#         p2 = len(nums) - 1
+#         cur = 0
+        
+#         while cur < p2:
+            
+#             if nums[cur] == 0:
+#                 nums[p0], nums[cur] = nums[cur], nums[p0]
+#                 p0+=1
+#                 cur +=1
+#             elif nums[cur] == 2:
+#                 nums[p2], nums[cur] = nums[cur], nums[p2]
+#                 p2-=1
+       
+#             else:
+#                 cur +=1
+                
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         zero = -1
+#         two = len(nums)
+
+#         i = 0
+
+#         while i < two:
+#             if nums[i] == 1:
+#                 i += 1
+
+#             elif nums[i] == 2:
+#                 two -= 1
+#                 nums[i], nums[two] = nums[two], nums[i]
+
+#             else:
+#                 zero += 1
+#                 nums[i], nums[zero] = nums[zero], nums[i]
+#                 i += 1
+        
