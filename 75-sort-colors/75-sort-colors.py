259ed09bda7220d81c927fdef0f3fa757f7cfb6b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 19:16:52 -0400

    Time: 52 ms (35.89%), Space: 14 MB (15.03%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,21 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0 = -1
-        p2 = len(nums)
+        p0 = 0 
+        p2 = len(nums) - 1
         cur = 0
         
-        while cur < p2:
-            if nums[cur] == 1:
-                cur += 1
-            elif nums[cur] == 2:
-                p2 -=1
-                nums[p2], nums[cur] = nums[cur], nums[p2]
-            else:
-                p0 += 1
+        while cur <= p2:
+            # index <= cur are sorted, no need to verify nums[cur] after the replacement
+            if nums[cur] == 0:
                 nums[p0], nums[cur] = nums[cur], nums[p0]
+                p0+=1
+                cur+=1
+            # replaced nums[cur] is not verified, can't increase cur
+            elif nums[cur] == 2:
+                nums[p2], nums[cur] = nums[cur], nums[p2]
+                p2 -=1
+            else:
                 cur += 1
-                
+            
         
         
         
@@ -33,47 +35,33 @@ class Solution:
         
         
         
-#         p0 = 0
-#         p2 = len(nums) - 1
+        
+        
+        
+        
+        
+        
+        
+#         p0 = -1
+#         p2 = len(nums)
 #         cur = 0
         
 #         while cur < p2:
-            
-#             if nums[cur] == 0:
-#                 nums[p0], nums[cur] = nums[cur], nums[p0]
-#                 p0+=1
-#                 cur +=1
+#             if nums[cur] == 1:
+#                 cur += 1
 #             elif nums[cur] == 2:
+#                 p2 -=1
 #                 nums[p2], nums[cur] = nums[cur], nums[p2]
-#                 p2-=1
-       
 #             else:
-#                 cur +=1
+#                 p0 += 1
+#                 nums[p0], nums[cur] = nums[cur], nums[p0]
+#                 cur += 1
                 
-                
         
         
+    
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+ 
         
         
         
