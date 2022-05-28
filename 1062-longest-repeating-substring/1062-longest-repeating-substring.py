1a47188ebf589918287745ce24f5b87d78feb57e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-27 20:43:41 -0400

    Time: 2898 ms (8.82%), Space: 235.5 MB (8.83%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,51 +1,74 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
+        seen = set()
+        res = 0
         
-        # O(NlogN), O(N^2) (if L is the len of substring, average is O(N*L))
+        for l in range(len(s)):
+            for r in range(l + 1, len(s) + 1):
+                if s[l:r] in seen:
+                    res = max(res, r-l)
+                seen.add(s[l:r])
+        return res 
         
-        n = len(s) 
-        """
-        Search a substring of given length
-        that occurs at least 2 times.
-        @return start position if the substring exits and -1 otherwise.
-        """
-        # find repeating substring 
-        def helper(length):
-            seen = set()
-            for i in range(n-length):
-                sub = s[i:i+length+1]
-                h = hash(sub)
-                if h in seen:
-                    return True
-                seen.add(h)
-            return False 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# https://www.youtube.com/watch?v=Tf_mD59DLf0        
+        
+#         # with hash, O(NlogN), O(N) 
+#         # without hash, O(NlogN), O(N^2) (if L is the len of substring, average is O(N*L))
+        
+#         n = len(s) 
+#         """
+#         Search a substring of given length
+#         that occurs at least 2 times.
+#         @return start position if the substring exits and -1 otherwise.
+#         """
+#         # find repeating substring 
+#         def helper(length):
+#             seen = set()
+#             for i in range(n-length):
+#                 sub = s[i:i+length+1]
+#                 h = hash(sub)
+#                 if h in seen:
+#                     return True
+#                 seen.add(h)
+#             return False 
         
 
-        l,r = 0, n -1
-        while l <=r:
-            # avoid overflow
-            mid = l + (r-l)//2
-            if helper(mid):
-                l = mid + 1
-            else:
-                r = mid - 1
-        return l 
+#         l,r = 0, n -1
+#         while l <=r:
+#             # avoid overflow
+#             mid = l + (r-l)//2
+#             if helper(mid):
+#                 l = mid + 1
+#             else:
+#                 r = mid - 1
+#         return l 
     
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+      
         
         
         
