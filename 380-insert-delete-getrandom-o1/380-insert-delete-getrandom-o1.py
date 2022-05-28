895effe99554288ed0f70607b980d5c5e4fdbede
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 00:44:26 -0400

    Time: 417 ms (89.71%), Space: 61.7 MB (6.97%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,32 +1,35 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.dict = {}
-        self.list = []
-        
+        self.dic = {}
+        self.lst = []
+
 
     def insert(self, val: int) -> bool:
-        if val in self.dict:
+        if val in self.dic:
             return False
-        self.dict[val] = len(self.list)
-        self.list.append(val)
+        self.dic[val] = len(self.lst)
+        self.lst.append(val)
         return True
-        
+
 
     def remove(self, val: int) -> bool:
-        if val not in self.dict:
+        if val not in self.dic:
             return False
-        last = self.list[-1]
-        index = self.dict[val]
-        self.list[index] = last
-        self.list.pop()
-        self.dict[last] = index
-        del self.dict[val]
-        return True
         
+        index = self.dic[val]
+        last = self.lst[-1]
+        self.lst[index] = last
+        self.dic[last] = index
+        self.lst.pop()
+        del self.dic[val]
+        return True
+            
+
 
     def getRandom(self) -> int:
-        return choice(self.list)
+        return random.choice(self.lst)
+
         
 
 
@@ -35,3 +38,47 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class RandomizedSet:
+
+#     def __init__(self):
+#         self.dict = {}
+#         self.list = []
+        
+
+#     def insert(self, val: int) -> bool:
+#         if val in self.dict:
+#             return False
+#         self.dict[val] = len(self.list)
+#         self.list.append(val)
+#         return True
+        
+
+#     def remove(self, val: int) -> bool:
+#         if val not in self.dict:
+#             return False
+#         last = self.list[-1]
+#         index = self.dict[val]
+#         self.list[index] = last
+#         self.list.pop()
+#         self.dict[last] = index
+#         del self.dict[val]
+#         return True
+        
+
+#     def getRandom(self) -> int:
+#         return choice(self.list)
