9aed55843bc280f929b246a3de277efe3172c4a3
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 19:36:47 -0400

    Time: 421 ms (49.67%), Space: 21.8 MB (23.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,26 +1,72 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         m = len(grid)
         n = len(grid[0])
         dir = [[-1,0], [1,0], [0,-1], [0,1]]
         
-        def dfs(r,c):
-            grid[r][c] = 0
-            
-            for x,y in dir:
-                nr = x + r
-                nc = y + c
-                
-                if 0<= nr < m and 0 <= nc < n and grid[nr][nc] == '1':
-                    dfs(nr, nc)
+        queue = deque()
         res = 0
+        visited = set()
+        
         for r in range(m):
             for c in range(n):
-                if grid[r][c] == '1':
-                    dfs(r,c)
-                    res += 1
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    res +=1
+                    visited.add((r,c))
+                    queue.append((r,c))
+                    
+                    while queue:
+                        row, col = queue.popleft()
+   
+                        for x,y in dir:
+                            nr = x + row
+                            nc = y + col
+
+                            if 0<= nr < m and 0 <= nc < n and grid[nr][nc] == '1' and (nr,nc) not in visited:
+                                queue.append((nr,nc))
+                                visited.add((nr,nc))
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         m = len(grid)
+#         n = len(grid[0])
+#         dir = [[-1,0], [1,0], [0,-1], [0,1]]
+        
+#         def dfs(r,c):
+#             grid[r][c] = '0'
+            
+#             for x,y in dir:
+#                 nr = x + r
+#                 nc = y + c
+                
+#                 if 0<= nr < m and 0 <= nc < n and grid[nr][nc] == '1':
+#                     dfs(nr, nc)
+#         res = 0
+#         for r in range(m):
+#             for c in range(n):
+#                 if grid[r][c] == '1':
+#                     dfs(r,c)
+#                     res += 1
+#         return res 
 
         
         
