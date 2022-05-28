dbea70804bbfbc56a4d59cb8799d4919bc7b5ea5
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-27 20:28:12 -0400

    Time: 34 ms (99.10%), Space: 14.3 MB (66.13%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,30 +1,31 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-#         n = len(s)
-#         """
-#         Search a substring of given length
-#         that occurs at least 2 times.
-#         @return start position if the substring exits and -1 otherwise.
-#         """
-        
-#         def helper(length):
-#             seen = set()
-#             for i in range(n-length+1):
-#                 sub = s[i:i+length]
-#                 if sub in seen:
-#                     return i
-#                 seen.add(sub)
-#             return -1
+        n = len(s) 
+        """
+        Search a substring of given length
+        that occurs at least 2 times.
+        @return start position if the substring exits and -1 otherwise.
+        """
+        # find repeating substring 
+        def helper(length):
+            seen = set()
+            for i in range(n-length):
+                sub = s[i:i+length+1]
+                if sub in seen:
+                    return True
+                seen.add(sub)
+            return False 
         
 
-#         l,r = 1, n
-#         while l <=r:
-#             mid = l + (r-l)//2
-#             if helper(mid) != -1:
-#                 l = mid + 1
-#             else:
-#                 r = mid - 1
-#         return l - 1
+        l,r = 0, n -1
+        while l <=r:
+            # avoid overflow
+            mid = l + (r-l)//2
+            if helper(mid):
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l 
     
         
         
@@ -45,21 +46,21 @@ class Solution:
         
         
         
+#   # O(n^3), O(n^3) since the slice is O(n) space complexity       
+#         if len(s) < 2:
+#             return 0
         
-        if len(s) < 2:
-            return 0
+#         seen = set()
+#         res = 0
         
-        seen = set()
-        res = 0
+#         # for r in range(1,len(s)+1):
+#         #     for l in range(r):
+#         for l in range(len(s)):
+#             # r is exclusive
+#             for r in range(l+1, len(s)+1):
+#                 if s[l:r] in seen:
+#                     res = max(res, r-l)
+#                 else:
+#                     seen.add(s[l:r])
         
-        # for r in range(1,len(s)+1):
-        #     for l in range(r):
-        for l in range(len(s)):
-            for r in range(l+1, len(s)+1):
-                if s[l:r] in seen:
-                    res = max(res, r-l)
-                else:
-                    seen.add(s[l:r])
-             
-        
-        return res 
+#         return res 
