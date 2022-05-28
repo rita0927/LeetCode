8755f0fd053fb1dc55a2658255e45b7f9f91b8a7
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 14:06:59 -0400

    Time: 50 ms (44.37%), Space: 14.2 MB (15.20%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -1,6 +1,56 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return ' '.join(reversed(s.split()))
+        l = 0
+        r = len(s) - 1
+        # remove the leading space
+        while l < len(s) and s[l] == ' ':
+                l+=1
+        # remove the tailing space
+        while r >= 0 and s[r] == ' ':
+            r -=1
+            
+        queue = deque()
+        temp = ''
+        
+        while l <=r:
+            if s[l] == ' ' and temp:
+                queue.appendleft(temp)
+                temp = ''
+            elif s[l] != ' ':
+                temp += s[l]
+            l += 1
+        
+        if temp:
+            queue.appendleft(temp)
+        return ' '.join(queue)
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # return ' '.join(reversed(s.split()))
         
         
         
