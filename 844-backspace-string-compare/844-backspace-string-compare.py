6b45dd73f85a8f9a8944eec077b1e2105110649a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 15:31:55 -0400

    Time: 50 ms (35.04%), Space: 14 MB (23.25%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,17 +1,63 @@
+import itertools
+
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        
+
+        # O(N), O(1)
         def helper(string):
-            stack = []
-            for i in string:
-                if stack and i == '#':
-                    stack.pop()
-                elif i != '#':
-                    stack.append(i)
-                    
-            return ''.join(stack)
+            skip = 0
+            
+            for ch in reversed(string):
+                if ch == '#':
+                    skip += 1
+                elif skip:
+                    skip -=1
+                else:
+                    yield ch
         
-        return helper(s) == helper(t)
+        # zip_longest zip up two strings/lists that match the length of longest one
+        # the missing element is filled with None
+        for ch1, ch2 in itertools.zip_longest(helper(s), helper(t)):
+            if ch1 != ch2:
+                return False
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def helper(string):
+#             stack = []
+#             for i in string:
+#                 if stack and i == '#':
+#                     stack.pop()
+#                 elif i != '#':
+#                     stack.append(i)
+                    
+#             return ''.join(stack)
+        
+#         return helper(s) == helper(t)
         
         
         
