d74049780824c1e56b4b82dcff8685e0b682d8d7
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 13:03:26 -0400

    Time: 122 ms (7.65%), Space: 14.1 MB (32.13%) - LeetHub

---
+++ 443-string-compression/443-string-compression.py
@@ -0,0 +1,60 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        index = 0
+        l = 0
+        
+        while l < len(chars):
+            r = l
+            while r < len(chars) and chars[r] == chars[l]:
+                r+=1
+
+            chars[index] = chars[l]
+            index +=1
+            
+            if r-l >1:
+                count = str(r-l)
+                for n in count:                    
+                    chars[index] = n
+                    index +=1
+
+            l = r
+              
+        return index
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(chars) == 1: return 1
+        
+#         l =0 
+        
+#         for r in range(len(chars)):
+#             if r == len(chars) - 1 and chars[r] == chars[l]:
+#                 l+=1
+#                 chars[l] = r-l
+#                 break
+
+            
+#             if chars[l] != chars[r] and r-l > 1:
+#                 l+=1
+#                 chars[l] = r-l+1
+#                 l+=1
+#                 chars[l] = chars[r]
+            
+#         for _ in range(len(chars) -1 - l):
+#             chars.pop()
+                                                         
+#         return l+1
+                                                         
+        
+        
+        
+                    
+                    
+        
