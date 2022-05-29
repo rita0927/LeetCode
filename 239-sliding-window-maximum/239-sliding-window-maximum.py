37aa4119a324c4ecb38dd48b00730c41ef098e7a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-28 20:10:12 -0400

    Time: 1905 ms (75.63%), Space: 30.3 MB (49.10%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,22 +1,159 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
         res = []
-        l,r = 0, 0 
+        l = 0 
         queue = deque()
         
-        while r < len(nums):
+        for r in range(len(nums)):
             
-            while queue and nums[r] > queue[-1]:
+            while queue and nums[queue[-1]] < nums[r]:
                 queue.pop()
-            queue.append(nums[r])
+            queue.append(r)
             
-            if r + 1>=k:
-                res.append(queue[0])
-                if nums[l] == queue[0]:
+            if r + 1 >=k:
+                res.append(nums[queue[0]])
+                
+                if l == queue[0]:
                     queue.popleft()
                 l+=1
-            r +=1
+                
         return res 
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Brute Force: O(nk), O(n-k+1) for output 
+#         return [max(nums[i:i+k]) for i in range(len(nums) - k + 1)]
+        
+        
+   
+        
+        
+        
+        
+# # O(n), O(n) where O(n-k+1) for output and O(k) for stack
+#         res = []
+#         stack = []
+#         l = 0
+        
+#         for r in range(len(nums)):
+            
+#             while stack and nums[stack[-1]] < nums[r]:
+#                 stack.pop()
+#             stack.append(r)
+            
+#             if (r-l+1) >= k:
+#                 res.append(nums[stack[0]])
+                
+#                 if stack[0] == l:
+#                     stack.pop(0)
+#                 l+=1
+#         return res 
+                
+                
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+#         l,r = 0, 0 
+#         queue = deque()
+        
+#         while r < len(nums):
+            
+#             while queue and nums[r] > queue[-1]:
+#                 queue.pop()
+#             queue.append(nums[r])
+            
+#             # start append to the res once reaches window width
+#             if r + 1>=k:
+#                 res.append(queue[0])
+                
+#                 # adjust the range of queue beofre moving the left pointer 
+#                 if nums[l] == queue[0]:
+#                     queue.popleft()
+#                 l+=1
+#             r +=1
+#         return res 
                 
         
         
