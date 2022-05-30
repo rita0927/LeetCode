428e7a1147452d39db9610967b08312203da2747
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-30 15:32:50 -0400

    Time: 121 ms (5.05%), Space: 18 MB (38.64%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.py
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.py
@@ -7,19 +7,49 @@
 class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
         
-
-# O(N), O(N) due to the call stack
-        def helper(node):
-            if not node:
-                return None
-            elif node.val < low:
-                return helper(node.right)
-            elif node.val > high:
-                return helper(node.left)
-            else:
-                node.left = helper(node.left)
-                node.right = helper(node.right)
-                return node 
+        if not root:
+            return None
+        elif root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        else:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
         
-        return helper(root)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+# # O(N), O(N) due to the call stack
+#         def helper(node):
+#             if not node:
+#                 return None
+#             elif node.val < low:
+#                 return helper(node.right)
+#             elif node.val > high:
+#                 return helper(node.left)
+#             else:
+#                 node.left = helper(node.left)
+#                 node.right = helper(node.right)
+#                 return node 
+        
+#         return helper(root)
         
