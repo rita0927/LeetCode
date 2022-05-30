c9c63a1bc6474b3f34632293d9d349276e9ccdd0
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-30 16:02:41 -0400

    Time: 64 ms (34.95%), Space: 14.3 MB (18.40%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,108 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+        
+        
+        while l<= r:
+            mid = l + (r-l)//2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] >= nums[l]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            elif nums[mid] <= nums[r]:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+        
+        
+        
+        
+        
+        
+        
+        
+#         while l < r:
+            
+#             mid = l + (r-l)//2
+            
+#             if nums[l] <= target< nums[mid]:
+#                 r = mid - 1
+#             elif nums[mid]< target <=nums[r]:
+#                 l = mid + 1
+#             elif target > nums[mid]:
+#                 l = mid
+#             elif target < nums[mid]:
+#                 r = mid 
+#             else:
+#                 return mid
+#         return -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not nums:
+#             return -1
+
+#         l, r = 0, len(nums) - 1
+#         while l <= r:
+#             mid = (l + r) // 2
+
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[l] == target:
+#                 return l
+#             elif nums[r] == target:
+#                 return r
+
+#             if nums[mid] > nums[l]:
+#                 if nums[l] < target < nums[mid]:
+#                     r = mid - 1
+#                 else:
+#                     l = mid + 1
+#             else:
+#                 if nums[mid] < target < nums[r]:
+#                     l = mid + 1
+#                 else:
+#                     r = mid - 1
+#         return -1
