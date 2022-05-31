ab7e27e31c8a20a438882b6e922581a61e1b51cd
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-31 00:30:44 -0400

    Time: 451 ms (33.50%), Space: 17.3 MB (62.72%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.py
@@ -0,0 +1,68 @@
+
+        
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 2069
+        self.buckets = [[] for _ in range(self.size)]
+        
+    def getIndex(self, key):
+        hashResult = key % self.size
+        bucket = self.buckets[hashResult]
+        
+        for i, (k,v) in enumerate(bucket):
+            if k == key:
+                return bucket, i
+        return bucket, -1
+        
+        
+    def put(self, key: int, value: int) -> None:
+        bucket, index = self.getIndex(key)
+        
+        if index < 0:
+            bucket.append((key, value))
+        else:
+            bucket[index] = (key, value)
+        
+
+    def get(self, key: int) -> int:
+        bucket, index = self.getIndex(key)
+        if index < 0:
+            return -1
+        else:
+            return bucket[index][1]
+        
+
+    def remove(self, key: int) -> None:
+        bucket, index = self.getIndex(key)
+        if index < 0:
+            return 
+        else:
+            bucket.pop(index)
+        
+        
+        
+        
+        
+
+
+# class MyHashMap:
+
+#     def __init__(self):
+        
+
+#     def put(self, key: int, value: int) -> None:
+        
+
+#     def get(self, key: int) -> int:
+        
+
+#     def remove(self, key: int) -> None:
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
