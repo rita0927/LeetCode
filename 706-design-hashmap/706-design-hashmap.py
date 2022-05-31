a7612b69402e9e211da38993393d6ed618e0eb18
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-31 00:47:40 -0400

    Time: 654 ms (20.27%), Space: 40.2 MB (12.97%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,44 +1,70 @@
+# Without hashing 
 
-        
 class MyHashMap:
 
     def __init__(self):
-        self.size = 2069
-        self.buckets = [[] for _ in range(self.size)]
+        self.data = [-1] * 1000001
         
-    def getIndex(self, key):
-        hashResult = key % self.size
-        bucket = self.buckets[hashResult]
-        
-        for i, (k,v) in enumerate(bucket):
-            if k == key:
-                return bucket, i
-        return bucket, -1
-        
-        
+
     def put(self, key: int, value: int) -> None:
-        bucket, index = self.getIndex(key)
-        
-        if index < 0:
-            bucket.append((key, value))
-        else:
-            bucket[index] = (key, value)
-        
+        self.data[key] = value
 
     def get(self, key: int) -> int:
-        bucket, index = self.getIndex(key)
-        if index < 0:
-            return -1
-        else:
-            return bucket[index][1]
-        
+        return self.data[key]
 
     def remove(self, key: int) -> None:
-        bucket, index = self.getIndex(key)
-        if index < 0:
-            return 
-        else:
-            bucket.pop(index)
+        self.data[key] = -1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+# class MyHashMap:
+
+#     def __init__(self):
+#         self.size = 2069
+#         self.buckets = [[] for _ in range(self.size)]
+        
+#     def getIndex(self, key):
+        
+#         # random hash function
+#         hashResult = key % self.size
+#         bucket = self.buckets[hashResult]
+        
+#         for i, (k,v) in enumerate(bucket):
+#             if k == key:
+#                 return bucket, i
+#         return bucket, -1
+        
+        
+#     def put(self, key: int, value: int) -> None:
+#         bucket, index = self.getIndex(key)
+        
+#         if index < 0:
+#             bucket.append((key, value))
+#         else:
+#             bucket[index] = (key, value)
+        
+
+#     def get(self, key: int) -> int:
+#         bucket, index = self.getIndex(key)
+#         if index < 0:
+#             return -1
+#         else:
+#             return bucket[index][1]
+        
+
+#     def remove(self, key: int) -> None:
+#         bucket, index = self.getIndex(key)
+#         if index < 0:
+#             return 
+#         else:
+#             bucket.pop(index)
         
         
         
