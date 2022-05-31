bf8d43cb5d778c087f6e93446f0ea2e06debc59c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-31 01:07:14 -0400

    Time: 349 ms (54.50%), Space: 17.3 MB (62.72%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.py
+++ 706-design-hashmap/706-design-hashmap.py
@@ -1,19 +1,76 @@
-# Without hashing 
-
 class MyHashMap:
 
     def __init__(self):
-        self.data = [-1] * 1000001
-        
+        self.size = 2069
+        self.data = [[] for _ in range(self.size)]
+    
+    def hash_function(self, key):
+        return key % self.size 
 
     def put(self, key: int, value: int) -> None:
-        self.data[key] = value
+        
+        hashed_key = self.hash_function(key)
+        bucket = self.data[hashed_key]
+        
+        for i in range(len(bucket)):
+            k, v = bucket[i]
+            if k == key:
+                bucket[i][1] = value
+                return
+   
+        bucket.append([key, value])
+        
 
     def get(self, key: int) -> int:
-        return self.data[key]
+        hashed_key = self.hash_function(key)
+        bucket = self.data[hashed_key]
+        
+        for i in range(len(bucket)):
+            k,v = bucket[i]
+            if k == key:
+                return v
+        return -1
+        
 
     def remove(self, key: int) -> None:
-        self.data[key] = -1
+        hashed_key = self.hash_function(key)
+        bucket = self.data[hashed_key]
+        
+        for i in range(len(bucket)):
+            k, v = bucket[i]
+            
+            if k == key:
+                bucket.pop(i)
+                break 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Without hashing 
+
+# class MyHashMap:
+
+#     def __init__(self):
+#         self.data = [-1] * 1000001
+        
+
+#     def put(self, key: int, value: int) -> None:
+#         self.data[key] = value
+
+#     def get(self, key: int) -> int:
+#         return self.data[key]
+
+#     def remove(self, key: int) -> None:
+#         self.data[key] = -1
     
     
     
