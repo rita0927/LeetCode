ec448f1ca1ee94e6039822c6cc1ae3ac65fbf99c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-05-30 20:34:37 -0400

    Time: 52 ms (71.61%), Space: 16.3 MB (95.29%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,14 +7,54 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def helper(node, low, high):
+        stack = [(root, float('-inf'), float('inf'))]
+        
+        while stack:
+            node, low, high = stack.pop()
+            
             if not node:
-                return True
+                continue
             
             if node.val <= low or node.val >= high:
                 return False
             
-            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+            stack.append((node.left, low, node.val))
+            stack.append((node.right, node.val, high))
+            
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(N), O(N)
+        
+#         def helper(node, low, high):
+#             if not node:
+#                 return True
+            
+#             if node.val <= low or node.val >= high:
+#                 return False
+            
+#             return helper(node.left, low, node.val) and helper(node.right, node.val, high)
             
         
-        return helper(root, float('-inf'), float('inf'))
+#         return helper(root, float('-inf'), float('inf'))
