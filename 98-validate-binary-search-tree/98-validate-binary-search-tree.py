9f4d8a1581b4adc58d9e11329c6c746b0c5ef6d9
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-03 02:27:52 -0400

    Time: 74 ms (28.27%), Space: 16.3 MB (91.08%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,17 +7,71 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def dfs(node, low, high):
-            
-            if not node:
-                return True
+        stack = [(root, float('-inf'), float('inf'))]
+        
+        while stack:
+            node, low, high = stack.pop()
             
             if node.val <= low or node.val >= high:
                 return False
             
-            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+            if node.left:
+                stack.append((node.left, low, node.val))
+            
+            if node.right:
+                stack.append((node.right, node.val, high))
+                
+        return True 
+            
         
-        return dfs(root, float('-inf'), float('inf'))
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def dfs(node, low, high):
+            
+#             if not node:
+#                 return True
+            
+#             if node.val <= low or node.val >= high:
+#                 return False
+            
+#             return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+        
+#         return dfs(root, float('-inf'), float('inf'))
         
         
         
