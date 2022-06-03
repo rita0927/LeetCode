659404346f91ceefd03f9f577b205210bf9a8f10
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-03 18:13:27 -0400

    Time: 11812 ms (67.21%), Space: 55.7 MB (92.76%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -3,13 +3,16 @@ class WordDictionary:
     def __init__(self):
         self.dict = {}
         
+        
 
     def addWord(self, word: str) -> None:
+        
         trie = self.dict
         
         for ch in word:
             if ch not in trie:
                 trie[ch] = {}
+                
             trie = trie[ch]
         
         trie['isWord'] = True
@@ -17,28 +20,70 @@ class WordDictionary:
 
     def search(self, word: str) -> bool:
         
-        trie = self.dict
-        
-        def dfs(node, index):
-            
+        def dfs(trie, index):
             if index == len(word):
-                return True if 'isWord' in node else False
+                return True if 'isWord' in trie else False
             
             ch = word[index]
             
-            if ch in node:
-                return dfs(node[ch], index + 1)
+            if ch in trie:
+                return dfs(trie[ch], index + 1)
             elif ch == '.':
-                for key in node:
-                    if key != 'isWord' and dfs(node[key], index + 1):
+                for key in trie:
+                    if key != 'isWord' and dfs(trie[key], index + 1):
                         return True
             else:
-                return False 
-
+                return False
             
-        return dfs(trie, 0)
+        return dfs(self.dict, 0)
+        
+
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+
+
+
+
+
+# class WordDictionary:
+
+#     def __init__(self):
+#         self.dict = {}
+        
+
+#     def addWord(self, word: str) -> None:
+#         trie = self.dict
+        
+#         for ch in word:
+#             if ch not in trie:
+#                 trie[ch] = {}
+#             trie = trie[ch]
+        
+#         trie['isWord'] = True
+        
+
+#     def search(self, word: str) -> bool:
+        
+#         trie = self.dict
+        
+#         def dfs(node, index):
+            
+#             if index == len(word):
+#                 return True if 'isWord' in node else False
+            
+#             ch = word[index]
+            
+#             if ch in node:
+#                 return dfs(node[ch], index + 1)
+#             elif ch == '.':
+#                 for key in node:
+#                     if key != 'isWord' and dfs(node[key], index + 1):
+#                         return True
+#             else:
+#                 return False 
+
+            
+#         return dfs(trie, 0)
