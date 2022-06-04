25230197f2106791859971fc98c1cb79ef4d02d9
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-03 23:25:49 -0400

    Time: 1252 ms (17.47%), Space: 27.4 MB (99.20%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,20 +1,21 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        
         if max(nums) < 0:
             return max(nums)
         
+        local_max = 0
+        global_max = 0
         
-        res = float('-inf')
-        temp = 0 
+        for n in nums:
+            local_max = max(local_max + n, 0)
+            global_max = max(global_max, local_max)
         
-        for i in range(len(nums)):
-            if temp + nums[i] <0:
-                temp = 0 
-            else:
-                temp += nums[i]
-            res = max([res, temp, nums[i]])
+        return global_max 
         
-        return res 
+        
+        
+        
         
         
 
@@ -42,6 +43,14 @@ class Solution:
         
         
         
+#         global_max= local_max= nums[0]
+        
+#         for n in nums[1:]:
+#             # whether to abandon the previous local_max
+#             local_max = max(n, local_max + n)
+#             global_max = max(global_max, local_max)
+            
+#         return global_max 
         
         
         
@@ -55,6 +64,77 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if max(nums) < 0:
+#             return max(nums)
+        
+#         local_max = 0
+#         global_max = 0
+        
+#         for n in nums:
+#             local_max = max(0, local_max + n)
+#             global_max = max(global_max, local_max)
+        
+#         return global_max 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if max(nums) < 0:
+#             return max(nums)
+        
+        
+#         res = float('-inf')
+#         temp = 0 
+        
+#         for i in range(len(nums)):
+#             if temp + nums[i] <0:
+#                 temp = 0 
+#             else:
+#                 temp += nums[i]
+#             res = max([res, temp, nums[i]])
+        
+#         return res 
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+      
         
 
 #         if max(nums) < 0:
