bfbcd6702d46338224dc96c7a77797cb03e810fe
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-05 01:04:38 -0400

    Time: 256 ms (85.33%), Space: 25.5 MB (18.67%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -3,22 +3,25 @@ class UndergroundSystem:
     def __init__(self):
         self.check_in_data = {}
         self.journey_data = defaultdict(lambda: [0,0])
+
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.check_in_data[id] = [stationName, t]
+
         
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         start_station, start_time = self.check_in_data[id]
-        time = t - start_time 
-        self.journey_data[(start_station, stationName)][0] += time
-        self.journey_data[(start_station, stationName)][1] += 1
-        
+        time = t - start_time
+        average, count = self.journey_data[(start_station, stationName)]
+        self.journey_data[(start_station, stationName)] = [(average * count + time)/(count + 1), count + 1]
+
+
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        time, count = self.journey_data[(startStation, endStation)]
-        return time/count 
+        return self.journey_data[(startStation, endStation)][0]
+
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
@@ -27,6 +30,10 @@ class UndergroundSystem:
 # obj.checkOut(id,stationName,t)
 # param_3 = obj.getAverageTime(startStation,endStation)
 
+
+
+# O(1)
+# O(P + S^2): P is the number of passengers, S is the number os stations. O(P) for the check_in hashmap, one entry for each passenger. O(S^2) for journey hashmap. There're S station to start, each station can go to S stations (including itself, O(S^2 - S) = O(S^2) ). 
 
 
 
