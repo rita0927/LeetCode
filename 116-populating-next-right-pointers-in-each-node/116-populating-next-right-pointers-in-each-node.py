cbba10319bca9cc62333c1e89d80fc9255c9f0b0
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-05 18:11:01 -0400

    Time: 85 ms (51.43%), Space: 15.8 MB (20.51%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -11,25 +11,54 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         
-        # O(N), O(N)
-        
         if not root:
-            return None 
+            return None
         
         queue = deque([root])
         
         while queue:
             size = len(queue)
-            
             for i in range(size):
+            
                 node = queue.popleft()
                 if i < size - 1:
                     node.next = queue[0]
                 if node.left and node.right:
                     queue.append(node.left)
                     queue.append(node.right)
-                
         return root 
+                
+            
+            
+ 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(N), O(N)
+        
+#         if not root:
+#             return None 
+        
+#         queue = deque([root])
+        
+#         while queue:
+#             size = len(queue)
+            
+#             for i in range(size):
+#                 node = queue.popleft()
+#                 if i < size - 1:
+#                     node.next = queue[0]
+#                 if node.left and node.right:
+#                     queue.append(node.left)
+#                     queue.append(node.right)
+                
+#         return root 
                 
                 
         
