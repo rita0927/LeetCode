a3903b0c08a200c800a12393b6a443432482bc66
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-06 14:57:14 -0400

    Time: 91 ms (5.08%), Space: 14.1 MB (99.47%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        l = 0
+        l = 0 
         r = len(nums) - 1
         
         while l <= r:
+            
             mid = l + (r-l)//2
             
             if nums[mid] == target:
@@ -20,13 +21,12 @@ class Solution:
                     l = mid + 1
                 else:
                     r = mid - 1
+      
         return -1
         
+        
+        
 
-        
-        
-        
-        
         
         
         
