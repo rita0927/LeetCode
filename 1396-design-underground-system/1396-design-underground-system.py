4e0fec1c32d4a538fdae2fb3f7c7fdc615f6260b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-06 22:22:49 -0400

    Time: 317 ms (51.27%), Space: 25.4 MB (18.65%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,22 +1,18 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.check_in_data = {}
+        self.checkin_data = {}
         self.journey_data = defaultdict(lambda: [0,0])
 
-        
-
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_in_data[id] = [stationName, t]
-
-        
+        self.checkin_data[id] = [stationName, t]
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start_station, start_time = self.check_in_data[id]
+        start_station, start_time = self.checkin_data[id]
         time = t - start_time
-        average, count = self.journey_data[(start_station, stationName)]
-        self.journey_data[(start_station, stationName)] = [(average * count + time)/(count + 1), count + 1]
-
+        avg_time, count = self.journey_data[(start_station, stationName)]
+        avg_time = (avg_time * count + time)/(count + 1)
+        self.journey_data[(start_station, stationName)] = [avg_time, count + 1]
 
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
@@ -29,6 +25,35 @@ class UndergroundSystem:
 # obj.checkIn(id,stationName,t)
 # obj.checkOut(id,stationName,t)
 # param_3 = obj.getAverageTime(startStation,endStation)
+
+
+
+# # store the average time in journey data instead of sum to avoid overflow
+
+#     def __init__(self):
+#         self.check_in_data = {}
+#         self.journey_data = defaultdict(lambda: [0,0])
+
+        
+
+#     def checkIn(self, id: int, stationName: str, t: int) -> None:
+#         self.check_in_data[id] = [stationName, t]
+
+        
+
+#     def checkOut(self, id: int, stationName: str, t: int) -> None:
+#         start_station, start_time = self.check_in_data[id]
+#         time = t - start_time
+#         average, count = self.journey_data[(start_station, stationName)]
+#         self.journey_data[(start_station, stationName)] = [(average * count + time)/(count + 1), count + 1]
+
+
+
+#     def getAverageTime(self, startStation: str, endStation: str) -> float:
+#         return self.journey_data[(startStation, endStation)][0]
+
+
+
 
 
 
