fc067efe8f7f3747975c4bcc5a52b863fa5a4038
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-07 19:03:31 -0400

    Time: 38 ms (85.76%), Space: 14.5 MB (91.78%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,201 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not head:
+            return None
+        
+        prev = dummy = Node()
+        stack = [head]
+        
+        while stack:
+            node = stack.pop()
+            prev.next = node
+            node.prev = prev
+            
+            if node.next:
+                stack.append(node.next)
+            if node.child:
+                stack.append(node.child)
+                node.child = None 
+            prev = node 
+        # detatch the dummy node from the head 
+        dummy.next.prev = None
+        return dummy.next 
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not head:
+#             return None
+        
+#         cur = dummy = Node()
+#         stack = [head]
+        
+#         while stack:
+#             node = stack.pop()
+            
+#             if not node:
+#                 return None
+#             if node.next: stack.append(node.next)
+#             if node.child: stack.append(node.child)
+#             cur.next = node
+#             node.prev = cur
+#             node.child = None
+#             cur = node 
+#         dummy.next.prev = None 
+#         return dummy.next 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not head:
+#             return None 
+        
+#         cur = dummy = Node()
+#         stack = [head]
+        
+#         while stack:
+#             node = stack.pop()
+            
+#             if node.next:
+#                 stack.append(node.next)
+#             if node.child:
+#                 stack.append(node.child)
+#             cur.next = node
+#             node.prev = cur
+#             node.child = None
+#             cur = node
+#         dummy.next.prev = None
+#         return dummy.next 
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+#         def dfs(node):
+#             if not node:
+#                 return None
+
+#             next = node.next 
+            
+#             if node.child:
+#                 node.next = dfs(node.child)
+#                 node.child = None 
+#             next.prev = node
+#             node.next = dfs(next)
+            
+#             return node 
+        
+        return dfs(head)
