343fe7c3b5cfb49ba97ab3efe017fd6bb694980b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-08 02:11:12 -0400

    Time: 133 ms (29.84%), Space: 14.6 MB (17.36%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,15 +1,51 @@
-# class Leaderboard:
+from heapq import *
 
-#     def __init__(self):
+class Leaderboard:
+
+    def __init__(self):
+        self.board = defaultdict(int)
         
 
-#     def addScore(self, playerId: int, score: int) -> None:
+    def addScore(self, playerId: int, score: int) -> None:
+        self.board[playerId] += score
         
 
-#     def top(self, K: int) -> int:
+    def top(self, K: int) -> int:
+        scores = list(self.board.values())
+        K = len(scores) - K
+        
+        def quickSelect(l,r):
+            pivot_index = random.randint(l,r)
+            pivot = scores[pivot_index]
+            scores[pivot_index], scores[r] = scores[r], scores[pivot_index]
+            p = l 
+            
+            for i in range(l,r):
+                if scores[i] < pivot:
+                    scores[p], scores[i] = scores[i], scores[p]
+                    p += 1
+            scores[p], scores[r] = scores[r], scores[p]
+            
+            if K< p:
+                return quickSelect(l, p - 1)
+            elif K > p:
+                return quickSelect(p + 1, r)
+            else:
+                return 
+           
+        quickSelect(0, len(scores) - 1)
+        return sum(scores[K:])
+        
+        
+        
+        
         
 
-#     def reset(self, playerId: int) -> None:
+        
+        
+
+    def reset(self, playerId: int) -> None:
+        del self.board[playerId]
         
 
 
@@ -107,22 +143,22 @@
     
 
 
-class Leaderboard:
+# class Leaderboard:
     
-    # space: O(N)
-    def __init__(self):
-        self.board = defaultdict(int)
+#     # space: O(N)
+#     def __init__(self):
+#         self.board = defaultdict(int)
         
-    # time: O(1)
-    def addScore(self, playerId: int, score: int) -> None:
-        self.board[playerId] += score
+#     # time: O(1)
+#     def addScore(self, playerId: int, score: int) -> None:
+#         self.board[playerId] += score
         
-    # time: O(NlogN)
-    def top(self, K: int) -> int:
-        values = sorted(self.board.values(), reverse = True)
-        return sum(values[:K])
+#     # time: O(NlogN)
+#     def top(self, K: int) -> int:
+#         values = sorted(self.board.values(), reverse = True)
+#         return sum(values[:K])
         
-    # time: O(1)
-    def reset(self, playerId: int) -> None:
-        del self.board[playerId]
+#     # time: O(1)
+#     def reset(self, playerId: int) -> None:
+#         del self.board[playerId]
         
