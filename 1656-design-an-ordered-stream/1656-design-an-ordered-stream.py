bdb8a6bc34b18e3b9facfe5c1e29a51553d2b452
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-09 12:45:22 -0400

    Time: 390 ms (16.12%), Space: 14.8 MB (47.31%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,22 +1,87 @@
 class OrderedStream:
-
-    def __init__(self, n: int):
-        self.stream = {}
-        self.ptr = 1
+    def __init__(self, n):
+        self.stream = {} 
+        self.ptr  = 1
         
-
-    def insert(self, idKey: int, value: str) -> List[str]:
+    def insert(self, idKey, value):
         
-        self.stream[idKey] = value
+        self.stream[idKey] = value 
+        
         res = []
-        
         while self.ptr in self.stream:
             res.append(self.stream[self.ptr])
             self.ptr+= 1
-        return res 
         
+        return res 
+
+    
+
+
+
+
+
+
+
+
+
+
+      
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 # Your OrderedStream object will be instantiated and called as such:
 # obj = OrderedStream(n)
 # param_1 = obj.insert(idKey,value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class OrderedStream:
+
+#     def __init__(self, n: int):
+#         self.stream = {}
+#         self.ptr = 1
+        
+
+#     def insert(self, idKey: int, value: str) -> List[str]:
+        
+#         self.stream[idKey] = value
+#         res = []
+        
+#         while self.ptr in self.stream:
+#             res.append(self.stream[self.ptr])
+#             self.ptr+= 1
+#         return res 
