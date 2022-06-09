7f4d1238c7d625bc3b6eda259d1fede20d8cdf8a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-09 02:36:56 -0400

    Time: 364 ms (40.13%), Space: 16.8 MB (9.84%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,37 +1,34 @@
-class DoubleLinkedList:
-    
-    def __init__(self, val):
-        self.val = val
-        self.prev = None
-        self.next = None 
-
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.cur = DoubleLinkedList(homepage)
-        
+        self.history = [homepage]
+        self.cur = 0
+
+    
     def visit(self, url: str) -> None:
-        node = DoubleLinkedList(url)
-        node.prev = self.cur
-        self.cur.next= node
-        self.cur = self.cur.next 
+        while self.cur < len(self.history) - 1:
+            self.history.pop()
         
+        self.history.append(url)
+        self.cur += 1
 
     def back(self, steps: int) -> str:
-        while steps and self.cur.prev:
-            self.cur = self.cur.prev
+        while steps > 0 and self.cur >0:
             steps -= 1
-        return self.cur.val 
-        
+            self.cur -= 1
+        return self.history[self.cur]
+            
+
 
     def forward(self, steps: int) -> str:
-        while steps and self.cur.next:
-            self.cur = self.cur.next
+        while steps > 0 and self.cur < len(self.history) - 1:
             steps -= 1
-        return self.cur.val 
-       
-    
-    
+            self.cur += 1
+        return self.history[self.cur]
+
+        
+        
+        
     
 
 
@@ -40,6 +37,37 @@ class BrowserHistory:
 # obj.visit(url)
 # param_2 = obj.back(steps)
 # param_3 = obj.forward(steps)
+
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.cur = 0
+        
+        
+    def visit(self, url: str) -> None:
+        while self.cur < len(self.history) - 1:
+            self.history.pop()
+        self.history.append(url)
+        self.cur += 1
+
+    def back(self, steps: int) -> str:
+        while steps > 0 and self.cur > 0:
+            steps -= 1
+            self.cur -= 1
+        
+        return self.history[self.cur]
+            
+
+
+    def forward(self, steps: int) -> str:
+        while steps > 0 and self.cur < len(self.history) - 1:
+            steps -= 1
+            self.cur += 1
+        return self.history[self.cur]
+    
 
 
 
