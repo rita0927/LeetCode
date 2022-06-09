c32a880a30533c7099178adf01929b678b966b4a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-09 00:08:50 -0400

    Time: 230 ms (58.60%), Space: 14 MB (86.07%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -5,34 +5,90 @@ class Solution:
         toCrush = False 
         
         for r in range(m):
-            for c in range(n-2):
-                if abs(board[r][c]) == abs(board[r][c + 1]) == abs(board[r][c + 2]) != 0:
-                    board[r][c] = board[r][c + 1] = board[r][c + 2] = -abs(board[r][c])
-                    toCrush = True
-        
+            for c in range(n - 2):
+                if abs(board[r][c]) == abs(board[r][c+1]) == abs(board[r][c + 2]) != 0:
+                    board[r][c] = board[r][c+1] = board[r][c + 2] = -abs(board[r][c])
+                    toCrush = True 
+                    
         for c in range(n):
             for r in range(m-2):
-                if abs(board[r][c]) == abs(board[r+1][c]) == abs(board[r+2][c]) != 0:
+                if abs(board[r][c]) == abs(board[r+1][c])  == abs(board[r+2][c]) != 0:
                     board[r][c] = board[r+1][c] = board[r+2][c] = -abs(board[r][c])
-                    toCrush = True 
-        
+                    toCrush = True
         
         
         for c in range(n):
-            # starting index for the positive num
-            # index > p is replaced with new num
-            # index <= p is updated to 0
             p = m - 1
             
             for r in range(m-1, -1, -1):
                 if board[r][c] > 0:
                     board[p][c] = board[r][c]
-                    p -= 1
+                    p -=1
             
             for r in range(p, -1, -1):
                 board[r][c] = 0 
-                
+        
+        
         return self.candyCrush(board) if toCrush else board 
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O((m*n) ^ 2), O(1) edit the board in place 
+#         m = len(board)
+#         n = len(board[0])
+#         toCrush = False 
+        
+#         for r in range(m):
+#             for c in range(n-2):
+
+#                # use abs to cover previous marked cell, e.g. -5,-5,-5,5,5
+#                 if abs(board[r][c]) == abs(board[r][c + 1]) == abs(board[r][c + 2]) != 0:
+#                     board[r][c] = board[r][c + 1] = board[r][c + 2] = -abs(board[r][c])
+#                     toCrush = True
+        
+#         for c in range(n):
+#             for r in range(m-2):
+#                 if abs(board[r][c]) == abs(board[r+1][c]) == abs(board[r+2][c]) != 0:
+#                     board[r][c] = board[r+1][c] = board[r+2][c] = -abs(board[r][c])
+#                     toCrush = True 
+        
+        
+        
+#         for c in range(n):
+            
+#             # starting index for the positive num in each col 
+#             # index > p is replaced with new num
+#             # index <= p is updated to 0
+#             p = m - 1
+            
+#             for r in range(m-1, -1, -1):
+#                 if board[r][c] > 0:
+#                     board[p][c] = board[r][c]
+#                     p -= 1
+            
+#             for r in range(p, -1, -1):
+#                 board[r][c] = 0 
+                
+#         return self.candyCrush(board) if toCrush else board 
                 
                 
         
