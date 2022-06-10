7e4277cc8b0058444ef232bcbfb4e35aa1893c35
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-09 22:07:06 -0400

    Time: 87 ms (21.60%), Space: 15.3 MB (91.96%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -12,24 +12,113 @@ class Solution:
     def connect(self, root: 'Node') -> 'Node':
         
         if not root:
-            return None
+            return None 
         
         queue = deque([root])
         
         while queue:
-            
             size = len(queue)
             
             for i in range(size):
-                node = queue.popleft()
+                node = queue.popleft()   
                 
                 if i < size - 1:
-                    node.next = queue[0]
+                    node.next = queue[0] 
+                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
         return root 
+                
+        
+
+
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+  # O(N), O(1)      
+        # # head and tail refers to the same dummy node, head record the path of the tail, used to retrieve the initial tail node
+    
+        # # can't set up the head/tail inside the while loop since they need to retain the value when node moves to the next in the same level (another while loop but the same level)
+        
+        # head = tail = Node()
+        # node = root
+        # while node:
+        #     tail.next = node.left
+        #     if tail.next:
+        #         tail = tail.next 
+        #     tail.next = node.right
+        #     if tail.next:
+        #         tail = tail.next
+        #     node = node.next
+        #     if not node:
+        #   # move the tail pointer back to the beginning of the level
+        #         tail = head
+        #   # set the node to the next leftmost of the next level 
+        #         node = head.next 
+        # return root 
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# # O(N), O(N)
+#         if not root:
+#             return None
+        
+#         queue = deque([root])
+        
+#         while queue:
+            
+#             size = len(queue)
+            
+#             for i in range(size):
+#                 node = queue.popleft()
+                
+#                 if i < size - 1:
+#                     node.next = queue[0]
+#                 if node.left:
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#         return root 
                     
                 
         
