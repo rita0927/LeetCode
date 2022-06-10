4938fa5c5d2c9f04ec0734fb02a46c2412311077
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-10 00:12:56 -0400

    Time: 323 ms (26.93%), Space: 14 MB (85.38%) - LeetHub

--- 723-candy-crush/723-candy-crush.py
+++ 723-candy-crush/723-candy-crush.py
@@ -1,20 +1,21 @@
 class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        
         m = len(board)
         n = len(board[0])
         toCrush = False 
         
         for r in range(m):
-            for c in range(n - 2):
-                if abs(board[r][c]) == abs(board[r][c+1]) == abs(board[r][c + 2]) != 0:
-                    board[r][c] = board[r][c+1] = board[r][c + 2] = -abs(board[r][c])
+            for c in range(n-2):
+                if abs(board[r][c]) == abs(board[r][c+1]) == abs(board[r][c+2]) != 0:
+                    board[r][c] = board[r][c+1] = board[r][c+2] = -abs(board[r][c])
                     toCrush = True 
-                    
+        
         for c in range(n):
             for r in range(m-2):
-                if abs(board[r][c]) == abs(board[r+1][c])  == abs(board[r+2][c]) != 0:
+                if abs(board[r][c]) == abs(board[r+1][c]) == abs(board[r+2][c]) != 0: 
                     board[r][c] = board[r+1][c] = board[r+2][c] = -abs(board[r][c])
-                    toCrush = True
+                    toCrush = True 
         
         
         for c in range(n):
@@ -23,14 +24,36 @@ class Solution:
             for r in range(m-1, -1, -1):
                 if board[r][c] > 0:
                     board[p][c] = board[r][c]
-                    p -=1
-            
+                    p-= 1
             for r in range(p, -1, -1):
-                board[r][c] = 0 
-        
+                board[r][c] = 0
         
         return self.candyCrush(board) if toCrush else board 
-                    
+                
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         
