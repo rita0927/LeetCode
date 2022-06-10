6cd825c0e43d0d889cf3360ecdb66bdfbb5d1c0f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-10 18:44:16 -0400

    Time: 552 ms (36.20%), Space: 14.8 MB (99.20%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -1,36 +1,91 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         
-         
-        # O(M^2 * N) where M is the length of each word and N is the number of words in the wordList. For each word (max length M) in the wordList, we iterate over its length to find all the intermediate words, N words and each has the length of M, tht total numnber of iteration is M*N. Forming the intermediate word takes O(M) for the string operation. The total is O(M^2 * N)
-        
-        
         words = set(wordList)
         
         if endWord not in words:
             return 0
-             
-        level = 0
-        queue = deque([beginWord])
         
+        queue = deque([beginWord])
+        level = 0
         while queue:
             size = len(queue)
             level += 1
-                
+            
             for _ in range(size):
                 word = queue.popleft()
+                
                 if word == endWord:
-                        return level 
-                    
+                    return level
+                
                 for i in range(len(word)):
-
                     for ch in string.ascii_lowercase:
                         new_word = word[:i] + ch + word[i+1:]
                         if new_word in words:
                             words.remove(new_word)
                             queue.append(new_word)
-        
         return 0
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         
+#         # O(M^2 * N) where M is the length of each word and N is the number of words in the wordList. For each word (max length M) in the wordList, we iterate over its length to find all the intermediate words, N words and each has the length of M, tht total numnber of iteration is M*N. Forming the intermediate word takes O(M) for the string operation. The total is O(M^2 * N)
+        
+#         # O(M*N)
+        
+#         words = set(wordList)
+        
+#         if endWord not in words:
+#             return 0
+             
+#         level = 0
+#         queue = deque([beginWord])
+        
+#         while queue:
+#             size = len(queue)
+#             level += 1
+                
+#             for _ in range(size):
+#                 word = queue.popleft()
+#                 if word == endWord:
+#                         return level 
+                    
+#                 for i in range(len(word)):
+
+#                     for ch in string.ascii_lowercase:
+#                         new_word = word[:i] + ch + word[i+1:]
+#                         if new_word in words:
+#                             words.remove(new_word)
+#                             queue.append(new_word)
+        
+#         return 0
         
 
                 
