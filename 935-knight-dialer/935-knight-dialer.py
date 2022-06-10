e3ef54dc8226159cb91b902ca79f37b88213b750
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-09 20:24:29 -0400

    Time: 978 ms (77.90%), Space: 13.9 MB (95.67%) - LeetHub

---
+++ 935-knight-dialer/935-knight-dialer.py
@@ -0,0 +1,100 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        mod = 10 ** 9  + 7
+        
+        neighbors = {
+            0: (4,6),
+            1: (6,8),
+            2: (7,9),
+            3: (4,8),
+            4: (0,3,9),
+            5: (),
+            6: (0,1,7),
+            7: (2,6),
+            8: (1,3),
+            9: (2,4) 
+        }
+        
+        cur = [1] * 10
+        
+        for _ in range(n-1):
+            next = [0] * 10
+            
+            for start in range(10):
+                for end in neighbors[start]:
+                    next[end] = (next[end] + cur[start]) % mod
+            cur = next
+        return sum(cur) % mod 
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n), O(1)
+        
+#         mod = 10 ** 9 + 7
+#         # map the starting key and the list of destination keys 
+#         neighbors = {
+#             0: (4,6),
+#             1: (6,8),
+#             2: (7,9),
+#             3: (4,8),
+#             4: (0,3,9),
+#             5: (),
+#             6: (0,1,7),
+#             7: (2,6),
+#             8: (1,3),
+#             9: (2,4),          
+#         }
+        
+#         # place the knight on any numeric cell initially 
+#         cur = [1] * 10
+        
+#         # the first dile is to place the knight on the cell, then perform n-1 jumps
+#         for _ in range(n - 1):
+#             next = [0] * 10
+            
+#             for start in range(10):
+#                 for end in neighbors[start]:
+#                     next[end] = (next[end] + cur[start])%mod
+#             cur = next 
+#         return sum(cur) %mod
+            
+        
+    
+        
+        
+        
+        
+        
+        
+        
