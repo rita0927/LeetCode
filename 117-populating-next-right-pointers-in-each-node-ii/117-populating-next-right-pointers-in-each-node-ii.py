aff5c5d9e7a0f34ece8cd8c4b19c535de5f19cf5
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-11 14:09:17 -0400

    Time: 103 ms (8.13%), Space: 15.2 MB (99.49%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.py
@@ -12,26 +12,31 @@ class Solution:
     def connect(self, root: 'Node') -> 'Node':
         
         if not root:
-            return None 
+            return None
         
         queue = deque([root])
-        
         while queue:
             size = len(queue)
             
             for i in range(size):
-                node = queue.popleft()   
+                node = queue.popleft()
                 
                 if i < size - 1:
-                    node.next = queue[0] 
+                    node.next = queue[0]
                 
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
         return root 
-                
         
+
+                
+  
+            
+        
+        
+
 
 
         
