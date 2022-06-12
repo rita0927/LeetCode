d1d6d5f5adf6456ce0a141a33f7965c97093086a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 01:55:11 -0400

    Time: 152 ms (51.73%), Space: 15.8 MB (63.95%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -16,3 +16,49 @@ class Solution:
             s[j] = ''
         
         return ''.join(s)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n), O(n)
+#         stack = []
+#         s = list(s)
+        
+#         for i in range(len(s)):
+#             if s[i] == '(':
+#                 stack.append(i)
+#             elif s[i] == ')':
+#                 if stack:
+#                     stack.pop()
+#                 else:
+#                     s[i] = ''
+#         for j in stack:
+#             s[j] = ''
+        
+#         return ''.join(s)
