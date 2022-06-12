31a0c4fa54e590db52abe924ffe45801682ee0ee
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 18:37:07 -0400

    Time: 42 ms (96.07%), Space: 14.5 MB (65.74%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,81 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        return Counter(s) == Counter(t)
+
+        
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         countS = Counter(s)
+#         countT = Counter(t)
+        
+#         return countS == countT
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+#       dic1 = {}
+#       dic2 = {}
+
+#       for i in s:
+#         dic1[i] = dic1.get(i, 0) + 1
+#       for i in t:
+#         dic2[i] = dic2.get(i, 0) +1
+
+#       return dic1 == dic2
+
+        # return sorted(s) == sorted(t)
+
+        
