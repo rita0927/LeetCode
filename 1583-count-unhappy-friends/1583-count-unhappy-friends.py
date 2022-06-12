de19d2c88fe201c1be447d31de3a2b31bdf47ae9
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 01:00:58 -0400

    Time: 440 ms (81.00%), Space: 32.8 MB (17.65%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.py
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -1,5 +1,72 @@
 class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
+        
+        pairMap = {}
+        preferMap = defaultdict(dict)
+        
+        for x,y in pairs:
+            pairMap[x] = y
+            pairMap[y] = x
+            
+        for x in range(n):
+            for index, y in enumerate(preferences[x]):
+                preferMap[x][y] = index
+        
+        res = 0
+        for x in range(n):
+            pair = pairMap[x]
+            
+            for y in preferences[x]:
+                y_pair = pairMap[y]
+                if preferMap[x][y] < preferMap[x][pair] and preferMap[y][x] < preferMap[y][y_pair]:
+                    res += 1
+                    break
+        return res 
+                
+                
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
   
 #         pairMap = defaultdict(int)
 #         preferMap = defaultdict(dict)
@@ -29,22 +96,7 @@ class Solution:
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+   
         
         
         
@@ -54,35 +106,35 @@ class Solution:
         
 #         # O(n^2), O(n^2)
         
-        pairMap = defaultdict(int)
-        preferMap = defaultdict(dict)
+#         pairMap = defaultdict(int)
+#         preferMap = defaultdict(dict)
         
-        # pair map 
-        for x,y in pairs:
-            pairMap[x] = y
-            pairMap[y] = x
+#         # pair map 
+#         for x,y in pairs:
+#             pairMap[x] = y
+#             pairMap[y] = x
         
-        # preference map, e.g. {0: {1:0, 2:1, 3:2}, 1: {3:0, 2;1, 0:2}}
-        for i in range(n):
-            for j in range(n-1):
-                prefer = preferences[i][j]
-                preferMap[i][prefer] = j
+#         # preference map, e.g. {0: {1:0, 2:1, 3:2}, 1: {3:0, 2;1, 0:2}}
+#         for i in range(n):
+#             for j in range(n-1):
+#                 prefer = preferences[i][j]
+#                 preferMap[i][prefer] = j
                 
-        res = 0
+#         res = 0
         
-        for i in range(n):
-            for j in range(n-1):
+#         for i in range(n):
+#             for j in range(n-1):
                 
-                # find pair, preferred friend, preferred friend's pair 
-                pair = pairMap[i]
-                prefer = preferences[i][j]
-                preferPair = pairMap[prefer]
+#                 # find pair, preferred friend, preferred friend's pair 
+#                 pair = pairMap[i]
+#                 prefer = preferences[i][j]
+#                 preferPair = pairMap[prefer]
                 
-                # prefer each other than their current pair 
-                if preferMap[i][prefer] < preferMap[i][pair] and preferMap[prefer][i] < preferMap[prefer][preferPair]:
-                    res += 1
-                    break
-        return res 
+#                 # prefer each other than their current pair 
+#                 if preferMap[i][prefer] < preferMap[i][pair] and preferMap[prefer][i] < preferMap[prefer][preferPair]:
+#                     res += 1
+#                     break
+#         return res 
         
                 
         
