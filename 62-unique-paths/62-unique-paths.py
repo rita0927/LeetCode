4c6caac234b61a00aca6363a22b0594691ffb966
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 14:43:36 -0400

    Time: 38 ms (71.53%), Space: 13.8 MB (72.88%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,65 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        
+        dp = [[1] * n] * m  
+        for r in range(1,m):
+            for c in range(1,n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+
+        return dp[m-1][n-1]
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#        #  O(M*N), O(M*N)
+#         dp = [[1] * n] * m
+        
+#         for r in range(1,m):
+#             for c in range(1,n):
+#                 dp[r][c] = dp[r-1][c] + dp[r][c-1]
+#         return dp[m-1][n-1]
+        
+        
+        
+        
+    
+        
+#         res = [[1] * n] * m
+#         for row in range(1, m):
+#             for col in range(1, n):
+#                 res[row][col] = res[row - 1][col] + res[row][col - 1]
+
+#         return res[m - 1][n - 1]
+        
