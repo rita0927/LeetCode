1f79aabf7aa6ccf747d6c11c58a878f69b9330e5
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 21:33:35 -0400

    Time: 310 ms (15.21%), Space: 14.4 MB (99.28%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,32 +1,82 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
+        def isPalindrome(l,r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True 
+        
+        
         l = 0 
         r = len(s) - 1
         
         while l < r:
             if s[l] != s[r]:
-                str1 = s[:l]+s[l+1:]
-                str2 = s[:r]+s[r+1:]
-                return str1 == str1[::-1] or str2 == str2[::-1]
+                return isPalindrome(l+1, r) or isPalindrome(l, r-1)
             l += 1
             r -= 1
         return True 
         
+                
+        
 
-       
-    
-    
-    
-    
-    
-    
-    
         
         
         
         
-#         # 0(N), O(1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         l = 0 
+#         r = len(s) - 1
+        
+#         while l < r:
+#             if s[l] != s[r]:
+#                 str1 = s[:l]+s[l+1:]
+#                 str2 = s[:r]+s[r+1:]
+#                 return str1 == str1[::-1] or str2 == str2[::-1]
+#             l += 1
+#             r -= 1
+#         return True 
+        
+
+        
+        
+
+        
+        
+        
+        
+        
+#         # 0(N) the while loop and recursion comprise one complete loop, O(1)
 #         def isPalindrome(l,r):
 #             while l < r:
 #                 if s[l] !=s[r]:
