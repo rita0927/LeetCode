df60f363674950b9ce207bc94f5e6852ab55bed0
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-13 18:12:02 -0400

    Time: 3847 ms (12.28%), Space: 73 MB (70.30%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -6,41 +6,97 @@
 #         self.right = right
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
-        parents = {}
-        self.start = None
-
-        def findNode(parent, node):
-
-            parents[node] = parent
-            if node.val == startValue:
-                self.start = node
-            
-            if node.left:
-                findNode(node, node.left)
-            if node.right:
-                findNode(node, node.right)
-            
-        findNode(None, root)
-        queue = deque([(self.start, '')])
-        visited = set()
-        # visited.add(self.start.val)
+        
+        graph = defaultdict(list)
+        queue = deque([root])
         
         while queue:
-
-            node, path = queue.popleft()
-            visited.add(node.val)
+            node = queue.popleft()
+            if node.left:
+                graph[node.val].append([node.left.val, 'L'])
+                graph[node.left.val].append([node.val, 'U'])
+                queue.append(node.left)
+            if node.right:
+                graph[node.val].append([node.right.val, 'R'])
+                graph[node.right.val].append([node.val, 'U'])
+                queue.append(node.right)
+        
+        queue = deque([(startValue, '')])
+        visited = set()
+        visited.add(startValue)
+        while queue:
+            val, path = queue.popleft()
             
-            if node.val == destValue:
+            if val == destValue:
                 return path
-            if node.left and node.left.val not in visited:
-                queue.append((node.left, path+'L'))
-                # visited.add(node.left.val)
-            if node.right and node.right.val not in visited:
-                queue.append((node.right, path+'R'))
-                # visited.add(node.right.val)
-            if parents[node] and parents[node].val not in visited:
-                queue.append((parents[node], path+'U'))
-                # visited.add(parents[node].val)
+            
+            for next, dir in graph[val]:
+                if next not in visited:
+                    visited.add(next)
+                    queue.append((next, path+dir))
+                
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         parents = {}
+#         self.start = None
+
+#         def findNode(parent, node):
+
+#             parents[node] = parent
+#             if node.val == startValue:
+#                 self.start = node
+            
+#             if node.left:
+#                 findNode(node, node.left)
+#             if node.right:
+#                 findNode(node, node.right)
+            
+#         findNode(None, root)
+#         queue = deque([(self.start, '')])
+#         visited = set()
+#         visited.add(self.start.val)
+        
+#         while queue:
+
+#             node, path = queue.popleft()
+            
+#             if node.val == destValue:
+#                 return path
+#             if node.left and node.left.val not in visited:
+#                 queue.append((node.left, path+'L'))
+#                 visited.add(node.left.val)
+#             if node.right and node.right.val not in visited:
+#                 queue.append((node.right, path+'R'))
+#                 visited.add(node.right.val)
+#             if parents[node] and parents[node].val not in visited:
+#                 queue.append((parents[node], path+'U'))
+#                 visited.add(parents[node].val)
                     
         
                         
