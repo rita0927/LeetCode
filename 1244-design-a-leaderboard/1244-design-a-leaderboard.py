43832239849a6e4d023c625ab30b01681d049f38
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 21:15:49 -0400

    Time: 100 ms (60.60%), Space: 14.3 MB (38.55%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -3,48 +3,27 @@ from heapq import *
 class Leaderboard:
 
     def __init__(self):
+        self.board = defaultdict(int)
         
-        self.board= defaultdict(int)
-  
-    def addScore(self, playerId: int, score: int) -> None:
-        
-        self.board[playerId] += score  
 
+    def addScore(self, playerId: int, score: int) -> None:
+        self.board[playerId] += score
+        
 
     def top(self, K: int) -> int:
-        scores = list(self.board.values())
-        K = len(scores) - K
         
-        def quickSelect(l,r):
-            pivot_index = random.randint(l,r)
-            pivot = scores[pivot_index]
-            scores[pivot_index], scores[r] = scores[r], scores[pivot_index]
-            
-            p = l
-            
-            for i in range(l,r):
-                if scores[i] < pivot:
-                    scores[i], scores[p] = scores[p], scores[i]
-                    p += 1
-            scores[p], scores[r] = scores[r], scores[p]
-            
-            if K < p:
-                return quickSelect(l, p - 1)
-            elif K > p:
-                return quickSelect(p + 1, r)
-            else:
-                return
-               
-        quickSelect(0, len(scores) - 1)
-        return sum(scores[K:])
-        
-        
+        heap = []
+        for score in self.board.values():
+            heapq.heappush(heap, score)
+            if len(heap) > K:
+                heapq.heappop(heap)
+        return sum(heap)
+
      
 
     def reset(self, playerId: int) -> None:
         del self.board[playerId]
 
-        
         
         
         
