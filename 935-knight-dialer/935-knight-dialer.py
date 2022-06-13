0f7e8e5609276a5d73aebe059f816c21daaea533
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 23:37:41 -0400

    Time: 1325 ms (62.10%), Space: 14 MB (82.93%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,30 +1,30 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        mod = 10**9+7
+        mod = 10 ** 9 + 7
         neighbors = {
-            0: [4,6],
-            1: [6,8],
-            2: [7,9],
-            3: [4,8],
-            4: [0,3,9],
-            5: [],
-            6: [0,1,7],
-            7: [2,6],
-            8: [1,3],
-            9 :[2,4]
+            0: (4,6),
+            1: (6,8),
+            2: (7,9),
+            3: (4,8),
+            4: (0,3,9),
+            5: (),
+            6: (0,1,7),
+            7: (2,6),
+            8: (1,3),
+            9: (2,4),          
         }
         
-        cur = [1]*10
-        
+        cur = [1] * 10
         for i in range(n-1):
-            next = [0]*10
+            next = [0] * 10
+            for num in range(10):
+                for neighbor in neighbors[num]:
+                    next[num] += cur[neighbor] % mod
+            cur = next 
+        return sum(cur) % mod
             
-            for i in range(10):
-                for neighbor in neighbors[i]:
-                    next[i] += cur[neighbor]%mod
-            cur = next
             
-        return sum(cur)%mod 
+
         
         
         
