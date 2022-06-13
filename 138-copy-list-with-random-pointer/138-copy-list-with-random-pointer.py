8a8eae76520ba3104453dae72cb00a995e98214f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-12 23:24:26 -0400

    Time: 71 ms (12.07%), Space: 15 MB (45.15%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,27 +9,84 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        # Dictionary which holds old nodes as keys and new nodes as its values
-        
         if not head:
-            return None
+            return None 
         
-        dic = {}
-        
-        node = head
-        
-        while node:
-            dic[node] = Node(node.val)
-            node = node.next
+        d = {}
         
         node = head
         
         while node:
-            dic[node].next = dic[node.next] if node.next else None
-            dic[node].random = dic[node.random] if node.random else None
-            node = node.next
+            d[node] = Node(node.val)
+            node = node.next 
         
-        return dic[head]
+        node = head
+        
+        while node:
+            new_node = d[node]
+            new_node.next = d[node.next] if node.next else None
+            new_node.random = d[node.random] if node.random else None 
+            node = node.next 
+            
+        return d[head]
+            
+        
+
+        
+
+            
+        
+  
+
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
+#         if not head:
+#             return None
+        
+#         # Dictionary which holds old nodes as keys and new nodes as its values
+#         dic = {}
+        
+#         node = head
+        
+#         # creat a new node for every node 
+#         while node:
+#             dic[node] = Node(node.val)
+#             node = node.next
+        
+#         node = head
+        
+#         while node:
+#             dic[node].next = dic[node.next] if node.next else None
+#             dic[node].random = dic[node.random] if node.random else None
+#             node = node.next
+        
+#         return dic[head]
         
         
         
