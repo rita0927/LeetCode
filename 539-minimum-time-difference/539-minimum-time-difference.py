3b2e136d351a5835aa992711d0aed515b47e3e77
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-15 19:09:24 -0400

    Time: 80 ms (84.03%), Space: 17.3 MB (16.52%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -6,11 +6,48 @@ class Solution:
             times.append(int(h) * 60 + int(m))
         
         times.sort()
-        # comparing the diff between last and first elements
-        # for loop compares the rest of the elements
-        res = (1440 + times[0]) - times[-1]
-        for i in range(1, len(times)):
+        res = 1440 + times[0] - times[-1]
+        
+        for i in range(1,len(times)):
             res = min(res, times[i] - times[i-1])
         
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Olog(N), O(N) due to sort
+#         times = []
+#         for t in timePoints:
+#             h,m = t.split(':')
+#             times.append(int(h) * 60 + int(m))
+        
+#         times.sort()
+#         # comparing the diff between last and first elements
+#         # for loop compares the rest of the elements
+#         res = (1440 + times[0]) - times[-1]
+#         for i in range(1, len(times)):
+#             res = min(res, times[i] - times[i-1])
+        
+#         return res 
         
