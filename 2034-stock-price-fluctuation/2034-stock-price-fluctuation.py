eba1ed1f4795033309ed879df53a2456373c785e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-15 18:20:37 -0400

    Time: 1242 ms (41.81%), Space: 62.7 MB (6.12%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.py
@@ -1,51 +1,36 @@
+from heapq import *
 class StockPrice:
 
     def __init__(self):
-        # record all price & timestamp 
         self.records = {}
-        # record latest timestamp
         self.latest = 0
         self.min_heap = []
         self.max_heap = []
-        
+
 
     def update(self, timestamp: int, price: int) -> None:
-        self.records[timestamp] = price
+        self.records[timestamp] = price 
         self.latest = max(self.latest, timestamp)
-        # don't remove the old price from the heap, leave it for max & min methods 
-        heapq.heappush(self.min_heap, [price, timestamp])
-        heapq.heappush(self.max_heap, [-price, timestamp])
-        
+        heappush(self.min_heap, [price, timestamp])
+        heappush(self.max_heap, [-price, timestamp])
+   
     def current(self) -> int:
         return self.records[self.latest]
         
-
     def maximum(self) -> int:
-        price, time = heapq.heappop(self.max_heap)
-        
-        # if the price doesn't match the records[time], the price has been updated
-        # max_heap stores -price while records stores price 
+        price, time = heappop(self.max_heap)
         while -price != self.records[time]:
-            # pop pop outdated price & time until price matches the current records[time]
-            price, time = heapq.heappop(self.max_heap)
-        # the last price & time are valid (not outdated) so need to push it back to the heap 
-        heapq.heappush(self.max_heap, [price, time])
+            price, time = heappop(self.max_heap)
         
+        heappush(self.max_heap, [price, time])
         return -price 
-        
 
     def minimum(self) -> int:
-        price, time = heapq.heappop(self.min_heap)
-        
+        price, time = heappop(self.min_heap)
         while price != self.records[time]:
-            price, time = heapq.heappop(self.min_heap)
-        
-        heapq.heappush(self.min_heap, [price, time])
+            price, time = heappop(self.min_heap)
+        heappush(self.min_heap, [price, time])
         return price 
-        
-        
-
-        
         
         
         
@@ -58,3 +43,54 @@ class StockPrice:
 # param_2 = obj.current()
 # param_3 = obj.maximum()
 # param_4 = obj.minimum()
+
+
+
+
+
+# class StockPrice:
+
+#     def __init__(self):
+#         # record all price & timestamp 
+#         self.records = {}
+#         # record latest timestamp
+#         self.latest = 0
+#         self.min_heap = []
+#         self.max_heap = []
+        
+
+#     def update(self, timestamp: int, price: int) -> None:
+#         self.records[timestamp] = price
+#         self.latest = max(self.latest, timestamp)
+#         # don't remove the old price from the heap, leave it for max & min methods 
+#         heapq.heappush(self.min_heap, [price, timestamp])
+#         heapq.heappush(self.max_heap, [-price, timestamp])
+        
+#     def current(self) -> int:
+#         return self.records[self.latest]
+        
+
+#     def maximum(self) -> int:
+#         price, time = heapq.heappop(self.max_heap)
+        
+#         # if the price doesn't match the records[time], the price has been updated
+#         # max_heap stores -price while records stores price 
+#         while -price != self.records[time]:
+#             # pop pop outdated price & time until price matches the current records[time]
+#             price, time = heapq.heappop(self.max_heap)
+#         # the last price & time are valid (not outdated) so need to push it back to the heap 
+#         heapq.heappush(self.max_heap, [price, time])
+        
+#         return -price 
+        
+
+#     def minimum(self) -> int:
+#         price, time = heapq.heappop(self.min_heap)
+        
+#         while price != self.records[time]:
+#             price, time = heapq.heappop(self.min_heap)
+        
+#         heapq.heappush(self.min_heap, [price, time])
+#         return price 
+        
+        
