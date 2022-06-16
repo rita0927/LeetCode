8abb5551d79787fa47ebdcaa78b31a493551bd39
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-15 21:33:08 -0400

    Time: 113 ms (41.79%), Space: 17.1 MB (70.16%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -1,17 +1,36 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        times = []
-        for t in timePoints:
+
+        def convertToMinutes(t):
             h,m = t.split(':')
-            times.append(int(h) * 60 + int(m))
+            return int(h) * 60 + int(m)
         
-        times.sort()
-        res = 1440 + times[0] - times[-1]
+        times = [False] * 1440
         
-        for i in range(1,len(times)):
-            res = min(res, times[i] - times[i-1])
+        for t in timePoints:
+            m = convertToMinutes(t)
+            
+            if times[m]:
+                return 0
+            
+            times[m] = True
+            
+        res = float('inf')
+        prev = 0
+        min_m, max_m = float('inf'), float('-inf')
         
+        for m in range(len(times)):
+            
+            if times[m]:
+                # don't compare the pair until the min_m & prev are updated to the m
+                if min_m != float('inf'):
+                    res = min(res, m - prev)
+                min_m = min(min_m, m)
+                max_m = max(max_m, m)
+                prev = m
+        res = min(res, 1440+min_m - max_m)
         return res 
+            
         
         
         
@@ -27,6 +46,20 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
         
         
         
