c38e8611e661fd341766f5e80ad09f0f7b824101
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-15 21:59:33 -0400

    Time: 78 ms (86.93%), Space: 16.8 MB (99.68%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -1,35 +1,95 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-
-        def convertToMinutes(t):
+        
+        def convert(t):
             h,m = t.split(':')
             return int(h) * 60 + int(m)
         
         times = [False] * 1440
-        
         for t in timePoints:
-            m = convertToMinutes(t)
-            
+            m = convert(t)
             if times[m]:
                 return 0
-            
             times[m] = True
             
-        res = float('inf')
         prev = 0
-        min_m, max_m = float('inf'), float('-inf')
-        
+        min_m = float('inf')
+        max_m = float('-inf')
+        res = float('inf')
         for m in range(len(times)):
-            
             if times[m]:
-                # don't compare the pair until the min_m & prev are updated to the m
+                
                 if min_m != float('inf'):
-                    res = min(res, m - prev)
+                    res = min(res, m-prev)     
+                prev = m
                 min_m = min(min_m, m)
                 max_m = max(max_m, m)
-                prev = m
-        res = min(res, 1440+min_m - max_m)
-        return res 
+        return min(res, 1440+min_m-max_m)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Bucket sort, time complexity O(n)
+
+#         def convertToMinutes(t):
+#             h,m = t.split(':')
+#             return int(h) * 60 + int(m)
+        
+#         # create bucket for each min
+#         times = [False] * 1440
+        
+#         for t in timePoints:
+#             m = convertToMinutes(t)
+#             # if two time points are the same (the times[m] was updated to True already)
+#             if times[m]:
+#                 return 0
+            
+#             times[m] = True
+            
+#         res = float('inf')
+#         # track the prev time in the times (sorted buckets)
+#         prev = 0
+#         min_m, max_m = float('inf'), float('-inf')
+        
+#         # index of the times is the minute we want to sort
+#         for m in range(len(times)):
+#             # minutes in the timePoints are marked as True
+#             if times[m]:
+#                 # don't compare the pair until the min_m & prev are updated to the actual m
+#                 if min_m != float('inf'):
+#                     res = min(res, m - prev)
+#                 min_m = min(min_m, m)
+#                 max_m = max(max_m, m)
+#                 prev = m
+#         res = min(res, 1440+min_m - max_m)
+#         return res 
             
         
         
