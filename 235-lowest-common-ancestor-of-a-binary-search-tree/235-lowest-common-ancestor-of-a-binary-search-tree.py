cc78f35bee18903824150384470c94032ca436ce
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-17 12:59:58 -0400

    Time: 107 ms (55.96%), Space: 18.9 MB (22.19%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,20 +8,54 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
-        # O(logN) on average binary search tree or worst case O(N) for skewed tree
-        # O(1) 
+        node = root 
         
-        cur = root
-        
-        while cur:
-            if p.val < cur.val and q.val < cur.val:
-                cur = cur.left
-                
-            elif p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-                
+        while node:
+            if p.val < node.val and q.val < node.val:
+                node = node.left 
+            elif p.val > node.val and q.val > node.val:
+                node = node.right 
             else:
-                return cur 
+                return node 
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(logN) on average binary search tree or worst case O(N) for skewed tree
+#         # O(1) 
+        
+#         cur = root
+        
+#         while cur:
+#             if p.val < cur.val and q.val < cur.val:
+#                 cur = cur.left
+                
+#             elif p.val > cur.val and q.val > cur.val:
+#                 cur = cur.right
+                
+#             else:
+#                 return cur 
         
       
                 
