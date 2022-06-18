2f398dc8288233a37b2cac3fbbd8e989f2673744
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-18 00:15:39 -0400

    Time: 47 ms (61.37%), Space: 13.9 MB (28.98%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -7,15 +7,14 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         
+        matrix.reverse()
+        
         for r in range(m):
-            for c in range(r, n):
+            for c in range(r,n):
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
-        
-        for r in range(m):
-            for c in range(n//2):
-                matrix[r][c], matrix[r][-c-1] = matrix[r][-c-1], matrix[r][c]
-        
         return matrix 
+        
+
                 
                 
                 
