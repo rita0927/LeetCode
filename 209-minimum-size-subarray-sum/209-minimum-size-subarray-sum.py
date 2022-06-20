f64bfab25c4826fca08facee6a57531f87836a07
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-20 17:28:26 -0400

    Time: 284 ms (6.23%), Space: 27.2 MB (14.01%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,59 +1,82 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        res = float('inf')
-        l = 0
-        total = 0
         
+        res = float('inf') 
+        l = 0
+        total = 0 
         for r in range(len(nums)):
             total += nums[r]
             
             while total >= target:
                 res = min(res, r-l+1)
                 total -= nums[l]
-                l+=1
+                l += 1
+        
+        return res if res != float('inf') else 0 
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n^3)    ETL
+#         res = float('inf')
+        
+#         for i in range(len(nums)):
+#             for j in range(i, len(nums)):
+
+#                 total = sum(nums[i:j+1])
+                    
+#                 if total >= target:
+#                     res = min(res, j-i+1)
+        
+#         return res if res != float('inf') else 0
                 
-        
-        return res if res != float('inf') else 0
-            
-            
-            
+                
+      
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
         
         
