03019734586f0737e3c1f62ebe9040cf2675279e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-21 00:59:51 -0400

    Time: 947 ms (5.04%), Space: 16.4 MB (50.63%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -1,12 +1,61 @@
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
-
-        row_ori = grid[0]
-        row_flip = [1-x for x in grid[0]]
         
-        for r in grid[1:]:
-            if r != row_ori and r != row_flip:
+        origin = grid[0]
+        flip = [1-x for x in grid[0]]
+        
+        for r in grid:
+            if r != origin and r != flip:
                 return False
+            
         return True 
+        
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # each pair of rows have to be the same or completely opposite, then the flip can make them the same
+
+#         row_ori = grid[0]
+#         row_flip = [1-x for x in grid[0]]
+        
+#         for r in grid[1:]:
+#             if r != row_ori and r != row_flip:
+#                 return False
+#         return True 
             
         
