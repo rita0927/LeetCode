da51651f206fd2b13b3963161fc9af870a0d0b1b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-21 02:25:59 -0400

    Time: 1657 ms (26.94%), Space: 17.1 MB (50.42%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -5,26 +5,28 @@ class Solution:
         indegree = defaultdict(int)
         res = []
         
-        for i in range(len(recipes)):
-            recipe = recipes[i]
-            for ingredient in ingredients[i]:
-                food[ingredient].append(recipe)
+        for r in range(len(recipes)):
+            recipe = recipes[r]
+            for i in ingredients[r]:
+                food[i].append(recipe)
                 indegree[recipe] += 1
         
         queue = deque(supplies)
+        
         while queue:
-            ingredient = queue.popleft()
-            for recipe in food[ingredient]:
-                indegree[recipe] -= 1
-                
-                if not indegree[recipe]:
-                    queue.append(recipe)
-                    if recipe in recipes:
-                        res.append(recipe)
-                        
-        return res 
-
+            i = queue.popleft()
             
+            for r in food[i]:
+                indegree[r] -= 1
+                
+                if not indegree[r]:
+                    queue.append(r)
+                    
+                    if r in recipes:
+                        res.append(r)
+        return res 
+        
+
         
         
         
