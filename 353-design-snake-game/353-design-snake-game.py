7717b33eed638b6c55a1ba3ed7b5ee3da13573c7
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-21 01:53:53 -0400

    Time: 311 ms (61.54%), Space: 15.8 MB (57.42%) - LeetHub

--- 353-design-snake-game/353-design-snake-game.py
+++ 353-design-snake-game/353-design-snake-game.py
@@ -2,60 +2,44 @@ class SnakeGame:
 
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.snake = deque([(0,0)])
-        # use set to check if the snake is going to bite the body
-        # set is optional, improve the time complexity at the cost of the space complexity
-        self.snake_set = set([(0,0)])
-        self.m = height
-        self.n = width
+        self.width = width
+        self.height = height
+        self.snake = [[0,0]]
         self.r = 0
         self.c = 0
-        self.food = food
         self.score = 0
+        self.food = food 
+        self.dir = {'R': [0,1], 'L': [0,-1], 'U': [-1,0], 'D': [1,0]}
+        
 
     def move(self, direction: str) -> int:
         
-        if direction == 'R':
-            self.c += 1
-        elif direction == 'L':
-            self.c -= 1
-        elif direction == 'U':
-            self.r -= 1
-        elif direction == 'D':
-            self.r += 1
+        self.r += self.dir[direction][0]
+        self.c += self.dir[direction][1]
         
-        if self.r < 0 or self.r >= self.m or self.c < 0 or self.c >= self.n:
-            return -1
-        # check if the snake is going to a cell that's occupied by the body already
-        # it's ok the snake is into a cell ocupied by the tail since the tail is leaving that cell once the snake is entering
-        if (self.r,self.c) in self.snake_set and (self.r, self.c) != self.snake[0]:
+        if self.r < 0 or self.r >= self.height or self.c < 0 or self.c >= self.width:
             return -1
         
-        if len(self.snake) != len(self.snake_set):
-            print(self.snake, self.snake_set)
+        if [self.r,self.c] in self.snake and [self.r, self.c] != self.snake[0]:
+            return -1
+        self.snake.append([self.r, self.c])
         
-        # if the new head cell contains food, need to pop it out from the left of the food 
-        # the tail doesn't move 
-        if self.food and self.food[0] == [self.r, self.c]:
+        if self.food and [self.r, self.c] == self.food[0]:
             self.food.pop(0)
             self.score += 1
         else:
-            # the tail pops out only if the new head cell doesn't contain the food 
-            # previous tail needs to be popped out (tail moves to the next position)
-            tail = self.snake.popleft()
-            self.snake_set.remove(tail)
-        # need to remove the tail (if it's needed) before adding the new head 
-        # if the tail occupies the new head cell, add before remove from set will cause error in snake set 
+            self.snake.pop(0)
+        
+        return self.score 
             
-        # append the new head cell to the snake
-        self.snake.append((self.r, self.c))
-        self.snake_set.add((self.r, self.c))
-        return self.score
         
         
         
         
         
+        
+        
+
         
         
         
