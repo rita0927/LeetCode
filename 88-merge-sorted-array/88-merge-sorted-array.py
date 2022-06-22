66826c8acc6218c48425048cb3cc925e523ebf81
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-22 01:37:50 -0400

    Time: 36 ms (95.41%), Space: 14 MB (37.61%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -2,28 +2,81 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        
         """
-
-        p1= m-1
-        p2 = n-1
         
+        p1 = m - 1
+        p2 = n - 1
         
-        for p in range(m + n - 1, -1, -1):
+        for p in range(m+n-1, -1, -1):
+            
             if p2 < 0:
                 break
-            if p1 >=0 and nums1[p1] > nums2[p2]:
+            elif p1 >= 0 and nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
-                p1-=1
+                p1 -= 1
             else:
                 nums1[p] = nums2[p2]
-                p2-=1
+                p2 -= 1
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#     # O(m + n), O(1)
+        
+#         p1= m-1
+#         p2 = n-1
+        
+        
+#         for p in range(m + n - 1, -1, -1):
+#             if p2 < 0:
+#                 break
+#             if p1 >=0 and nums1[p1] > nums2[p2]:
+#                 nums1[p] = nums1[p1]
+#                 p1-=1
+#             else:
+#                 nums1[p] = nums2[p2]
+#                 p2-=1
             
             
             
             
             
             
-            
+# O((m+n)*log(m+n))
+# O(n)
 
 #         for i in range(n):
 #             nums1[i + m] = nums2[i]
