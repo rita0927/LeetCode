96ee9ab6f7acd5b6963f1070086360917ea5279a
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-22 01:43:35 -0400

    Time: 69 ms (21.34%), Space: 13.8 MB (85.40%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -9,15 +9,21 @@ class Solution:
         p2 = n - 1
         
         for p in range(m+n-1, -1, -1):
-            
             if p2 < 0:
                 break
-            elif p1 >= 0 and nums1[p1] > nums2[p2]:
+            elif p1 >= 0 and nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            elif p1 >= 0:
                 nums1[p] = nums1[p1]
                 p1 -= 1
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
+                
+                
+                
+
 
         
         
