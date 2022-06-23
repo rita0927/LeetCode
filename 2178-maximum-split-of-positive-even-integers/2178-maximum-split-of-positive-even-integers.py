c88842846be08e9d542c61bdac49914ce7c2fef9
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-23 13:05:34 -0400

    Time: 970 ms (12.83%), Space: 26.8 MB (41.38%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
@@ -1,98 +1,23 @@
 class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
+        res = []
         
-        res = set()
-        
-        if finalSum %2:
+        if finalSum % 2 or finalSum < 2:
             return []
         
-        num = 2
+        cur_sum = 0
+        cur_num = 2
+
+        while cur_sum < finalSum:
+            res.append(cur_num)
+            cur_sum += cur_num
+            cur_num +=2
         
-        while finalSum >0:
-            res.add(num)
-            finalSum -= num
-            num += 2
-            
-            if finalSum < 0:
-                res.remove(-finalSum)
+        if cur_sum == finalSum:
+            return res 
+        else:
+            # res.remove(cur_sum - finalSum)
+            res.pop((cur_sum-finalSum)//2 -1)
+        
         return res 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res = set()
-        
-#         if finalSum % 2:
-#             return []
-        
-#         num = 2
-        
-#         while finalSum >0:
-#             res.add(num)
-#             finalSum -= num
-#             num += 2
-            
-#             if finalSum <0:
-#                 res.remove(-finalSum)
-#                 break
-#         return res 
-                
-            
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res = []
-        
-#         if finalSum % 2 or finalSum < 2:
-#             return []
-        
-#         cur_sum = 0
-#         cur_num = 2
-
-#         while cur_sum < finalSum:
-#             res.append(cur_num)
-#             cur_sum += cur_num
-#             cur_num +=2
-        
-#         if cur_sum == finalSum:
-#             return res 
-#         else:
-#             # res.remove(cur_sum - finalSum)
-#             res.pop(res.index(cur_sum-finalSum))
-        
-#         return res 
         
