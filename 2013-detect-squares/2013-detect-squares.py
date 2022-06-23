386e526c8d3fab292697f607f9d92c6263b4ce1b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-23 18:23:08 -0400

    Time: 649 ms (30.90%), Space: 16 MB (56.13%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -2,26 +2,26 @@ class DetectSquares:
 
     def __init__(self):
         self.points = defaultdict(int)
-        self.arr = []
+        # self.arr = []
 
 
     def add(self, point: List[int]) -> None:
         # count the freq of each coordinate
         self.points[tuple(point)] += 1
-        self.arr.append(point)
+        # self.arr.append(point)
 
 
     def count(self, point: List[int]) -> int:
         count = 0
         x,y = point
-        for x1,y1 in self.arr:
+        for x1,y1 in self.points:
             # must have positive area (can't be identical coordinates)
             if abs(x1-x) == abs(y1-y) and x1 != x and y1 != y:
-                # if (x1,y) in self.points and (x,y1) in self.points:
-                #     count += self.points[(x1,y)] * self.points[(x,y1)]
+                if (x1,y) in self.points and (x,y1) in self.points:
+                    count += self.points[(x1,y1)] * self.points[(x1,y)] * self.points[(x,y1)]
                 
                 # if either point doesn't exist, the count won't increase
-                count += self.points[(x,y1)] * self.points[(x1,y)]
+                # count += self.points[(x1,y1)] *self.points[(x,y1)] * self.points[(x1,y)]
         return count 
                 
             
