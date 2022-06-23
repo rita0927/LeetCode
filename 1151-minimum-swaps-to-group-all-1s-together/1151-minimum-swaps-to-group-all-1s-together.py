fef6f6d27d26ac5b88588167e0e82c086d9e3007
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-23 00:21:03 -0400

    Time: 1047 ms (57.73%), Space: 17.5 MB (52.06%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.py
@@ -1,22 +1,25 @@
 class Solution:
     def minSwaps(self, data: List[int]) -> int:
-        
         ones = sum(data)
+        
+        if ones <= 1:
+            return 0
+        
         maxOnes = 0
         curOnes = 0
-        l = 0
         
-        for r in range(len(data)):
-            curOnes += data[r]
-            
-            if r-l+1 == ones:
+        for i in range(len(data)):
+            curOnes += data[i]
+            if i + 1 >= ones:
                 maxOnes = max(maxOnes, curOnes)
-                curOnes -= data[l]
-                l+= 1
+                curOnes -= data[i-ones+1]
+        
         return ones - maxOnes 
                 
         
-
+        
+        
+ 
         
         
         
