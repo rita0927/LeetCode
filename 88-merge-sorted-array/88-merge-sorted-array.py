562b2d2abbc6501e750f4a67f656dcd9f387caa8
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-23 20:55:47 -0400

    Time: 40 ms (89.05%), Space: 13.9 MB (37.50%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,28 +4,21 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         
         """
-        
+
         p1 = m - 1
-        p2 = n - 1
+        p2 = n - 1 
         
-        for p in range(m+n-1, -1, -1):
+        for i in range(m+n-1, -1, -1):
             if p2 < 0:
                 break
-            elif p1 >= 0 and nums1[p1] < nums2[p2]:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-            elif p1 >= 0:
-                nums1[p] = nums1[p1]
+            elif p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[p] = nums2[p2]
+                nums1[i] = nums2[p2]
                 p2 -= 1
-                
-                
-                
-
-
         
+        return nums1
         
         
         
