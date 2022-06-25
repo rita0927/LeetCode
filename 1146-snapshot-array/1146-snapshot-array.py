413084244c4b92b4949cc37780537b11ba743791
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-24 20:28:08 -0400

    Time: 519 ms (72.58%), Space: 34.4 MB (52.97%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -1,39 +1,95 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        # create a list of list, the outter list refers to the array of given length, each element (inner list) is the initial & updates on that element
-        # the tuple refers to the snap_id and val stored in that snap_id
         self.snaps = [[(0,0)] for _ in range(length)]
         self.snap_id = 0
-
+        
+        
     def set(self, index: int, val: int) -> None:
-        # if the last snap_id in the tuple matches current snaps_id
         if self.snaps[index][-1][0] == self.snap_id:
-            # tuple is not mutable, need to replace the entire tuple
             self.snaps[index][-1] = (self.snap_id, val)
-        # the snap_id doesn't match, need to append another update
         else:
             self.snaps[index].append((self.snap_id, val))
+
         
     def snap(self) -> int:
-        self.snap_id +=1
+        self.snap_id += 1
         return self.snap_id - 1
-        
 
     def get(self, index: int, snap_id: int) -> int:
         lst = self.snaps[index]
         l = -1
         r = len(lst)
         
-        while l+1 != r:
-            
+        while l+1 !=r:
             mid = (l+r)//2
             
             if lst[mid][0] <= snap_id:
                 l = mid
             else:
-                r = mid
+                r = mid 
+                
         return lst[l][1] if l != -1 else 0
+    
+    
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class SnapshotArray:
+
+#     def __init__(self, length: int):
+#         # create a list of list, the outter list refers to the array of given length, each element (inner list) is the initial & updates on that element
+#         # the tuple refers to the snap_id and val stored in that snap_id
+#         self.snaps = [[(0,0)] for _ in range(length)]
+#         self.snap_id = 0
+
+#     def set(self, index: int, val: int) -> None:
+#         # if the last snap_id in the tuple matches current snaps_id
+#         if self.snaps[index][-1][0] == self.snap_id:
+#             # tuple is not mutable, need to replace the entire tuple
+#             self.snaps[index][-1] = (self.snap_id, val)
+#         # the snap_id doesn't match, need to append another update
+#         else:
+#             self.snaps[index].append((self.snap_id, val))
+        
+#     def snap(self) -> int:
+#         self.snap_id +=1
+#         return self.snap_id - 1
+        
+
+#     def get(self, index: int, snap_id: int) -> int:
+#         lst = self.snaps[index]
+#         l = -1
+#         r = len(lst)
+        
+#         while l+1 != r:
+            
+#             mid = (l+r)//2
+            
+#             if lst[mid][0] <= snap_id:
+#                 l = mid
+#             else:
+#                 r = mid
+#         return lst[l][1] if l != -1 else 0
                 
                 
         
