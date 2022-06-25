3e2469e1c245bb09f6b90537715fa29a5ba6062e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-24 23:10:56 -0400

    Time: 229 ms (89.11%), Space: 18.5 MB (78.17%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -0,0 +1,42 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        # create a range based on the sum of all the w[i]
+        # each index is represented by a segment of the range 
+        # pick a random num from the range, and find the corresponding index/segment
+        
+        # append the prefix (segment cut off point)
+        self.prefix = []
+        prefix = 0
+        for i in w:
+            prefix += i
+            self.prefix.append(prefix)
+        self.total = prefix
+        
+    def pickIndex(self) -> int:
+        
+        # pick a random num from the range of self.total 
+        # use random.random so that the range is [0,total), doesn't include the total number in the range 
+        num = self.total * random.random()
+        l = -1 
+        r = len(self.prefix)
+        
+        while l+1 != r:
+            mid = (l+r)//2
+            
+            if self.prefix[mid] < num:
+                l = mid
+            else:
+                r = mid 
+        return r 
+        
+        
+
+
+        
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
