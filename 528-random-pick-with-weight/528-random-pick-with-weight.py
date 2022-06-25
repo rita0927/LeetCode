cea8e5f44588963aa464747e41357f888909bc9c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-25 00:06:05 -0400

    Time: 218 ms (92.93%), Space: 18.4 MB (94.68%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,37 +1,29 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        # create a range based on the sum of all the w[i]
-        # each index is represented by a segment of the range 
-        # pick a random num from the range, and find the corresponding index/segment
-        
-        # append the prefix (segment cut off point)
         self.prefix = []
-        prefix = 0
+        temp = 0
         for i in w:
-            prefix += i
-            self.prefix.append(prefix)
-        self.total = prefix
-        
+            temp += i
+            self.prefix.append(temp)
+        self.sum = temp
+              
     def pickIndex(self) -> int:
+        num = self.sum * random.random()
         
-        # pick a random num from the range of self.total 
-        # use random.random so that the range is [0,total), doesn't include the total number in the range 
-        num = self.total * random.random()
-        l = -1 
+        l = -1
         r = len(self.prefix)
         
         while l+1 != r:
             mid = (l+r)//2
-            # l is the ending of previous segment
+            
             if self.prefix[mid] <= num:
                 l = mid
-            # find the segment that contains the num, self.prefix[l] <= num < self.prefix[r]
             else:
-                r = mid 
+                r = mid
         return r 
         
-        
+
 
 
         
@@ -41,3 +33,43 @@ class Solution:
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
+
+
+
+
+
+
+
+
+# class Solution:
+
+#     def __init__(self, w: List[int]):
+#         # create a range based on the sum of all the w[i]
+#         # each index is represented by a segment of the range 
+#         # pick a random num from the range, and find the corresponding index/segment
+        
+#         # append the prefix (segment cut off point)
+#         self.prefix = []
+#         prefix = 0
+#         for i in w:
+#             prefix += i
+#             self.prefix.append(prefix)
+#         self.total = prefix
+        
+#     def pickIndex(self) -> int:
+        
+#         # pick a random num from the range of self.total 
+#         # use random.random so that the range is [0,total), doesn't include the total number in the range 
+#         num = self.total * random.random()
+#         l = -1 
+#         r = len(self.prefix)
+        
+#         while l+1 != r:
+#             mid = (l+r)//2
+#             # l is the ending of previous segment, num is not in this segment 
+#             if self.prefix[mid] <= num:
+#                 l = mid
+#             # find the segment that contains the num, self.prefix[l] <= num < self.prefix[r]
+#             else:
+#                 r = mid 
+#         return r 
