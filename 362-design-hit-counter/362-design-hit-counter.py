f61f062d3fc7ea28bbf393309540d60f4132d0bf
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-25 19:26:14 -0400

    Time: 43 ms (59.91%), Space: 14 MB (16.44%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -1,41 +1,73 @@
 class HitCounter:
-
-    # space: O(N) in the case of repetitions in the same timestamp, the space required for storing those values O(1)
+    
     def __init__(self):
-        self.queue = deque()
-        # self.count = 0
+        self.hits = []
+
 
     def hit(self, timestamp: int) -> None:
-        if self.queue and timestamp == self.queue[-1][0]:
-            self.queue[-1][1] += 1
+        if self.hits and self.hits[-1][0] == timestamp:
+            self.hits[-1][1] += 1
         else:
-            self.queue.append([timestamp, 1])
-        # self.count+=1
-        
-    # time: O(N) 
+            self.hits.append([timestamp, 1])
+
     def getHits(self, timestamp: int) -> int:
         
-        l = -1 
-        r = len(self.queue)
+        l = -1
+        r = len(self.hits)
         
-        # find the cut off of the past 300 sec, [r, timestamp]
-        while l + 1 != r:
+        while l+1 != r:
             mid = (l+r)//2
             
-            if self.queue[mid][0] <= timestamp - 300:
-                l = mid
+            if self.hits[mid][0] <= timestamp - 300:
+                l = mid 
             else:
                 r = mid
+        self.hits = self.hits[r:]
+        return sum(x[1] for x in self.hits)
+        
+
+
+
+
+
+
+# class HitCounter:
+
+#     # space: O(N) in the case of repetitions in the same timestamp, the space required for storing those values O(1)
+    
+#     def __init__(self):
+#         self.queue = deque()
+#         # self.count = 0
+
+#     def hit(self, timestamp: int) -> None:
+#         if self.queue and timestamp == self.queue[-1][0]:
+#             self.queue[-1][1] += 1
+#         else:
+#             self.queue.append([timestamp, 1])
+#         # self.count+=1
+        
+#     # time: O(logN) with binary search, otherwise O(N) 
+#     def getHits(self, timestamp: int) -> int:
+        
+#         l = -1 
+#         r = len(self.queue)
+        
+#         # find the cut off of the past 300 sec, [r, timestamp]
+#         while l + 1 != r:
+#             mid = (l+r)//2
+            
+#             if self.queue[mid][0] <= timestamp - 300:
+#                 l = mid
+#             else:
+#                 r = mid
                 
-        return sum([self.queue[i][1] for i in range(len(self.queue)) if i >= r])
+#         return sum([self.queue[i][1] for i in range(len(self.queue)) if i >= r])
+
         
-        
-        
-        
-#         while self.queue and self.queue[0][0] <= timestamp - 300:
-#             t,c = self.queue.popleft()
-#             self.count -= c
-#         return self.count 
+# #         while self.queue and self.queue[0][0] <= timestamp - 300:
+# #             t,c = self.queue.popleft()
+# #             self.count -= c
+# #         return self.count 
             
     
 
