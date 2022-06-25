12ed455c086a55176a7af9a16d20ab1318ce0367
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-25 19:12:16 -0400

    Time: 34 ms (88.56%), Space: 14 MB (16.44%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -3,22 +3,41 @@ class HitCounter:
     # space: O(N) in the case of repetitions in the same timestamp, the space required for storing those values O(1)
     def __init__(self):
         self.queue = deque()
-        self.count = 0
+        # self.count = 0
 
     def hit(self, timestamp: int) -> None:
         if self.queue and timestamp == self.queue[-1][0]:
             self.queue[-1][1] += 1
         else:
             self.queue.append([timestamp, 1])
-        self.count+=1
+        # self.count+=1
         
     # time: O(N) 
     def getHits(self, timestamp: int) -> int:
-        while self.queue and self.queue[0][0] <= timestamp - 300:
-            t,c = self.queue.popleft()
-            self.count -= c
-        return self.count 
+        
+        l = -1 
+        r = len(self.queue)
+        
+        # find the cut off of the past 300 sec, [r, timestamp]
+        while l + 1 != r:
+            mid = (l+r)//2
             
+            if self.queue[mid][0] <= timestamp - 300:
+                l = mid
+            else:
+                r = mid
+                
+        return sum([self.queue[i][1] for i in range(len(self.queue)) if i >= r])
+        
+        
+        
+        
+#         while self.queue and self.queue[0][0] <= timestamp - 300:
+#             t,c = self.queue.popleft()
+#             self.count -= c
+#         return self.count 
+            
+    
 
             
 
