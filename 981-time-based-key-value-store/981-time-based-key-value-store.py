44db5fec4a6671577ca9d53a09fb85d89124e9cd
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-26 12:22:23 -0400

    Time: 1613 ms (6.06%), Space: 73.2 MB (5.14%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -1,17 +1,26 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = defaultdict(defaultdict)
+        self.store = defaultdict(list)
         
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key][timestamp] = value
+        self.store[key].append([timestamp, value])
         
 
     def get(self, key: str, timestamp: int) -> str:
-        while timestamp >= 1 and timestamp not in self.store[key]:
-            timestamp -= 1
-        return self.store[key][timestamp] if timestamp > 0 else ''
-            
+        lst = self.store[key]
+        
+        l = -1 
+        r = len(lst)
+        while l+1 != r:
+            mid = (l+r)//2
+            if lst[mid][0] <= timestamp:
+                l = mid
+            else:
+                r = mid
+        return lst[l][1] if l != -1 else ''
+        
+
         
 
 
@@ -19,3 +28,37 @@ class TimeMap:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class TimeMap:
+
+#     def __init__(self):
+#         self.store = defaultdict(defaultdict)
+        
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         self.store[key][timestamp] = value
+        
+
+#     def get(self, key: str, timestamp: int) -> str:
+#         while timestamp >= 1 and timestamp not in self.store[key]:
+#             timestamp -= 1
+#         return self.store[key][timestamp] if timestamp > 0 else ''
