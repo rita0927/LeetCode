7cb7aab2b043c7f37d33df0d17802dd7299c3c85
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-26 20:00:43 -0400

    Time: 204 ms (50.05%), Space: 22.1 MB (13.29%) - LeetHub

--- 616-add-bold-tag-in-string/616-add-bold-tag-in-string.py
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.py
@@ -1,34 +1,105 @@
 class Solution:
     def addBoldTag(self, s: str, words: List[str]) -> str:
+        word_key = 'isWord'
+        trie = {}
+        n =len(s)
+
+        for w in words:
+            node = trie
+            for ch in w:
+                node = node.setdefault(ch, {})
+            node[word_key] = w 
+  
+        intervals = []
+    
+        def add_interval(interval):
+            if intervals and intervals[-1][1] >= interval[0]:
+                if intervals[-1][1] < interval[1]:
+                    intervals[-1][1] = interval[1]
+            else:
+                intervals.append(interval)
+    
+        for i in range(n):
+            node = trie
+            r = 0
+            
+            for l in range(i,n):
+                if s[l] not in node:
+                    break
+                node = node[s[l]]
+                if word_key in node:
+                    r = max(r, l+1)
+            if r:
+                # add_interval([i, r])
+                # if intervals and intervals[-1][1] >= i:
+                #     if intervals[-1][1] < interval[1]:
+                #         intervals[-1][1] = interval[1]
+                # else:
+                #     intervals.append(interval)
+                if intervals and intervals[-1][1] >= i:
+                    intervals[-1][1] = max(r, intervals[-1][1])
+                else:
+                    intervals.append([i,r])
+ 
+        res = ''      
+        prev = 0
+        
+        for start, end in intervals:
+            res += s[prev:start] + '<b>' + s[start:end] + '</b>'
+            prev = end
+        return res + s[prev:]
+        
+        
+
 
         
-        """
-        a b c x y z 1 2 3
-        T T T T F F T T T 
-       <b>   </b>  <b> </b>        
-        """
-        n = len(s)
-        buckets = [False] * n
-        l = 0
         
-        for l in range(n):
-            r = 0
-            for w in words:
-                if s.startswith(w,l):
-                    r = max(r, l+len(w))
-            for i in range(l,r):
-                buckets[i] = True 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n = len(s)
+#         buckets = [False] * n
+#         l = 0
+        
+#         for l in range(n):
+#             r = 0
+#             for w in words:
+#                 if s.startswith(w,l):
+#                     r = max(r, l+len(w))
+#             for i in range(l,r):
+#                 buckets[i] = True 
             
         
-        res = ''
+#         res = ''
         
-        for i in range(n):
-            if buckets[i] and (i == 0 or i >0 and not buckets[i-1]):
-                res += '<b>'
-            res += s[i]
-            if buckets[i] and (i == n-1 or i < n - 1 and not buckets[i+1]):
-                res += '</b>'
-        return res 
+#         for i in range(n):
+#             if buckets[i] and (i == 0 or i >0 and not buckets[i-1]):
+#                 res += '<b>'
+#             res += s[i]
+#             if buckets[i] and (i == n-1 or i < n - 1 and not buckets[i+1]):
+#                 res += '</b>'
+#         return res 
                     
         
         
