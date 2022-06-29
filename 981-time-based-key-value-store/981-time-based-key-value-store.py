7d8932dccf49ae4f317b631232156b62f79aac8f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-29 15:51:30 -0400

    Time: 1575 ms (7.63%), Space: 73 MB (13.31%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.py
+++ 981-time-based-key-value-store/981-time-based-key-value-store.py
@@ -2,18 +2,21 @@ class TimeMap:
 
     def __init__(self):
         self.store = defaultdict(list)
-        
-        
+       
+               
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.store[key].append([timestamp, value])
+        
+       
        
 
     def get(self, key: str, timestamp: int) -> str:
         lst = self.store[key]
+        
         l = -1
         r = len(lst)
         
-        while l+1 != r:
+        while l + 1!= r:
             mid = (l+r)//2
             
             if lst[mid][0] <= timestamp:
@@ -21,7 +24,10 @@ class TimeMap:
             else:
                 r = mid
         return lst[l][1] if l != -1 else ''
-    
+        
+
+                
+
     
         
         
