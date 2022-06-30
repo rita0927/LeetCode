81f29c27e25b54621d15ff0ba63df4e43d49b95c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-29 22:34:42 -0400

    Time: 168 ms (56.13%), Space: 15.4 MB (86.75%) - LeetHub

--- 296-best-meeting-point/296-best-meeting-point.py
+++ 296-best-meeting-point/296-best-meeting-point.py
@@ -16,30 +16,30 @@ class Solution:
                 if grid[r][c] == 1:
                     cols.append(c)
                     
-        def minDist(lst):
-            l = 0
-            r = len(lst) - 1
-            dist = 0
-            while l < r:
-                dist+= lst[r] - lst[l]
-                l+=1
-                r-=1
-            return dist
+#         def minDist(lst):
+#             l = 0
+#             r = len(lst) - 1
+#             dist = 0
+#             while l < r:
+#                 dist+= lst[r] - lst[l]
+#                 l+=1
+#                 r-=1
+#             return dist
         
-        return minDist(rows) + minDist(cols)
+#         return minDist(rows) + minDist(cols)
                 
         
         
-#         mid_row = rows[len(rows)//2]
-#         mid_col = cols[len(cols)//2]
+        mid_row = rows[len(rows)//2]
+        mid_col = cols[len(cols)//2]
         
-#         def minDist(lst, mid):
-#             dist = 0
-#             for p in lst:
-#                 dist+= abs(p-mid)
-#             return dist
+        def minDist(lst, mid):
+            dist = 0
+            for p in lst:
+                dist+= abs(p-mid)
+            return dist
         
-#         return minDist(rows, mid_row) + minDist(cols, mid_col)
+        return minDist(rows, mid_row) + minDist(cols, mid_col)
         
         
         
