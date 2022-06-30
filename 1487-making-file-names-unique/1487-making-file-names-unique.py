cd8e0fc3eb4e48bcaeef5da4959036bf95a09a90
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-30 00:54:55 -0400

    Time: 739 ms (15.20%), Space: 28.1 MB (41.80%) - LeetHub

--- 1487-making-file-names-unique/1487-making-file-names-unique.py
+++ 1487-making-file-names-unique/1487-making-file-names-unique.py
@@ -1,20 +1,75 @@
 class Solution:
     def getFolderNames(self, names: List[str]) -> List[str]:
-        
-        dic = {}
-        
+        d = defaultdict(int)
+
         for name in names:
             
-            modified = name
-            if name in dic:
-                k = dic[name]
+            modified = name 
+            
+            if name in d:
+                c = d[name]
+                while d[modified]:
+                    
+                    modified = f'{name}({c})'
+                    c += 1  
+                d[name] = c
+            d[modified] = 1
+                     
+        return d.keys() 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dic = {}
+        
+#         for name in names:
+            
+#             modified = name
+#             if name in dic:
+#                 k = dic[name]
                 
-                while modified in dic:
-                    k+=1
-                    modified = f'{name}({k})'
-                dic[name] = k
-            dic[modified] = 0
-        return dic.keys()    
+#                 while modified in dic:
+#                     k+=1
+#                     modified = f'{name}({k})'
+#                 dic[name] = k
+#             dic[modified] = 0
+#         return dic.keys()    
             
             
             
