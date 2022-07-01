4697e759a2a7cf9e084bd8999af65dca3acadf08
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-06-30 22:58:17 -0400

    Time: 668 ms (61.16%), Space: 30.8 MB (90.48%) - LeetHub

---
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        count = list(Counter(arr).values())
+
+        heapq.heapify(count)
+
+        while k:
+            k -= heapq.heappop(count)
+
+            if k < 0:
+                return len(count) + 1
+
+        return len(count)
+            
+        
+            
+        
+        
+        
