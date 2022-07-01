3cfed5e00df20dc0e94e3772f730a2a2fd3fae22
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-01 19:51:41 -0400

    Time: 40 ms (72.32%), Space: 13.9 MB (33.05%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        res = [[1] * n] * m
+        matrix = [[1] * n for _ in range(m)]
         
-        for r in range(1, m):
+        for r in range(1,m):
             for c in range(1,n):
-                res[r][c] = res[r-1][c] + res[r][c-1]
+                matrix[r][c] = matrix[r-1][c] + matrix[r][c-1]
         
-        return res[m-1][n-1]
-        
-        
+        return matrix[m-1][n-1]
+
         
         
         
