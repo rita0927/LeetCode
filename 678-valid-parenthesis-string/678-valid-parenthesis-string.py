20f8895a56e7c01ae2e56e81b5beebd95b852f63
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-02 00:07:20 -0400

    Time: 65 ms (16.95%), Space: 13.9 MB (68.15%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,26 +1,105 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         
-        low = 0
-        high = 0
+        count = []
+        stack = []
         
-        for p in s:
+        for i,p in enumerate(s):
             if p == '(':
-                low += 1
-                high += 1
+                stack.append(i)
             elif p == ')':
-                low -= 1
-                high -= 1
-                if high  < 0 : 
-                    return False 
+                if not stack and not count:
+                    return False
+                elif not stack:
+                    count.pop()
+                else:
+                    stack.pop()
             else:
-                low -=1
-                high += 1
+                count.append(i)
+                
+        while count and stack:
+            if stack.pop() > count.pop():
+                return False
+        
+        return not stack
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#     # O(n), O(1)        
+#         # keep track of max & min count of balanced open parenthesis 
+#         # maintain a range of balanced open parenthesis 
+#         low = 0
+#         high = 0
+        
+#         for p in s:
+#             if p == '(':
+#                 low += 1
+#                 high += 1
+                
+#             # deduct the count of balanced open parenthesis
+#             elif p == ')':
+#                 low -= 1
+#                 high -= 1
+                
+#             # when p == * 
+#             else:
+                
+#                 # treat * as )
+#                 low -= 1
+#                 # treat * as (
+#                 high += 1
             
-            low = max(low, 0)
+#             # low can't be less than 0
+#             # if it's negative, reset to 0
             
-        return low == 0 
+#             low = max(low, 0)
+            
+#             # if high is zero, there're unbalanced ), which can't be balanced later 
+#             # example ))**
+            
+#             if high < 0:
+#                 return False
+            
+#         # low is always >= 0
+#         # If low > 0, balanced parenthesis is not in the range, and there're always extra (
+#         return low == 0       
 
+
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -34,27 +113,3 @@ class Solution:
 
 
 
-
-
-    
-    
-    
-#         low = 0
-#         high = 0
-        
-#         for p in s:
-#             if p == '(':
-#                 low += 1
-#                 high += 1
-#             elif p == ')':
-#                 low -= 1
-#                 high -= 1
-#             else:
-#                 low -=1
-#                 high += 1
-            
-#             low = max(low, 0)
-#             if high < 0:
-#                 return False
-            
-#         return low == 0 
