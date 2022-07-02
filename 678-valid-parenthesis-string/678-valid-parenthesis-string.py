9eade96f48e923119c55c1371ab2bae722585fba
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-01 23:49:15 -0400

    Time: 42 ms (64.52%), Space: 13.9 MB (25.58%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -11,12 +11,50 @@ class Solution:
             elif p == ')':
                 low -= 1
                 high -= 1
+                if high  < 0 : 
+                    return False 
             else:
                 low -=1
                 high += 1
             
             low = max(low, 0)
-            if high < 0:
-                return False
             
         return low == 0 
+
+    
+    
+    
+    
+    
+    
+    
+  
+
+
+
+
+
+
+
+    
+    
+    
+#         low = 0
+#         high = 0
+        
+#         for p in s:
+#             if p == '(':
+#                 low += 1
+#                 high += 1
+#             elif p == ')':
+#                 low -= 1
+#                 high -= 1
+#             else:
+#                 low -=1
+#                 high += 1
+            
+#             low = max(low, 0)
+#             if high < 0:
+#                 return False
+            
+#         return low == 0 
