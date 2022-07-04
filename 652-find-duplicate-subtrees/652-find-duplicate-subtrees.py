54cd6416179255f7575776e0e757c466415663af
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-04 03:38:48 -0400

    Time: 111 ms (42.93%), Space: 23.7 MB (42.35%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -6,7 +6,6 @@
 #         self.right = right
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        
         res = []
         seen = defaultdict(int)
         
@@ -17,7 +16,49 @@ class Solution:
             seen[path] += 1
             if seen[path] == 2:
                 res.append(node)
-            return path
+            return path 
         
         dfs(root)
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # either res or seen can be set
+#         # each node are considered different in set
+#         # seen is used to count the occurance of the same path
+#         res = []
+#         seen = defaultdict(int)
+        
+#         def dfs(node):
+#             if not node:
+#                 # None can be marked by any non-digit, including empty string
+#                 return 'N'
+#             path = f'{str(node.val)} {dfs(node.left)} {dfs(node.right)}'
+#             seen[path] += 1
+#             # can't use set because same path may be more than 2
+#             if seen[path] == 2:
+#                 res.append(node)
+#             return path
+        
+#         dfs(root)
+#         return res 
