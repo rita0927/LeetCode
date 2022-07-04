3d1d023fd4415ac548e5455119ee4664f1db6326
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-04 01:06:27 -0400

    Time: 2585 ms (5.00%), Space: 44 MB (18.80%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -8,38 +8,36 @@ class Solution:
         
         if not head or not head.next: 
             return head 
-        
-        def merge(l1,l2):
-            dummy = cur = ListNode()
-            while l1 and l2:
-                if l1.val < l2.val:
-                    cur.next = ListNode(l1.val)
-                    l1 = l1.next
-                else:
-                    cur.next = ListNode(l2.val)
-                    l2 = l2.next
-                cur = cur.next 
-            cur.next = l1 if l1 else l2
-            return dummy.next 
-        
-        def divide(l):
-            slow = l
-            fast = l.next 
-            while fast and fast.next:
-                fast = fast.next.next 
-                slow = slow.next
-            return slow
                
-        mid = divide(head)
+        mid = self.divide(head)
         l1 = head
         l2 = mid.next 
         mid.next = None 
         
-        return merge(self.sortList(l1), self.sortList(l2))
+        l1 = self.sortList(l1)
+        l2 = self.sortList(l2)
+        return self.merge(l1,l2)
         
-        
+    def merge(self,l1,l2):
+        dummy = cur = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                cur.next = ListNode(l2.val)
+                l2 = l2.next
+            cur = cur.next 
+        cur.next = l1 if l1 else l2
+        return dummy.next 
 
-        
+    def divide(self,l):
+        slow = l
+        fast = l.next 
+        while fast and fast.next:
+            fast = fast.next.next 
+            slow = slow.next
+        return slow
         
         
         
