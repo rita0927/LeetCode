ed65f4be314f4a309fc9239de8cd5faf7835e3a8
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-04 17:42:06 -0400

    Time: 57 ms (78.63%), Space: 14.3 MB (38.12%) - LeetHub

--- 281-zigzag-iterator/281-zigzag-iterator.py
+++ 281-zigzag-iterator/281-zigzag-iterator.py
@@ -1,24 +1,21 @@
 class ZigzagIterator:
+    
     def __init__(self, v1: List[int], v2: List[int]):
         self.lst = [v1,v2]
         self.queue = deque()
-
-        for index, v in enumerate(self.lst):
-            # only insert vectors with element, not empty vector
+        for i,v in enumerate(self.lst):
             if v:
-                # append the vector index in the self.lst and the element index in the vector
-                self.queue.append((index, 0))
-
+                self.queue.append((i,0))
+        
     def next(self) -> int:
-        if self.queue:
-            v_idx, el_idx = self.queue.popleft()
-            next_el_idx = el_idx + 1
-            if  next_el_idx < len(self.lst[v_idx]):
-                self.queue.append((v_idx, next_el_idx))
-            return self.lst[v_idx][el_idx]
+        v_idx, e_idx = self.queue.popleft()
+        if e_idx + 1 < len(self.lst[v_idx]):
+            self.queue.append((v_idx, e_idx+1))
+        return self.lst[v_idx][e_idx]
 
     def hasNext(self) -> bool:
         return self.queue
+    
        
         
         
@@ -33,6 +30,36 @@ class ZigzagIterator:
 # i, v = ZigzagIterator(v1, v2), []
 # while i.hasNext(): v.append(i.next())
 
+
+
+
+# class ZigzagIterator:
+    
+#     # space O(k) where k is the number of input vectors (assume more than two vectors)
+#     def __init__(self, v1: List[int], v2: List[int]):
+        
+#         self.lst = [v1,v2]
+#         self.queue = deque()
+
+#         for index, v in enumerate(self.lst):
+#             # only insert vectors with element, not empty vector
+#             if v:
+#                 # append the vector index in the self.lst and the element index in the vector
+#                 self.queue.append((index, 0))
+
+#     # time: O(1) 
+#     def next(self) -> int:
+#         if self.queue:
+#             v_idx, el_idx = self.queue.popleft()
+#             next_el_idx = el_idx + 1
+#             # append the vector index and next element index back to queue if the next element exists 
+#             if  next_el_idx < len(self.lst[v_idx]):
+#                 self.queue.append((v_idx, next_el_idx))
+#             return self.lst[v_idx][el_idx]
+
+#     def hasNext(self) -> bool:
+#         return self.queue
+       
 
 
 
