5391364d2fc08f3c624a93b31c101ec183bf1945
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-04 17:31:51 -0400

    Time: 91 ms (23.25%), Space: 14.2 MB (73.85%) - LeetHub

--- 281-zigzag-iterator/281-zigzag-iterator.py
+++ 281-zigzag-iterator/281-zigzag-iterator.py
@@ -1,29 +1,77 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
-        self.v1 = v1
-        self.v2 = v2
-        self.i1 = 0 
-        self.i2 = 0
-        self.next_is_v1 = True
-        
+        self.lst = [v1,v2]
+        self.queue = deque()
+
+        for index, v in enumerate(self.lst):
+            # only insert vectors with element, not empty vector
+            if v:
+                # append the vector index in the self.lst and the element index in the vector
+                self.queue.append((index, 0))
+
     def next(self) -> int:
-        if self.next_is_v1 and self.i1 < len(self.v1):
-            res = self.v1[self.i1]
-            self.i1 += 1
-            if self.i2 < len(self.v2):
-                self.next_is_v1 = False
-            return res 
-        else:
-            res = self.v2[self.i2]
-            self.i2 += 1
-            if self.i1 < len(self.v1):
-                self.next_is_v1 = True 
-            return res    
+        if self.queue:
+            v_idx, el_idx = self.queue.popleft()
+            next_el_idx = el_idx + 1
+            if  next_el_idx < len(self.lst[v_idx]):
+                self.queue.append((v_idx, next_el_idx))
+            return self.lst[v_idx][el_idx]
 
     def hasNext(self) -> bool:
-        return self.i1 < len(self.v1) or self.i2 < len(self.v2)
+        return self.queue
+       
+        
+        
+        
+        
+        
+        
+        
         
 
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
 # while i.hasNext(): v.append(i.next())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class ZigzagIterator:
+#     def __init__(self, v1: List[int], v2: List[int]):
+#         self.v1 = v1
+#         self.v2 = v2
+#         self.i1 = 0 
+#         self.i2 = 0
+#         self.next_is_v1 = True
+    
+    
+#     # O(1), O(1) for two input vectors 
+#     def next(self) -> int:
+#         if self.next_is_v1 and self.i1 < len(self.v1):
+#             res = self.v1[self.i1]
+#             self.i1 += 1
+#             if self.i2 < len(self.v2):
+#                 self.next_is_v1 = False
+#             return res 
+#         else:
+#             res = self.v2[self.i2]
+#             self.i2 += 1
+#             if self.i1 < len(self.v1):
+#                 self.next_is_v1 = True 
+#             return res    
+
+#     def hasNext(self) -> bool:
+#         return self.i1 < len(self.v1) or self.i2 < len(self.v2)
