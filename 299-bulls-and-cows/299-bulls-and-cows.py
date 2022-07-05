275acc8e21aeb111783ad7b37295c9185908c4a9
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-04 20:23:40 -0400

    Time: 101 ms (8.98%), Space: 13.9 MB (30.98%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,28 +1,72 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        bulls = 0
-        cows = 0
+        bull = 0
+        cow = 0
         d = defaultdict(int)
-        
         for i in range(len(secret)):
             s = secret[i]
             g = guess[i]
-            
             if s == g:
-                bulls += 1
+                bull += 1
             else:
-                # int(True) == 1, int(False) == 0
-                # if d[s] < 0, cows += 1; if d[g] > 0: cows += 1
-                
-                cows += int(d[s] < 0) + int(d[g] > 0)
-                
-                # if d[s] < 0:
-                #     cows += 1
-                # if d[g] > 0:
-                #     cows += 1
+                cow += int(d[s] < 0) + int(d[g] > 0)
                 d[s] += 1
                 d[g] -= 1
-        return f'{bulls}A{cows}B'
+        return f'{bull}A{cow}B'
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         bulls = 0
+#         cows = 0
+#         d = defaultdict(int)
+        
+#         for i in range(len(secret)):
+#             s = secret[i]
+#             g = guess[i]
+            
+#             if s == g:
+#                 bulls += 1
+#             else:
+#                 # int(True) == 1, int(False) == 0
+#                 # if d[s] < 0, cows += 1; if d[g] > 0: cows += 1
+                
+#                 cows += int(d[s] < 0) + int(d[g] > 0)
+                
+#                 # if d[s] < 0:
+#                 #     cows += 1
+#                 # if d[g] > 0:
+#                 #     cows += 1
+#                 d[s] += 1
+#                 d[g] -= 1
+#         return f'{bulls}A{cows}B'
                 
             
             
