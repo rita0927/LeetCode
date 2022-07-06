7392d9379b519310b7223f76a579368c9d01ee0d
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-06 03:28:28 -0400

    Time: 43 ms (57.42%), Space: 14 MB (24.52%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -1,26 +1,32 @@
 class MyStack:
 
     def __init__(self):
-        self.queue = deque()
+        self.q1 = deque()
+        self.q2 = deque()
+        self.top = None 
         
 
     def push(self, x: int) -> None:
-        self.queue.append(x)
-        n = len(self.queue) 
-        while n > 1:
-            self.queue.append(self.queue.popleft())
-            n -= 1
+        self.q1.append(x)
+        self.top = x
+
 
     def pop(self) -> int:
-        return self.queue.popleft()
-        
+
+        while len(q1)> 1:
+            self.top = self.q1.popleft()
+            self.q2.append(self.top)
+            
+        return self.q1.popleft()
+        self.q1, self.q2 = self.q2, self.q1
 
     def top(self) -> int:
-        return self.queue[0]
+        return self.top
         
 
     def empty(self) -> bool:
-        return not self.queue
+        return not self.q1
+        
 
 
 
@@ -34,3 +40,33 @@ class MyStack:
 # param_2 = obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.empty()
+
+
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+        
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        
+        # rotate with positive int (default is 1): move from right to left
+        self.queue.rotate(1)
+        
+        # n = len(self.queue) 
+        # while n > 1:
+        #     self.queue.append(self.queue.popleft())
+        #     n -= 1
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+        
+
+    def top(self) -> int:
+        return self.queue[0]
+        
+
+    def empty(self) -> bool:
+        return not self.queue
