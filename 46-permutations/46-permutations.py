bf247b1c258bda677bd05ef401870d66cc204f45
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-08 19:26:28 -0400

    Time: 45 ms (85.74%), Space: 13.9 MB (97.51%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -2,10 +2,9 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
         res = []
-        # base case
+        
         if len(nums) == 1:
             return [nums.copy()]
-            return [nums[:]]
         
         for _ in range(len(nums)):
             n = nums.pop(0)
@@ -13,27 +12,77 @@ class Solution:
             
             for perm in perms:
                 perm.append(n)
-            
+                
             res.extend(perms)
             nums.append(n)
+        return res 
             
-        return res
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+#         # base case
+#         if len(nums) == 1:
+#             return [nums.copy()]
+#             return [nums[:]]
+        
+#         for _ in range(len(nums)):
+#             n = nums.pop(0)
+#             perms = self.permute(nums)
+            
+#             for perm in perms:
+#                 perm.append(n)
+            
+#             res.extend(perms)
+#             nums.append(n)
+            
+#         return res
             
         
             
     
     
-             
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
         
         
