aad17c9247041814164b23900b234d1bb41e7c93
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-16 13:19:09 -0400

    Time: 55 ms (49.95%), Space: 14.4 MB (12.27%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -2,25 +2,100 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
         res = []
+        left = 0
+        right = 0
         
-        def backtrack(path,l,r):
+        def backtrack(temp):
+            nonlocal left
+            nonlocal right
             
-            if l+r == 2 *n:
-                res.append(''.join(path))
+            if left == n and right == n:
+                res.append(('').join(temp))
                 return
             
-            if l < n:
-                path.append('(')
-                backtrack(path,l+1,r)
-                path.pop()
+            if left < n:
+                left += 1
+                temp.append('(')
+                backtrack(temp)
+                temp.pop()
+                left -= 1
+            
+            if right < left:
+                right += 1
+                temp.append(')')
+                backtrack(temp)
+                temp.pop()
+                right -= 1
                 
-            if r < l:
-                path.append(')')
-                backtrack(path, l, r+1)
-                path.pop()
-        
-        backtrack([],0,0)
+        backtrack([])
         return res 
+            
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+#        # O(4^n / n^0.5), O(4^n / n^0.5)
+        
+#         res = []
+        
+#         def backtrack(path,l,r):
+            
+#             if l+r == 2 *n:
+#                 res.append(''.join(path))
+#                 return
+            
+#             if l < n:
+#                 path.append('(')
+#                 backtrack(path,l+1,r)
+#                 path.pop()
+                
+#             if r < l:
+#                 path.append(')')
+#                 backtrack(path, l, r+1)
+#                 path.pop()
+        
+#         backtrack([],0,0)
+#         return res 
         
         
         
