b8baa3cf7cfec0ac2ef3d19fbde74bba2ab298d0
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-17 15:04:20 -0400

    Time: 634 ms (14.62%), Space: 15.6 MB (44.38%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,31 +1,32 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        
         l = -1
         r = len(arr)
         
-        while l + 1 != r:
+        while l+1 != r:
             mid = (l+r)//2
+            
             if x >= arr[mid]:
-                l = mid
+                l = mid 
             else:
                 r = mid
         
         while r-l-1 < k:
             if l == -1:
                 r += 1
-                continue
+                continue 
             if r == len(arr):
                 l -= 1
                 continue
-            
+                
             if abs(arr[l] - x) <= abs(arr[r] - x):
                 l -= 1
             else:
                 r += 1
-        
+                
         return arr[l+1:r]
         
+
         
         
         
@@ -144,7 +145,7 @@ class Solution:
             
 #             # reach the right boundary ( or x > arr[-1])
 #             if r == len(arr):
-#                 l += 1
+#                 l -= 1
 #                 continue 
             
 #             # based on the question, |a - x| <= |b - x| is considered closer 
