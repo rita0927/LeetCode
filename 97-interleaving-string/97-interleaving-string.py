11bbd9b3742288aecf8732c23831b40badf6f5ad
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-17 17:30:16 -0400

    Time: 68 ms (51.93%), Space: 14.3 MB (51.29%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -10,24 +10,27 @@ class Solution:
         if A + B != C:
             return False 
         
-        @lru_cache 
-        
+        @lru_cache
         def helper(i1, i2, i3):
-            if i3 == len(s3):
+            if i3 == C:
                 return True 
             
             p1 = False
-            p2 = False 
+            p2 = False
             if i1 < A and s1[i1] == s3[i3]:
                 p1 = helper(i1+1, i2, i3+1)
+            
             if i2 < B and s2[i2] == s3[i3]:
                 p2 = helper(i1, i2+1, i3+1)
-            
-            return p1 or p2 
-        
-        return helper(0,0,0)
                 
+            return p1 or p2 
+        return helper(0,0,0)
         
+        
+        
+        
+        
+
         
         
         
