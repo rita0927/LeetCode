1092124ae605cdb0ff9a7e7033f2561a05d90534
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-17 18:18:48 -0400

    Time: 492 ms (46.68%), Space: 16.3 MB (80.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,30 +4,26 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         dir = [[-1,0], [1,0], [0,-1], [0,1]]
-        res = 0
-        visited = set()
+        count = 0
         
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col] == '1' and (row, col) not in visited:
-                    visited.add((row, col))
-                    res += 1
-                    queue = deque([(row, col)])
-                    
-                    while queue:
-                        r,c = queue.popleft()
-                        
-                        for x,y in dir:
-                            nr = r + x
-                            nc = c + y
-                            
-                            if 0 <= nr < m and 0 <= nc < n and (nr,nc) not in visited and grid[nr][nc] == '1':
-                                visited.add((nr,nc))
-                                queue.append((nr,nc))
-        return res 
-
+        def dfs(r,c):
+            grid[r][c] = '0'
+            
+            for x,y in dir:
+                nr = x + r
+                nc = y + c
                 
+                if 0<= nr < m and 0<= nc < n and grid[nr][nc] == '1':
+                    dfs(nr,nc)
+            
         
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == '1':
+                    count += 1
+                    dfs(r,c)
+        return count 
+
         
         
 
