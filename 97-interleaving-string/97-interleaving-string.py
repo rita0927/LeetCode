dd5b1c514147161162019a762693bb21fe06d035
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-16 21:37:41 -0400

    Time: 57 ms (72.31%), Space: 14.2 MB (59.56%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -2,25 +2,75 @@ from functools import *
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        
+        A = len(s1)
+        B = len(s2)
+        C = len(s3)
+        
+        if A + B != C:
             return False 
         
-        @lru_cache
-        def helper(i1,i2,i3):
-            if i3 == len((s3)):
-                return True
+        @lru_cache 
+        
+        def helper(i1, i2, i3):
+            if i3 == len(s3):
+                return True 
             
-            path1 = False
-            path2 = False
-            if i1 < len(s1) and s1[i1] == s3[i3]:
-                path1 = helper(i1 + 1, i2, i3+1)
-
-            if i2 < len(s2) and s2[i2] == s3[i3]:
-                path2 = helper(i1, i2+1, i3+1)
+            p1 = False
+            p2 = False 
+            if i1 < A and s1[i1] == s3[i3]:
+                p1 = helper(i1+1, i2, i3+1)
+            if i2 < B and s2[i2] == s3[i3]:
+                p2 = helper(i1, i2+1, i3+1)
             
-            return path1 or path2
+            return p1 or p2 
         
         return helper(0,0,0)
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(s1) + len(s2) != len(s3):
+#             return False 
+        
+#         @lru_cache
+#         def helper(i1,i2,i3):
+#             if i3 == len((s3)):
+#                 return True
+            
+#             path1 = False
+#             path2 = False
+#             if i1 < len(s1) and s1[i1] == s3[i3]:
+#                 path1 = helper(i1 + 1, i2, i3+1)
+
+#             if i2 < len(s2) and s2[i2] == s3[i3]:
+#                 path2 = helper(i1, i2+1, i3+1)
+            
+#             return path1 or path2
+        
+#         return helper(0,0,0)
         
         
         
