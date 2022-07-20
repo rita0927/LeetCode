6b670198b98e5215ff4ee1647a5cc05d50be050f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-20 01:43:28 -0400

    Time: 63 ms (16.51%), Space: 13.8 MB (97.78%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -6,8 +6,16 @@ class Solution:
         for r in range(1,m):
             for c in range(1,n):
                 dp[r][c] = dp[r-1][c] + dp[r][c-1]
-        return dp[m-1][n-1]
         
+        return dp[m-1][n-1]
+                
+            
+        
+        
+        
+        
+        
+
 
 
     
