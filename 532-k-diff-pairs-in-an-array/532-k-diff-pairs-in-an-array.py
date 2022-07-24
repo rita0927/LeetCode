b161c42bcbabce76669dc746340121d3a6546aba
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-23 22:02:56 -0400

    Time: 76 ms (96.39%), Space: 15.6 MB (56.64%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -8,9 +8,48 @@ class Solution:
             if k > 0 and n+k in count:
                 res += 1
             elif k == 0 and count[n] > 1:
-                res +=1
-        
+                res += 1
         return res 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = 0
+#         count = Counter(nums)
+        
+#         for n in count:
+#             if k > 0 and n+k in count:
+#                 res += 1
+#             elif k == 0 and count[n] > 1:
+#                 res +=1
+        
+#         return res 
             
         
         
