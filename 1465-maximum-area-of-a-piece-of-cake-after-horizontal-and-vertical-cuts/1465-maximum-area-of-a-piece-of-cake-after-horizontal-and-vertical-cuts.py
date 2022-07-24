899d6770db2a8d46c0d8d9008c0ad5b8d1cc9657
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-24 00:29:19 -0400

    Time: 532 ms (36.82%), Space: 26.8 MB (78.60%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,19 +1,73 @@
 class Solution:
     def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
-        mod = 10 **9 + 7 
+        
+        
+        mod = 10 **9 + 7
         horizontalCuts.extend([0,h])
         verticalCuts.extend([0,w])
         horizontalCuts.sort()
         verticalCuts.sort()
         
-        
-        def getMaxDiff(arr):
+        def getMaxDiff(lst):
             diff = 0
-            for i in range(len(arr)-1):
-                diff = max(diff, arr[i+1] - arr[i])
+            for i in range(1, len(lst)):
+                diff = max(diff, lst[i] - lst[i-1])
             return diff
         
-        return (getMaxDiff(horizontalCuts) * getMaxDiff(verticalCuts)) % mod
+        return (getMaxDiff(horizontalCuts) * getMaxDiff(verticalCuts)) %mod 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         mod = 10 **9 + 7 
+#         horizontalCuts.extend([0,h])
+#         verticalCuts.extend([0,w])
+#         horizontalCuts.sort()
+#         verticalCuts.sort()
+        
+        
+#         def getMaxDiff(arr):
+#             diff = 0
+#             for i in range(len(arr)-1):
+#                 diff = max(diff, arr[i+1] - arr[i])
+#             return diff
+        
+#         return (getMaxDiff(horizontalCuts) * getMaxDiff(verticalCuts)) % mod
 
         
         
