5921b7ac53a227b82e813c943d60a5d8b312696e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-24 22:15:31 -0400

    Time: 1507 ms (97.15%), Space: 14.1 MB (12.30%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -4,23 +4,38 @@ class Solution:
         m = len(board)
         n = len(board[0])
         dir = [[-1,0], [1,0], [0,-1], [0,1]]
+        visited = set()
+        
+        if len(word) > m* n:
+            return False
+        
+        count = Counter(word)
+        d = defaultdict(int)
+        for r in range(m):
+            for c in range(n):
+                d[board[r][c]] += 1
+        
+        for ch in count:
+            if ch not in d or count[ch] > d[ch]:
+                return False 
+               
         
         def backtrack(r,c,i):
             if i == len(word):
                 return True 
             
-            if r <0 or r >= m or c < 0 or c >= n or board[r][c] != word[i]:
+            if r <0 or r >= m or c < 0 or c >= n or board[r][c] != word[i] or (r,c) in visited:
                 return False
         
-            ch = board[r][c]
-            board[r][c] = '#'
+            visited.add((r,c))
             
             for x,y in dir:
                 nr = r + x
                 nc = c + y
                 if backtrack(nr,nc,i+1):
                     return True 
-            board[r][c] = ch
+            visited.remove((r,c))
+            return False 
             
         
         for r in range(m):
