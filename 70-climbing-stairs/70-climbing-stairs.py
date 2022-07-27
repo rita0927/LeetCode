2464c301f7af2d540d1156e05c6aed35d14036a4
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-27 18:56:05 -0400

    Time: 38 ms (73.25%), Space: 13.8 MB (56.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,15 +1,43 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        
-        dp = [0] * (n+1)
-        dp[0] = 1
-        dp[1] = 1
-        
+        two = 1
+        one = 1
+        cur = 1
         for i in range(2,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
+            cur = two + one 
+            two = one
+            one = cur 
         
-        return dp[n]
+        return cur 
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(n), O(n)
+#         dp = [0] * (n+1)
+#         dp[0] = 1
+#         dp[1] = 1
+        
+#         for i in range(2,n+1):
+#             dp[i] = dp[i-1]+dp[i-2]
+        
+#         return dp[n]
         
         
         
