d3928de2014bad942b4904edd27595990f162aa8
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-07-26 21:56:47 -0400

    Time: 876 ms (17.83%), Space: 20.3 MB (35.66%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.py
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.py
@@ -1,30 +1,95 @@
 class Solution:
     def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        
         m = len(heightMap)
         n = len(heightMap[0])
-        dir = [[-1,0],[1,0],[0,-1], [0,1]]
+        dir = [[-1,0],[1,0],[0,-1],[0,1]]
+        visited = set()
         heap = []
         res = 0
-        visited = set()
-        
-        
         for r in range(m):
             for c in range(n):
-                if r == 0 or r  == m - 1 or c == 0 or c == n-1:
-                    heappush(heap, [heightMap[r][c], r,c])
+                if r == 0 or r == m - 1 or c == 0 or c == n-1:
+                    heappush(heap,[heightMap[r][c], r,c])
                     visited.add((r,c))
-
         while heap:
-            h,r,c = heappop(heap)
+            h,r,c= heappop(heap)
+            
             for x,y in dir:
-                nr = r + x 
+                nr = r + x
                 nc = c + y
                 
-                if 0<= nr < m and 0 <= nc <n and (nr,nc) not in visited:
+                if 0<=nr<m and 0<=nc<n and (nr,nc) not in visited:
                     visited.add((nr,nc))
-                    heappush(heap,[max(h,heightMap[nr][nc]), nr,nc])
                     res += max(0, h-heightMap[nr][nc])
-        return res
+                    heappush(heap, [max(h, heightMap[nr][nc]), nr,nc])
+        return res 
+                    
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # O(mnlog(mn)), every cell is pushed and popped from the heap
+#         # O(mn)
+#         m = len(heightMap)
+#         n = len(heightMap[0])
+#         dir = [[-1,0],[1,0],[0,-1], [0,1]]
+#         heap = []
+#         res = 0
+#         visited = set()
+        
+#         # edges can't store water
+#         for r in range(m):
+#             for c in range(n):
+#                 if r == 0 or r  == m - 1 or c == 0 or c == n-1:
+#                     heappush(heap, [heightMap[r][c], r,c])
+#                     visited.add((r,c))
+
+#         while heap:
+#             # assume the edges construct a bucket, the shortted edge matters the most 
+#             # pop out the min_hight from the current edges
+#             # visit all cells inside, replace the shorter edges with taller inner cell
+#             h,r,c = heappop(heap)
+#             for x,y in dir:
+#                 nr = r + x 
+#                 nc = c + y
+                
+#                 if 0<= nr < m and 0 <= nc <n and (nr,nc) not in visited:
+#                     visited.add((nr,nc))
+                    
+#                     # if neighbor is taller, form a new edge to replace the previous edge
+#                     # if neighbor is shorter, store water in this neighbor cell 
+#                     heappush(heap,[max(h,heightMap[nr][nc]), nr,nc])
+#                     res += max(0, h-heightMap[nr][nc])
+#         return res
                     
         
                 
