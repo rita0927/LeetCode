2842d498f1bba37830c2b4be5902d5902f7e458e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-01 22:39:57 -0400

    Time: 173 ms (43.73%), Space: 15.5 MB (88.87%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -2,39 +2,30 @@ class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
         adj = defaultdict(list)
+        indegree = defaultdict(int)
         
         for crs, pre in prerequisites:
-            adj[crs].append(pre)
+            adj[pre].append(crs)
+            indegree[crs]+=1
         
-        visited = set()
-        cycle =set()
-        res = []
-        
-        def dfs(crs):
-            
-            if crs in visited:
-                return True 
-            
-            if crs in cycle:
-                return False
-            
-            cycle.add(crs)
-            for pre in adj[crs]:
-                if not dfs(pre):
-                    return False
-            cycle.remove(crs)
-            visited.add(crs)
-            res.append(crs)
-            return True 
-            
-            
-        
-        
+        queue = deque()
         for crs in range(numCourses):
-            if not dfs(crs):
-                return [] 
-        return res  
+            if not indegree[crs]:
+                queue.append(crs)
+        
+        res = []
+        while queue:
+            pre = queue.popleft()
+            res.append(pre)
             
+            for crs in adj[pre]:
+                indegree[crs] -= 1
+                if not indegree[crs]:
+                    queue.append(crs)
+                    
+            
+        return res if len(res) == numCourses else []
+
 
                 
                 
