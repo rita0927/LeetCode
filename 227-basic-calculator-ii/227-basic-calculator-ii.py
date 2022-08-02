e54e5c9f3ccf77c21f6b48673103531c7b5a479c
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-02 01:54:06 -0400

    Time: 114 ms (72.44%), Space: 15.3 MB (84.04%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,35 +1,95 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        num = 0
-        operators = {'+', '-', '*', '/'}
-        # keep track of previous operator
+        
+        if not s:
+            return 0
+        
+        cur = 0
+        prev = 0
+        res = 0
+        operators = '+-*/'
         operator = '+'
         
-        for i in range(len(s)):
-            l = s[i]
-            
-            if l.isdigit():
-                num = num * 10 + int(l)
-            
-            if l in operators or i == len(s) - 1:
-                if operator == '+':
-                    stack.append(num)
-                if operator == '-':
-                    stack.append(-num)
-                if operator == '*':
-                    num *= stack.pop()
-                    stack.append(num)
-                if operator == '/':
-                    pre = stack.pop()
-                    if pre > 0:
-                        stack.append(pre//num)
-                    else:
-                        stack.append(-(-pre//num))      
+        for i, ch in enumerate(s):
+            if ch.isdigit():
+                cur = cur * 10 + int(ch)
                 
-                operator = l
-                num = 0
-        return sum(stack)
+            if ch in operators or i == len(s)-1:
+                if operator in '+-':
+                    res += prev 
+                    prev = cur if operator == '+' else -cur
+                elif operator == '*':
+                    prev *= cur
+                elif operator == '/':
+                    if prev > 0:
+                        prev //= cur
+                    else:
+                        prev= -(-prev//cur)
+                operator = ch 
+                cur = 0
+        
+        res += prev 
+        return res 
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         stack = []
+#         num = 0
+#         operators = {'+', '-', '*', '/'}
+#         # keep track of previous operator
+#         operator = '+'
+        
+#         for i in range(len(s)):
+#             l = s[i]
+            
+#             if l.isdigit():
+#                 num = num * 10 + int(l)
+            
+#             if l in operators or i == len(s) - 1:
+#                 if operator == '+':
+#                     stack.append(num)
+#                 if operator == '-':
+#                     stack.append(-num)
+#                 if operator == '*':
+#                     num *= stack.pop()
+#                     stack.append(num)
+#                 if operator == '/':
+#                     pre = stack.pop()
+#                     if pre > 0:
+#                         stack.append(pre//num)
+#                     else:
+#                         stack.append(-(-pre//num))      
+                
+#                 operator = l
+#                 num = 0
+#         return sum(stack)
         
 
 
