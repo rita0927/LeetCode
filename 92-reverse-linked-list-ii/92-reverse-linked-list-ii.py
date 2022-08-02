a74b993bedac2f9c2d84c403c81a4c42c23a7e0d
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-01 21:29:14 -0400

    Time: 45 ms (64.20%), Space: 14.1 MB (51.16%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -9,16 +9,23 @@ class Solution:
         if left == right:
             return head 
         
+        # dummy.next refers to the head 
         dummy = prev = ListNode(0, head)
+        # position starting from 1
         i = 1
         
+        # find the node before left, mark with prev 
         while i < left:
             prev = prev.next 
             i += 1
         
+        # prev is the node before the left, prev.next -> left (starting reverse)
         cur = prev.next 
         next = cur.next 
         
+        # when i == right: 
+        #    cur -> right, need to connect with prev (node before left)
+        #    next -> node after right, need to connect with left
         while i < right:
             temp = next.next 
             next.next = cur
@@ -26,9 +33,12 @@ class Solution:
             next = temp
             i += 1
         
+        # left (initial prev.next) connects to the node after right 
         prev.next.next = next 
+        # prev connects to the right (ending cur)
         prev.next = cur 
         
+        # dummy refers to the node before head 
         return dummy.next 
         
 
