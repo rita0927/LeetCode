ffc507f71d07a065f44842fd8c725c2fecd11c35
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-02 01:15:46 -0400

    Time: 124 ms (29.02%), Space: 18.3 MB (78.65%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -10,21 +10,93 @@ class Node:
 
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p_node = p
-        q_node = q
         
-        while p_node != q_node:
-            if p_node:
-                p_node = p_node.parent
+        n1 = p 
+        n2 = q
+        
+        while n1 != n2:
+            if n1:
+                n1 = n1.parent
             else:
-                p_node = q
+                n1 = q
             
-            if q_node:
-                q_node = q_node.parent
+            if n2:
+                n2 = n2.parent
             else:
-                q_node = p
+                n2 = p
+        return n1 
         
-        return p_node 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         n1 = p 
+#         n2 = q
+        
+#         while n1 != n2:
+#             if n1:
+#                 n1 = n1.parent
+#             else:
+#                 n1 = q
+            
+#             if n2:
+#                 n2 = n2.parent
+#             else:
+#                 n2 = p
+#         return n1 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         #O(m+n), O(1)
+#         p_node = p
+#         q_node = q
+        
+#         while p_node != q_node:
+#             if p_node:
+#                 p_node = p_node.parent
+#             else:
+#                 p_node = q
+            
+#             if q_node:
+#                 q_node = q_node.parent
+#             else:
+#                 q_node = p
+        
+#         return p_node 
         
         
         
