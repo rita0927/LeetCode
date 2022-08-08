1b1db46331fbe71a9ec9953dc5c33a2c1e32c045
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-08 10:59:09 -0400

    Time: 131 ms (75.05%), Space: 14.7 MB (83.32%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,81 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
         dictS = defaultdict(int)
         dictT = Counter(t)
         l = 0
-        count = 0
         res = ''
+        count = 0
         
         for r, ch in enumerate(s):
-            
             if dictT[ch]:
                 dictS[ch] += 1
                 
                 if dictT[ch] >= dictS[ch]:
                     count += 1
-            
+                    
             if count == len(t):
                 while dictT[s[l]] < dictS[s[l]] or s[l] not in dictT:
                     if dictT[s[l]] < dictS[s[l]]:
                         dictS[s[l]] -= 1
                     l += 1
-                
                 if not res or len(res) > r-l+1:
                     res = s[l:r+1]
+                    
         return res 
-                
-
-                
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         dict_t = Counter(t)
-#         res = ''
-#         l = 0 
-#         count = 0
-#         dict_s = defaultdict(int)
-        
-#         for r, ch in enumerate(s):
-#             if dict_t[ch]:
-#                 dict_s[ch] += 1
-#                 if dict_t[ch] >= dict_s[ch]:
-#                     count += 1
-            
-#             if count == len(t):
-#                 while dict_t[s[l]] < dict_s[s[l]] or s[l] not in dict_t:
-#                     if dict_t[s[l]] < dict_s[s[l]]:
-#                         dict_s[s[l]] -= 1       
-#                     l += 1
-                
-#                 if not res or len(res) > r-l+1:
-#                     res = s[l:r+1]
-#         return res 
-                
         
         
         
