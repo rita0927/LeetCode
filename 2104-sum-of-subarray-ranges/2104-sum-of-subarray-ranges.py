8dddcd124719cc5a17e6fa7ab32d4502a424705e
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-09 22:44:05 -0400

    Time: 5805 ms (32.05%), Space: 14.1 MB (45.55%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,24 +1,20 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         res = 0
-
-        for l in range(len(nums)):
-            min_val = float('inf')
-            max_val = float('-inf')
         
-            for r in range(l, len(nums)):
-                min_val = min(min_val, nums[r])
-                max_val = max(max_val, nums[r])
+        for i in range(len(nums)):
+            max_val = nums[i]
+            min_val = nums[i]
+            
+            for j in range(i,len(nums)):
+                max_val = max(max_val, nums[j])
+                min_val = min(min_val, nums[j])
                 res += (max_val - min_val)
+        
         return res 
-                
-                
         
         
-        
-        
-        
-        
+
         
         
         
