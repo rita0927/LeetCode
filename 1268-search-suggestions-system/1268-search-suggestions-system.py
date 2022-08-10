32e369019e4c560bfdb9b8d8569f8d53a408a4cb
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-10 01:07:53 -0400

    Time: 141 ms (67.27%), Space: 17 MB (98.48%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -3,16 +3,100 @@ class Solution:
         
         res = []
         products.sort()
+        pos = 0
+        prefix = ''
         
-        for i, ch in enumerate(searchWord):
-            temp = []
-            for p in products:
-                if i < len(p) and p[i] == ch:
-                    temp.append(p)
-            products = temp
-            res.append(temp[:3])
+        for ch in searchWord:
+            prefix += ch
+            pos = bisect_left(products, prefix, pos)
+            res.append([p for p in products[pos:pos+3] if p.startswith(prefix)])
         
         return res 
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# # Time - O(nlogn + mlogn), O(nlogn) for sorting, m is the length of searchWord, O(logn) is for binary search of the products for a prefix
+# # Space - O(1)
+        
+#         products.sort()
+#         res = []
+#         pos = 0
+#         prefix = ''
+        
+#         for ch in searchWord:
+#             prefix += ch 
+#             pos = bisect_left(products, prefix, pos)
+#             res.append([p for p in products[pos: pos+3] if p.startswith(prefix)])
+        
+#         return res 
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# Time - O(nlogn + n*m), O(nlogn) for sorting 
+# Space - O(N)         
+        
+        
+#         res = []
+#         # sort product in lexicographical order 
+#         products.sort()
+        
+#         for i, ch in enumerate(searchWord):
+#             temp = []
+#             for p in products:
+#                 if i < len(p) and p[i] == ch:
+#                     temp.append(p)
+                    
+#             # update products to the words match searchWord[:i+1]
+#             products = temp
+#             # return up to 3 products for each prefix  
+#             res.append(temp[:3])
+        
+#         return res 
         
         
         
