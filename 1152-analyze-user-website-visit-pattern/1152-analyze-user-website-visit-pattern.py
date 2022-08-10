70e33551f60af5a66f0f20ca59bcf384676ca21f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-08-09 22:50:14 -0400

    Time: 61 ms (84.85%), Space: 14.6 MB (54.67%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.py
@@ -5,65 +5,11 @@ class Solution:
         
         for user, time, site in sorted(zip(username, timestamp, website), key = lambda x: x[1]):
             history[user].append(site)
-        
+            
         patterns = Counter()
         
         for user, sites in history.items():
             patterns.update(Counter(set(combinations(sites, 3))))
-        print(patterns)
+        
         return max(sorted(patterns), key = patterns.get)
-    
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         # {user: list of sites}
-#         history = defaultdict(list)
-        
-#         # create tuple of (user, time, site) using zip
-#         # sort by lambda: user (group by user), time (time is not sorted)
-#         for user, time, site in sorted(zip(username, timestamp, website), key = lambda x: (x[0], x[1])):
-#             history[user].append(site)
-        
-#         patterns = Counter()
-        
-#         for user, sites in history.items():
-#             # itertools.combinations generate combinations (maintain the element order)
-#             # remove duplicate of combinations using set
-#             # count the pattern for each user 
-#             # Counter.update will update the val on each key 
-#             patterns.update(Counter(set(combinations(sites, 3))))
-        
-#         # sort partterns keys in lexicographically order
-#         # take the max with the key of patterns.get
-#         return max(sorted(patterns), key = patterns.get)
-        
-        
         
