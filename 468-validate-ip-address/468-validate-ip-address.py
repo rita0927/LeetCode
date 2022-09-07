99c3d37943fe065bd8ce3937be9032eb9e8fd01b
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-09-06 23:59:29 -0400

    Time: 68 ms (5.03%), Space: 13.9 MB (37.13%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.py
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -6,21 +6,63 @@ class Solution:
         
         if len(ip4Arr) == 4:
             for x in ip4Arr:
-                if not (x.isdigit() and (x=='0'or x == x.lstrip('0')) and 0 <=int(x)<=255):
+                if not (x.isdigit() and (x=='0' or x==x.lstrip('0')) and 0<=int(x)<=255):
                     return 'Neither'
-            return 'IPv4' 
-
+            return 'IPv4'
+        
         str = '1234567890abcdefABCDEF'
         if len(ip6Arr) == 8:
             for x in ip6Arr:
-                if len(x) < 1 or len(x) > 4:
+                if (len(x) == 0 or len(x) > 4):
                     return 'Neither'
                 for ch in x:
-                    if not ch in str:
-                        return 'Neither' 
+                    if ch not in str:
+                        return 'Neither'
             return 'IPv6'
         
-        return 'Neither' 
+        return 'Neither'
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+        
+        
+        
+        
+        
+        
+#         ip4Arr = queryIP.split('.')
+#         ip6Arr = queryIP.split(':')
+        
+#         if len(ip4Arr) == 4:
+#             for x in ip4Arr:
+#                 if not (x.isdigit() and (x=='0'or x == x.lstrip('0')) and 0 <=int(x)<=255):
+#                     return 'Neither'
+#             return 'IPv4' 
+
+#         str = '1234567890abcdefABCDEF'
+#         if len(ip6Arr) == 8:
+#             for x in ip6Arr:
+#                 if len(x) < 1 or len(x) > 4:
+#                     return 'Neither'
+#                 for ch in x:
+#                     if not ch in str:
+#                         return 'Neither' 
+#             return 'IPv6'
+        
+#         return 'Neither' 
                 
         
 
