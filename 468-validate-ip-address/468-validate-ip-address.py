66551f9288ebe67c8ef2150d94db343251f85c31
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-09-06 23:10:08 -0400

    Time: 70 ms (5.03%), Space: 14 MB (37.13%) - LeetHub

---
+++ 468-validate-ip-address/468-validate-ip-address.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        
+        ip4Arr = queryIP.split('.')
+        ip6Arr = queryIP.split(':')
+        
+        if len(ip4Arr) == 4:
+            for x in ip4Arr:
+                if not (x.isdigit() and (x=='0'or x == x.lstrip('0')) and 0 <=int(x)<=255):
+                    return 'Neither'
+            return 'IPv4' 
+
+        str = '1234567890abcdefABCDEF'
+        if len(ip6Arr) == 8:
+            for x in ip6Arr:
+                if len(x) < 1 or len(x) > 4:
+                    return 'Neither'
+                for ch in x:
+                    if not ch in str:
+                        return 'Neither' 
+            return 'IPv6'
+        
+        return 'Neither' 
+                
+        
+
+        
