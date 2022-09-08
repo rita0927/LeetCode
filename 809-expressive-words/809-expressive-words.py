0827a4f8e4b686138d0927c20997a70fd5fd6d56
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-09-07 20:11:20 -0400

    Time: 45 ms (98.54%), Space: 14.1 MB (30.46%) - LeetHub

--- 809-expressive-words/809-expressive-words.py
+++ 809-expressive-words/809-expressive-words.py
@@ -1,40 +1,53 @@
 class Solution:
     def expressiveWords(self, s: str, words: List[str]) -> int:
         
-        def compress(w):
-            letters = []
-            counts = []
+        def compress(word):
+            letters, counts = [], []
             
-            for ch in w:
+            for ch in word:
                 if not letters or ch != letters[-1]:
                     letters.append(ch)
                     counts.append(1)
                 else:
                     counts[-1]+=1
-                    
+            
             return letters, counts
         
         s_letters, s_counts = compress(s)
         
-        def is_stretchy(w):
-            w_letters, w_counts = compress(w)
+        def is_stretchy(word):
+            w_letters, w_counts = compress(word)
+            if s_letters != w_letters:
+                return False 
             
-            if w_letters != s_letters:
-                return False
-            
-            for i in range(len(w_counts)):
+            for i in range(len(w_letters)):
+                if w_letters[i] != s_letters[i]:
+                    return False
                 if s_counts[i] < 3 and s_counts[i] != w_counts[i]:
                     return False
                 if w_counts[i] > s_counts[i]:
                     return False
-            return True
+                
+            return True 
         
         res = 0
-        for w in words:
-            if is_stretchy(w):
+        for word in words:
+            if is_stretchy(word):
                 res += 1
         return res 
+                    
             
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
         
         
         
