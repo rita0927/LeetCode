c312ce8c3a3c0c3e6b2c4be73d2c8ca7bb28418f
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-09-08 01:35:20 -0400

    Time: 36 ms (86.25%), Space: 13.9 MB (65.43%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums) < 3:
+        if len(nums) <3:
             return max(nums)
         
         dp = [0] * len(nums)
-        
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
         
@@ -14,6 +13,57 @@ class Solution:
             
         
         return dp[-1]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # to maxmize the amt, skip the adjacent house. Either rob index 0,2,4.. or rob 1,3,5...
+#         # if skip the house -> take the previous dp
+#         # if rob the house -> take the dp[i-2] + nums[i]
+#         # compare two amount, and record the max as dp[i]
+        
+#         # if only 1 or 2 nums, take the max
+#         if len(nums) < 3:
+#             return max(nums)
+        
+#         dp = [0] * len(nums)
+        
+#         dp[0] = nums[0]
+#         dp[1] = max(nums[0], nums[1])
+        
+#         for i in range(2, len(nums)):
+#             # either the previous dp, or the skip the previous dp, take the sum of the i-2 and current num
+#             dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+#         # return the last dp 
+#         return dp[-1]
         
         
         
