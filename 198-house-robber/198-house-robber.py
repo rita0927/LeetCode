07bbdc5786c5cd1aa703d182ed5063ebeb0cf5ac
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-09-08 14:48:44 -0400

    Time: 50 ms (49.91%), Space: 13.9 MB (19.72%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums) <3:
+        if len(nums) < 3:
             return max(nums)
         
         dp = [0] * len(nums)
@@ -9,11 +9,13 @@ class Solution:
         dp[1] = max(nums[0], nums[1])
         
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-            
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
         
         return dp[-1]
         
+        
+        
+
         
         
         
