3fdbb8d4d7ffdde4dd31f6a7c37cf9056fc6bb9d
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-05 16:51:13 -0400

    Time: 79 ms (19.80%), Space: 14 MB (37.90%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -6,11 +6,9 @@ class Solution:
         """
 
         for i in range(len(nums2)):
-            nums1[m+i] = nums2[i]
+            nums1[m+i]=nums2[i]
+        
         nums1.sort()
-        
-        
-        
         
         
         
