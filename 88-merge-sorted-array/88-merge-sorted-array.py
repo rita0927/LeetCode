fe2c8b0e08736224236ebee21225ad0244443333
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-05 21:01:20 -0400

    Time: 78 ms (22.20%), Space: 13.9 MB (37.90%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -5,10 +5,22 @@ class Solution:
         
         """
 
-        for i in range(len(nums2)):
-            nums1[m+i]=nums2[i]
+        i1=m-1
+        i2=n-1
         
-        nums1.sort()
+        for i in range(m+n-1,-1,-1):
+            if i2<0:
+                break
+            if i1>=0 and nums1[i1]>nums2[i2]:
+                nums1[i]=nums1[i1]
+                i1-=1
+            else:
+                nums1[i]=nums2[i2]
+                i2-=1
+        return nums1
+            
+        
+
         
         
         
