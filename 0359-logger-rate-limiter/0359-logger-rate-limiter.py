691a9d1cdd66513ded252e6da60bcdc15625c0ce
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-16 12:35:36 -0400

    Time: 365 ms (19.03%), Space: 20.7 MB (98.08%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,15 +1,59 @@
 class Logger:
-
+    
+    
     def __init__(self):
-        self.d = dict()
+        self.set = set()
+        self.queue = deque()
+        
+    def shouldPrintMessage(self, timestamp, message):
+        while self.queue and timestamp - self.queue[0][1] >= 10:
+            msg, time = self.queue.popleft()
+            self.set.remove(msg)
+        
+        if message in self.set:
+            return False
+        else:
+            self.set.add(message)
+            self.queue.append((message, timestamp))
+            return True 
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Logger:
+
+#     def __init__(self):
+#         self.d = dict()
            
 
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.d or (message in self.d and timestamp - self.d[message] >=10):
-            self.d[message]=timestamp
-            return True 
-        else:
-            return False
+#     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+#         if message not in self.d or (message in self.d and timestamp - self.d[message] >=10):
+#             self.d[message]=timestamp
+#             return True 
+#         else:
+#             return False
 
          
 
