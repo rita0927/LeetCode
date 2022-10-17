d02ee0f77d24d5bc2b9c6002aa51b00fe282fab0
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-16 23:15:53 -0400

    Time: 184 ms (31.23%), Space: 14.3 MB (61.70%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -1,20 +1,46 @@
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
-        
-        # Time: O(N*S + Nlog(N)) => O(Nlog(N)) where N is the number of votes, and S is the length of each vote. There're only 26 chars so Max(S) is 26, constant O(1)
-        
-        # Space: O(N*S) => O(N)
-        
-        d = dict()
+        d=defaultdict(list)
         
         for vote in votes:
             for i, char in enumerate(vote):
                 if char not in d:
                     d[char]=[0]*len(vote)
                 d[char][i]+=1
+        
+        names=sorted(votes[0])
+        return ''.join(sorted(names, key=d.get, reverse=True))
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         # Time: O(N*S + Nlog(N)) => O(Nlog(N)) where N is the number of votes, and S is the length of each vote. There're only 26 chars so Max(S) is 26, constant O(1)
+        
+#         # Space: O(N*S) => O(N)
+        
+#         d = dict()
+        
+#         for vote in votes:
+#             for i, char in enumerate(vote):
+#                 if char not in d:
+#                     d[char]=[0]*len(vote)
+#                 d[char][i]+=1
                 
-        names=sorted(d.keys())
-        return ''.join(sorted(names, key=lambda x:d[x], reverse=True))
+#         names=sorted(d.keys())
+#         return ''.join(sorted(names, key=lambda x:d[x], reverse=True))
         
                 
         
