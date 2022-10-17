f4cbecdc02f5d8c81f85a3b79c0e4f498c6b9b98
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-17 10:59:05 -0400

    Time: 505 ms (42.47%), Space: 20.5 MB (82.56%) - LeetHub

---
+++ 1236-web-crawler/1236-web-crawler.py
@@ -0,0 +1,28 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        hostname='http://'+startUrl.split('/',3)[2]
+        res=set([startUrl])
+        # use the stack to iterate all related urls with the same hostname
+        stack=[startUrl]
+        while stack:
+            url=stack.pop()
+            # find all sub_url related to url
+            for sub_url in htmlParser.getUrls(url):
+                # if the related url contains the same hostname
+                if hostname in sub_url and sub_url not in res:
+                    res.add(sub_url)
+                    stack.append(sub_url)
+        return res 
+        
+        
