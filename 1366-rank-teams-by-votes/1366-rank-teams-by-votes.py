c2f32f828c381364827203da60175ef07861a40d
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-16 23:04:53 -0400

    Time: 83 ms (91.40%), Space: 14.3 MB (27.09%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        
+        # Time: O(N*S + Nlog(N)) => O(Nlog(N)) where N is the number of votes, and S is the length of each vote. There're only 26 chars so Max(S) is 26, constant O(1)
+        
+        # Space: O(N*S) => O(N)
+        
+        d = dict()
+        
+        for vote in votes:
+            for i, char in enumerate(vote):
+                if char not in d:
+                    d[char]=[0]*len(vote)
+                d[char][i]+=1
+                
+        names=sorted(d.keys())
+        return ''.join(sorted(names, key=lambda x:d[x], reverse=True))
+        
+                
+        
+        
+        
+        
+        
