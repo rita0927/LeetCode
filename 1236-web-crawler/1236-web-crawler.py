a3f36082b7e05da28a7da4354711d428a80541de
Yu Zhang <83844510+rita0927@users.noreply.github.com>
2022-10-17 11:05:47 -0400

    Time: 332 ms (74.39%), Space: 20.6 MB (53.20%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.py
+++ 1236-web-crawler/1236-web-crawler.py
@@ -13,16 +13,47 @@ class Solution:
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
         hostname='http://'+startUrl.split('/',3)[2]
         res=set([startUrl])
-        # use the stack to iterate all related urls with the same hostname
         stack=[startUrl]
         while stack:
             url=stack.pop()
-            # find all sub_url related to url
             for sub_url in htmlParser.getUrls(url):
-                # if the related url contains the same hostname
                 if hostname in sub_url and sub_url not in res:
                     res.add(sub_url)
                     stack.append(sub_url)
-        return res 
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # hostname='http://'+startUrl.split('/',3)[2]
+        # res=set([startUrl])
+        # # use the stack to iterate all related urls with the same hostname
+        # stack=[startUrl]
+        # while stack:
+        #     url=stack.pop()
+        #     # find all sub_url related to url
+        #     for sub_url in htmlParser.getUrls(url):
+        #         # if the related url contains the same hostname
+        #         if hostname in sub_url and sub_url not in res:
+        #             res.add(sub_url)
+        #             stack.append(sub_url)
+        # return res 
         
         
